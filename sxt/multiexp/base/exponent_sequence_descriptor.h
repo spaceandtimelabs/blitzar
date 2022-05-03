440a155ce3917a146430fdbe6f634b494c8a4f8a
Ryan Burn <ryan.burn@gmail.com>
2022-05-03 02:03:31 +0000

    add commitment type

---
+++ sxt/multiexp/base/exponent_sequence_descriptor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+namespace sxt::mtxb {
+//--------------------------------------------------------------------------------------------------
+// exponent_sequence_descriptor
+//--------------------------------------------------------------------------------------------------
+struct exponent_sequence_descriptor {
+  // the number of bytes used to represent an element in the sequence
+  // element_nbytes must be a power of 2 and must satisfy
+  //    1 <= element_nbytes <= 32
+  uint8_t element_nbytes;
+
+  // the number of elements in the sequence
+  uint64_t n;
+
+  // pointer to the data for the sequence of elements where there are n elements
+  // in the sequence and each element enocodes a number of element_nbytes bytes
+  // represented in the little endian format
+  const void* data;
+};
+}  // namespace sxt::mtxb
