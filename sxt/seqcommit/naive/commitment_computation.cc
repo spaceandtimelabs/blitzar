0b40118d25254c89c33f072ea05265391812a5ca
Ryan Burn <ryan.burn@gmail.com>
2022-05-03 03:58:55 +0000

    add assertion

--- sxt/seqcommit/naive/commitment_computation.cc
+++ sxt/seqcommit/naive/commitment_computation.cc
@@ -1,5 +1,7 @@
 #include "sxt/seqcommit/naive/commitment_computation.h"
 
+#include <cassert>
+
 #include "sxt/seqcommit/base/commitment.h"
 #include "sxt/multiexp/base/exponent_sequence.h"
 
@@ -10,6 +12,7 @@ namespace sxt::sqcnv {
 void compute_commitments(
     basct::span<sqcb::commitment> commitments,
     basct::cspan<mtxb::exponent_sequence> value_sequences) noexcept {
+  assert(commitments.size() == value_sequences.size());
   (void)commitments;
   (void)value_sequences;
 }
