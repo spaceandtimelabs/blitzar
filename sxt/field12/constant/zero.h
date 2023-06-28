9fdaa6a8218bc65daf7b52bc2c5ba41a1a8ebcaf
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-06-28 13:27:57 -0400

    feat: add constants package to field12 package group (PROOF-512) (#265)
    feat: add constants package to field12 package group ( PROOF-512 )

---
+++ sxt/field12/constant/zero.h
@@ -0,0 +1,26 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "sxt/field12/type/element.h"
+
+namespace sxt::f12cn {
+//--------------------------------------------------------------------------------------------------
+// zero_v
+//--------------------------------------------------------------------------------------------------
+static constexpr f12t::element zero_v{0, 0, 0, 0, 0, 0};
+} // namespace sxt::f12cn
