1918857ed54406147d9124e998bbde9bdbff36e0
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-07-07 15:39:50 -0400

    feat: add cmov component to operations package for G1 curve support (PROOF-551) (#271)
    * feat: implement cmov for the G1 curve and Fp base field ( PROOF-551 )
    
    * refactor: use num_of_limbs variable to refactor cmov loop ( PROOF-551 )

---
+++ sxt/curve_g1/operation/cmov.h
@@ -0,0 +1,34 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+namespace sxt::cg1t {
+class element_p2;
+}
+
+namespace sxt::cg1o {
+//--------------------------------------------------------------------------------------------------
+// cmov
+//--------------------------------------------------------------------------------------------------
+/*
+ Replace (f,g) with (g,g) if b == 1;
+ replace (f,g) with (f,g) if b == 0.
+ *
+ Preconditions: b in {0,1}.
+ */
+void cmov(cg1t::element_p2& f, const cg1t::element_p2& g, unsigned int b) noexcept;
+} // namespace sxt::cg1o
