cc676972b637417e4eaf6b6d2237ebb842b3c401
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-07-10 13:22:29 -0400

    feat: add component to identify if an element is the identity to the G1 curve property package ( PROOF-553 ) (#273)
    feat: add identity componenet to property package ( PROOF-553 )

---
+++ sxt/curve_g1/property/identity.t.cc
@@ -0,0 +1,36 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "sxt/curve_g1/property/identity.h"
+
+#include "sxt/base/test/unit_test.h"
+#include "sxt/curve_g1/constant/generator.h"
+#include "sxt/curve_g1/constant/identity.h"
+
+using namespace sxt;
+using namespace sxt::cg1p;
+
+TEST_CASE("the identity can be identified") {
+  SECTION("as an affine element") {
+    REQUIRE(cg1p::is_identity(cg1cn::identity_affine_v));
+    REQUIRE(!cg1p::is_identity(cg1cn::generator_affine_v));
+  }
+
+  SECTION("as a projective element") {
+    REQUIRE(cg1p::is_identity(cg1cn::identity_p2_v));
+    REQUIRE(!cg1p::is_identity(cg1cn::generator_p2_v));
+  }
+}
