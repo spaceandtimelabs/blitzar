22e799048267b76443b1dc6887377c77b7a4bcf2
Joe <joseribeiro1017@gmail.com>
2023-08-24 20:39:52 -0300

    chore: small change to build script

--- ci/build.sh
+++ ci/build.sh
@@ -50,7 +50,7 @@ if [ "$#" -eq 3 ]; then
         cd ..
         zip -r ${DIST_PATH}/blitzar-sys-v${NEW_VERSION}.zip ${LIB_PATH}
         tar -czvf ${DIST_PATH}/blitzar-sys-v${NEW_VERSION}.tar.gz ${LIB_PATH}
-        
+
         cd ${LIB_PATH}
         cargo publish --allow-dirty --token ${CRATES_TOKEN}
     fi
