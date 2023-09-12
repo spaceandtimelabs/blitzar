1b3f407f48e369c30facad95112143efbfcd2e80
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-09-12 16:25:51 -0400

    ci: undo gcc compiler option removal ( PROOF-633 )

--- ci/docker/setup_build_environment.sh
+++ ci/docker/setup_build_environment.sh
@@ -24,6 +24,12 @@ apt-get install --no-install-recommends --no-install-suggests -y \
                 graphviz \
                 valgrind
 
+# Upgrade to g++10
+# See https://ahelpme.com/linux/ubuntu/install-and-make-gnu-gcc-10-default-in-ubuntu-20-04-focal/
+apt-get install --no-install-recommends --no-install-suggests -y \
+                gcc-10 g++-10 cpp-10
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+
 # Upgrade to clang-18
 # See https://linux.how2shout.com/how-to-install-clang-on-ubuntu-linux/
 wget https://apt.llvm.org/llvm.sh
