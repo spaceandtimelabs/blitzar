069e8cb9d30e24bab20bd5386d32d5a12e1a6a01
Ryan Burn <ryan.burn@gmail.com>
2023-09-12 16:30:34 -0700

    bump bazel version

--- ci/docker/run_docker_gpu.sh
+++ ci/docker/run_docker_gpu.sh
@@ -3,6 +3,7 @@
 set -e
 
 IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-1
+IMAGE=bz-bld
 
 # If you have a GPU instance configured in your machine
-docker run --rm -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE" /bin/bash -l
+docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE" /bin/bash -l
