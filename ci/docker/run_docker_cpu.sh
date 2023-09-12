a3f7ae61a8c58ac414d56aa9ce24d2431d1e66b4
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-09-12 16:49:13 -0400

    ci: update the Docker image tag ( PROOF-638 )

--- ci/docker/run_docker_cpu.sh
+++ ci/docker/run_docker_cpu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-0
+IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-1
 
 # If you don't have a GPU instance configured
 docker run --rm -v "$PWD":/src -w /src --privileged -it "$IMAGE" /bin/bash -l
