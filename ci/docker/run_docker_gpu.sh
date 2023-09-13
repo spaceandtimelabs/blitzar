34b98da76f3a671b56d9d5f56f22ce1fb4b4dc6b
Ryan Burn <ryan.burn@gmail.com>
2023-09-13 12:42:01 -0700

    fix gpu image

--- ci/docker/run_docker_gpu.sh
+++ ci/docker/run_docker_gpu.sh
@@ -3,7 +3,6 @@
 set -e
 
 IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-1
-IMAGE=bz-bld
 
 # If you have a GPU instance configured in your machine
 docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE" /bin/bash -l
