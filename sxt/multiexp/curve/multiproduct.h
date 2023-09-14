bfe63c453964d37e4104465b93a7164e03eb1b68
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-09-14 13:53:27 -0400

    :refactor: initial check in of generic multiproduct code ( PROOF-589 )

--- sxt/multiexp/curve/multiproduct.h
+++ sxt/multiexp/curve/multiproduct.h
@@ -14,44 +14,51 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "sxt/multiexp/curve21/multiproduct.h"
+#pragma once
 
 #include "sxt/algorithm/base/gather_mapper.h"
+#include "sxt/base/container/span.h"
+#include "sxt/base/curve/element.h"
 #include "sxt/base/macro/cuda_callable.h"
 #include "sxt/curve21/operation/accumulator.h"
-#include "sxt/curve21/operation/neg.h"
 #include "sxt/execution/async/future.h"
+#include "sxt/execution/async/future_fwd.h"
 #include "sxt/multiexp/multiproduct_gpu/multiproduct.h"
 
-namespace sxt::mtxc21 {
+namespace sxt::basdv {
+class stream;
+}
+
+namespace sxt::mtxcrv {
 //--------------------------------------------------------------------------------------------------
 // signed_mapper
 //--------------------------------------------------------------------------------------------------
 namespace {
+template <bascrv::element Element>
 class signed_mapper {
 public:
-  using value_type = c21t::element_p3;
+  using value_type = Element;
 
-  CUDA_CALLABLE signed_mapper(const c21t::element_p3* generators, const unsigned* indexes) noexcept
+  CUDA_CALLABLE signed_mapper(const Element* generators, const unsigned* indexes) noexcept
       : generators_{generators}, indexes_{indexes} {}
 
-  CUDA_CALLABLE void map_index(c21t::element_p3& val, unsigned int index) const noexcept {
+  CUDA_CALLABLE void map_index(Element& val, unsigned int index) const noexcept {
     constexpr auto sign_bit = 1u << 31;
     auto i = indexes_[index];
     auto is_neg = static_cast<unsigned>((i & sign_bit) != 0);
     i = i & ~sign_bit;
     val = generators_[i];
-    c21o::cneg(val, is_neg);
+    cneg(val, is_neg);
   }
 
-  CUDA_CALLABLE c21t::element_p3 map_index(unsigned int index) const noexcept {
-    c21t::element_p3 res;
+  CUDA_CALLABLE Element map_index(unsigned int index) const noexcept {
+    Element res;
     this->map_index(res, index);
     return res;
   }
 
 private:
-  const c21t::element_p3* generators_;
+  const Element* generators_;
   const unsigned* indexes_;
 };
 } // namespace
@@ -59,18 +66,19 @@ private:
 //--------------------------------------------------------------------------------------------------
 // async_compute_multiproduct
 //--------------------------------------------------------------------------------------------------
-xena::future<> async_compute_multiproduct(basct::span<c21t::element_p3> products,
-                                          const basdv::stream& stream,
-                                          basct::cspan<c21t::element_p3> generators,
-                                          basct::cspan<unsigned> indexes,
-                                          basct::cspan<unsigned> product_sizes,
-                                          bool is_signed) noexcept {
+template <bascrv::element Element>
+inline xena::future<> async_compute_multiproduct(basct::span<Element> products,
+                                                 const basdv::stream& stream,
+                                                 basct::cspan<Element> generators,
+                                                 basct::cspan<unsigned> indexes,
+                                                 basct::cspan<unsigned> product_sizes,
+                                                 bool is_signed) noexcept {
   if (!is_signed) {
-    using Mapper = algb::gather_mapper<c21t::element_p3>;
+    using Mapper = algb::gather_mapper<Element>;
     return mtxmpg::compute_multiproduct<c21o::accumulator, Mapper>(products, stream, generators,
                                                                    indexes, product_sizes);
   }
-  return mtxmpg::compute_multiproduct<c21o::accumulator, signed_mapper>(
+  return mtxmpg::compute_multiproduct<c21o::accumulator, signed_mapper<Element>>(
       products, stream, generators, indexes, product_sizes);
 }
-} // namespace sxt::mtxc21
+} // namespace sxt::mtxcrv
