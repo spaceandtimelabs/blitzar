463839d0f233c60b4be7e83d7ed3e0edc91af846
Ryan Burn <ryan.burn@gmail.com>
2023-09-20 16:52:57 -0700

    work on bucket method

---
+++ sxt/multiexp/bucket_method/accumulate_kernel.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cstdint>
+#include <concepts>
+
+#include "sxt/base/num/divide_up.h"
+#include "sxt/algorithm/base/mapper.h"
+#include "sxt/algorithm/base/reducer.h"
+
+namespace sxt::mtxbk {
+//--------------------------------------------------------------------------------------------------
+// bucket_accumulate
+//--------------------------------------------------------------------------------------------------
+template <algb::reducer Reducer, unsigned int BlockSize, algb::mapper Mapper>
+  requires std::same_as<typename Reducer::value_type, typename Mapper::value_type>
+__global__ void bucket_accumulate(typename Reducer::value_type* bucket_sums,
+                                  Mapper generator_mapper, const uint8_t* scalars,
+                                  unsigned num_generators) {
+  using T = typename Reducer::value_type;
+  auto block_index = blockIdx.x;
+  auto scalar_byte_index = threadIdx.x;
+  auto scalar_num_bytes = blockDim.x;
+  auto num_blocks = gridDim.x;
+  auto num_buckets_per_generator = blockDim.x;
+
+  auto num_generators_per_block = basn::divide_up(num_generators, num_blocks);
+  auto generator_index = block_index * num_generators_per_block;
+  auto generator_last = min(generator_index + num_generators_per_block, num_generators);
+
+  T g;
+  T sums[255] = {};
+  for (; generator_index<generator_last; ++generator_index) {
+    generator_mapper.map_index(g, generator_index);
+    auto scalar = scalars + generator_index * scalar_num_bytes;
+    auto val = scalar[scalar_byte_index];
+    if (val == 0) {
+      continue;
+    }
+    sums[val-1] += g;
+    (void)g;
+  }
+  (void)num_blocks;
+  (void)num_buckets_per_generator;
+
+  (void)bucket_sums;
+  (void)scalars;
+  (void)num_generators;
+}
+} // namespace sxt::mtxbk
