43e81ebce463409e78905644abe82add8ee11271
Ryan Burn <ryan.burn@gmail.com>
2023-09-22 15:54:59 -0700

    fill in combination kernel

--- sxt/multiexp/bucket_method/combination_kernel.t.cc
+++ sxt/multiexp/bucket_method/combination_kernel.t.cc
@@ -1,2 +1,23 @@
 #include "sxt/multiexp/bucket_method/combination_kernel.h"
 
+#include "sxt/algorithm/reduction/test_reducer.h"
+#include "sxt/base/device/synchronization.h"
+#include "sxt/base/test/unit_test.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/managed_device_resource.h"
+using namespace sxt;
+using namespace sxt::mtxbk;
+
+TEST_CASE("we can combine partial bucket sums") {
+  memmg::managed_array<uint64_t> partial_bucket_sums(memr::get_managed_device_resource());
+  memmg::managed_array<uint64_t> bucket_sums(memr::get_managed_device_resource());
+
+  SECTION("we handle the case of a single bucket") {
+    partial_bucket_sums = {123};
+    bucket_sums = {0};
+    combine_partial_bucket_sums<algr::test_add_reducer, 1>
+        <<<1, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 1);
+    basdv::synchronize_device();
+    REQUIRE(bucket_sums[0] == 123);
+  }
+}
