d94469b57b3963a4488f2bf7f159f8cdf7049d1b
Ryan Burn <ryan.burn@gmail.com>
2023-09-22 15:14:31 -0700

    fill in combination kernel

--- sxt/multiexp/bucket_method/combination_kernel.h
+++ sxt/multiexp/bucket_method/combination_kernel.h
@@ -9,12 +9,17 @@ namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
 template <algb::reducer Reducer>
 __global__ void bucket_accumulate(typename Reducer::value_type* bucket_sums,
-                                  const unsigned* lengths) {
+                                  const unsigned* num_partial_buckets) {
   auto bucket_index = blockIdx.x;
+  auto num_buckets_per_generator = gridDim.x;
   auto output_index = blockIdx.y;
+  for (unsigned i=0; i<output_index; ++i) {
+    bucket_sums += *num_partial_buckets++ * num_buckets_per_generator;
+  }
+  auto n = *num_partial_buckets;
+  bucket_sums += bucket_index;
   (void)bucket_index;
   (void)output_index;
   (void)bucket_sums;
-  (void)lengths;
 }
 } // namespace sxt::mtxbk
