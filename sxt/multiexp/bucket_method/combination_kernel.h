79f8a6b5e00c0d7f85edfd3fdec5844ef1426643
Ryan Burn <ryan.burn@gmail.com>
2023-09-22 15:46:16 -0700

    fill in combination kernel

--- sxt/multiexp/bucket_method/combination_kernel.h
+++ sxt/multiexp/bucket_method/combination_kernel.h
@@ -1,25 +1,36 @@
 #pragma once
 
-#include "sxt/base/num/divide_up.h"
 #include "sxt/algorithm/base/reducer.h"
+#include "sxt/algorithm/reduction/warp_reduction.h"
+#include "sxt/base/num/divide_up.h"
 
 namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
-// bucket_accumulate
+// combine_partial_bucket_sums
 //--------------------------------------------------------------------------------------------------
-template <algb::reducer Reducer>
-__global__ void bucket_accumulate(typename Reducer::value_type* bucket_sums,
-                                  const unsigned* num_partial_buckets) {
+template <algb::reducer Reducer, unsigned BlockSize>
+__global__ void combine_partial_bucket_sums(typename Reducer::value_type* out,
+                                            typename Reducer::value_type* partial_bucket_sums,
+                                            unsigned num_partial_buckets) {
+  static_assert(BlockSize <= 32);
+  using T = typename Reducer::value_type;
+  auto thread_index = threadIdx.x;
   auto bucket_index = blockIdx.x;
   auto num_buckets_per_generator = gridDim.x;
   auto output_index = blockIdx.y;
-  for (unsigned i=0; i<output_index; ++i) {
-    bucket_sums += *num_partial_buckets++ * num_buckets_per_generator;
+  partial_bucket_sums += num_partial_buckets * num_buckets_per_generator * output_index;
+  partial_bucket_sums += bucket_index;
+  __shared__ T shared_data[BlockSize];
+  shared_data[thread_index] = partial_bucket_sums[thread_index];
+  auto index = thread_index + BlockSize;
+  while (index < num_partial_buckets) {
+    Reducer::accumulate_inplace(shared_data[thread_index], partial_bucket_sums[index]);
+    index += BlockSize;
   }
-  auto n = *num_partial_buckets;
-  bucket_sums += bucket_index;
-  (void)bucket_index;
-  (void)output_index;
-  (void)bucket_sums;
+  algr::warp_reduce<Reducer, BlockSize>(shared_data, thread_index);
+  out += num_buckets_per_generator * output_index;
+  if (thread_index == 0) {
+    out[bucket_index] = shared_data[0];
+  }
 }
 } // namespace sxt::mtxbk
