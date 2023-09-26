e7a6ad69a83fdf95481f93c0d43b32bbd0232dab
Ryan Burn <ryan.burn@gmail.com>
2023-09-26 16:26:46 -0700

    fill in bucket algorithm

--- sxt/multiexp/bucket_method/accumulate_kernel.h
+++ sxt/multiexp/bucket_method/accumulate_kernel.h
@@ -3,9 +3,10 @@
 #include <cstdint>
 #include <concepts>
 
-#include "sxt/base/num/divide_up.h"
 #include "sxt/algorithm/base/mapper.h"
 #include "sxt/algorithm/base/reducer.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/base/num/divide_up.h"
 
 namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
@@ -49,6 +50,42 @@ __global__ void bucket_accumulate(typename Reducer::value_type* bucket_sums,
       continue;
     }
     Reducer::accumulate_inplace(bucket_sums[val-1], g);
+  }
+}
+
+template <bascrv::element T>
+__global__ void bucket_accumulate(T* bucket_sums, const T* generators, const uint8_t* scalars,
+                                  unsigned length) {
+  auto block_index = blockIdx.x;
+  auto scalar_byte_index = threadIdx.x;
+  auto scalar_num_bytes = blockDim.x;
+  auto num_blocks = gridDim.x;
+  auto output_index = blockIdx.y;
+  auto num_bucket_groups = blockDim.x;
+  static constexpr int bucket_group_size = 255;
+
+  scalars += length * scalar_num_bytes * output_index;
+  auto num_generators = length;
+  auto num_generators_per_block = basn::divide_up(num_generators, num_blocks);
+  auto generator_index = block_index * num_generators_per_block;
+  auto generator_last = min(generator_index + num_generators_per_block, num_generators);
+
+  bucket_sums += bucket_group_size * scalar_byte_index +
+                 bucket_group_size * num_bucket_groups * block_index +
+                 bucket_group_size * num_bucket_groups * num_blocks * output_index;
+
+  scalars += scalar_byte_index + generator_index * scalar_num_bytes;
+
+  for (int i=0; i<bucket_group_size; ++i) {
+    bucket_sums[i] = T::identity();
+  }
+  for (; generator_index<generator_last; ++generator_index) {
+    auto val = *scalars;
+    scalars += scalar_num_bytes;
+    if (val == 0) {
+      continue;
+    }
+    add(bucket_sums[val - 1], bucket_sums[val - 1], generators[generator_index]);
   }
 }
 } // namespace sxt::mtxbk
