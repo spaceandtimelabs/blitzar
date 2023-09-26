3c525614a5a1128c98b3e7f1e84b87bf811c70e0
Ryan Burn <ryan.burn@gmail.com>
2023-09-25 18:27:56 -0700

    fill bucket accumulation

--- sxt/multiexp/bucket_method/bucket_accumulation.h
+++ sxt/multiexp/bucket_method/bucket_accumulation.h
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 
+#include "sxt/base/error/assert.h"
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
 
@@ -12,6 +13,15 @@ namespace sxt::mtxbk {
 template <bascrv::element T>
 void accumulate_buckets(basct::span<T> bucket_sums, basct::cspan<T> generators,
                         basct::cspan<const uint8_t*> exponents) noexcept {
+  constexpr size_t bucket_group_size = 255;
+  constexpr size_t num_bucket_groups = 32;
+  auto num_outputs = exponents.size();
+  SXT_DEBUG_ASSERT(
+      bucket_sums.size() == bucket_group_size * num_bucket_groups * num_outputs
+  );
+  for (auto& e : bucket_sums) {
+    e = T::identity();
+  }
   (void)bucket_sums;
   (void)generators;
   (void)exponents;
