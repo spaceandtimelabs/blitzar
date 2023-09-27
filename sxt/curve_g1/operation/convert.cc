7619881a44d3affd4619f44a2199150d776f271b
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-09-27 13:59:10 -0400

    feat: add converstion between g1 projective and affine elements ( PROOF-666 )

---
+++ sxt/curve_g1/operation/convert.cc
@@ -0,0 +1,57 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Adopted from zcash/librustzcash
+ *
+ * Copyright (c) 2017
+ * Zcash Company
+ *
+ * See third_party/license/zcash.LICENSE
+ */
+#include "sxt/curve_g1/operation/convert.h"
+
+#include "sxt/curve_g1/constant/identity.h"
+#include "sxt/curve_g1/operation/cmov.h"
+#include "sxt/curve_g1/type/element_affine.h"
+#include "sxt/curve_g1/type/element_p2.h"
+#include "sxt/field12/operation/cmov.h"
+#include "sxt/field12/operation/invert.h"
+#include "sxt/field12/operation/mul.h"
+#include "sxt/field12/type/element.h"
+
+namespace sxt::cg1o {
+//--------------------------------------------------------------------------------------------------
+// convert
+//--------------------------------------------------------------------------------------------------
+CUDA_CALLABLE
+void convert(cg1t::element_affine& a, const cg1t::element_p2& p) noexcept {
+  f12t::element z_inv;
+  const bool is_zero{f12o::invert(z_inv, p.Z)};
+  f12o::cmov(z_inv, f12cn::zero_v, is_zero);
+
+  f12t::element x;
+  f12t::element y;
+  f12o::mul(x, p.X, z_inv);
+  f12o::mul(y, p.Y, z_inv);
+
+  a.X = x;
+  a.Y = y;
+  a.infinity = false;
+
+  cg1o::cmov(a, cg1cn::identity_affine_v, is_zero);
+}
+} // namespace sxt::cg1o
