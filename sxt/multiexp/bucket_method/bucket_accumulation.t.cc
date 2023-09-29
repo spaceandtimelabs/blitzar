2a4b3b44d54cccd699af357463970e13ea3d09a8
Ryan Burn <ryan.burn@gmail.com>
2023-09-29 14:42:59 -0700

    fill in bucket testing

--- sxt/multiexp/bucket_method/bucket_accumulation.t.cc
+++ sxt/multiexp/bucket_method/bucket_accumulation.t.cc
@@ -1,5 +1,6 @@
 #include "sxt/multiexp/bucket_method/bucket_accumulation.h"
 
+#include "sxt/execution/schedule/scheduler.h"
 #include "sxt/base/curve/example_element.h"
 #include "sxt/base/test/unit_test.h"
 #include "sxt/memory/management/managed_array.h"
@@ -10,12 +11,23 @@ TEST_CASE("we can perform a bucket accumulation pass") {
   using E = bascrv::element97;
   memmg::managed_array<E> bucket_sums(255 * 32);
 
-  SECTION("OtTH", "we handle the empty case") {
+  SECTION("we handle the empty case") {
     bucket_sums.reset();
     auto fut = accumulate_buckets<E>(bucket_sums, {}, {});    
     REQUIRE(fut.ready());
   }
+
+  SECTION("we handle a case with a single zero element") {
+    uint8_t scalar[32] = {};
+    const uint8_t* scalars[] = {
+      scalar
+    };
+    E generators[] = {7};
+    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    for (auto val : bucket_sums) {
+      REQUIRE(val == 0);
+    }
+  }
 }
-/* template <bascrv::element T> */
-/* xena::future<> accumulate_buckets(basct::span<T> bucket_sums, basct::cspan<T> generators, */
-/*                                   basct::cspan<const uint8_t*> exponents) noexcept { */
