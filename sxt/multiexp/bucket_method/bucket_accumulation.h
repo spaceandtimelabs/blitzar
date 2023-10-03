fa3423c434ef9b89be982cd622deebd300dc4c99
Ryan Burn <ryan.burn@gmail.com>
2023-10-02 21:28:01 -0700

    rework bucket method

--- sxt/multiexp/bucket_method/bucket_accumulation.h
+++ sxt/multiexp/bucket_method/bucket_accumulation.h
@@ -36,54 +36,6 @@ make_exponents_viewable(memmg::managed_array<uint8_t>& exponents_viewable_data,
 //--------------------------------------------------------------------------------------------------
 template <bascrv::element T>
 xena::future<> accumulate_buckets_impl(basct::span<T> bucket_sums, basct::cspan<T> generators,
-                                       basct::cspan<const uint8_t*> exponents,
-                                       basit::index_range rng) noexcept {
-  unsigned n = rng.size();
-  auto num_outputs = exponents.size();
-  auto num_blocks = std::min(192u, n);
-  static constexpr int num_bytes = 32; // hard code to 32 for now
-
-  basdv::stream stream;
-
-  // partial_bucket_sums
-  memr::async_device_resource resource{stream};
-  memmg::managed_array<T> partial_bucket_sums{bucket_sums.size() * num_blocks, &resource};
-
-  // generators_viewable
-  memmg::managed_array<T> generators_viewable_data{&resource};
-  auto generators_viewable = xendv::make_active_device_viewable(
-      generators_viewable_data, generators.subspan(rng.a(), rng.size()));
-
-  // exponents_viewable
-  memmg::managed_array<uint8_t> exponents_viewable_data{&resource};
-  auto exponents_viewable =
-      make_exponents_viewable(exponents_viewable_data, exponents, rng, stream);
-
-  // partial bucket accumulation kernel
-  xendv::synchronize_event(stream, generators_viewable);
-  bucket_accumulate<<<dim3(num_blocks, num_outputs, 1), num_bytes, 0, stream>>>(
-      partial_bucket_sums.data(), generators_viewable.value().data(), exponents_viewable.data(), n);
-  generators_viewable_data.reset();
-  exponents_viewable_data.reset();
-
-
-  // combine partial sums
-  memmg::managed_array<T> bucket_sums_dev{bucket_sums.size(), &resource};
-  combine_partial_bucket_sums<<<dim3(255, num_outputs, 1), num_bytes, 0, stream>>>(
-      bucket_sums_dev.data(), partial_bucket_sums.data(), num_blocks);
-  partial_bucket_sums.reset();
-
-  // add buckets
-  memmg::managed_array<T> bucket_sums_host{bucket_sums.size(), memr::get_pinned_resource()};
-  basdv::async_copy_device_to_host(bucket_sums_host, bucket_sums_dev, stream);
-  co_await xendv::await_stream(stream);
-  for (size_t bucket_index=0; bucket_index<bucket_sums.size(); ++bucket_index) {
-    add_inplace(bucket_sums[bucket_index], bucket_sums_host[bucket_index]);
-  }
-}
-
-template <bascrv::element T>
-xena::future<> accumulate_buckets_impl2(basct::span<T> bucket_sums, basct::cspan<T> generators,
                                         basct::cspan<const uint8_t*> exponents,
                                         basit::index_range rng) noexcept {
   unsigned n = rng.size();
@@ -132,24 +84,6 @@ xena::future<> accumulate_buckets(basct::span<T> bucket_sums, basct::cspan<T> ge
                                   basct::cspan<const uint8_t*> exponents) noexcept {
   constexpr size_t bucket_group_size = 255;
   constexpr size_t num_bucket_groups = 32;
-  auto num_outputs = exponents.size();
-  SXT_DEBUG_ASSERT(
-      bucket_sums.size() == bucket_group_size * num_bucket_groups * num_outputs
-  );
-  for (auto& e : bucket_sums) {
-    e = T::identity();
-  }
-  co_await xendv::concurrent_for_each(
-      basit::index_range{0, generators.size()}, [&](const basit::index_range& rng) noexcept {
-        return accumulate_buckets_impl(bucket_sums, generators, exponents, rng);
-      });
-}
-
-template <bascrv::element T>
-xena::future<> accumulate_buckets2(basct::span<T> bucket_sums, basct::cspan<T> generators,
-                                  basct::cspan<const uint8_t*> exponents) noexcept {
-  constexpr size_t bucket_group_size = 255;
-  constexpr size_t num_bucket_groups = 32;
   static constexpr unsigned num_bytes = 32; // hard code to 32 for now
   auto num_outputs = exponents.size();
   SXT_DEBUG_ASSERT(
@@ -174,7 +108,7 @@ xena::future<> accumulate_buckets2(basct::span<T> bucket_sums, basct::cspan<T> g
   size_t step = bucket_group_size * num_bucket_groups * num_outputs;
   size_t i = 0;
   co_await xendv::concurrent_for_each(first, last, [&](const basit::index_range& rng) noexcept {
-    return accumulate_buckets_impl2(partial_bucket_sums.subspan(step * i++, step), generators,
+    return accumulate_buckets_impl(partial_bucket_sums.subspan(step * i++, step), generators,
                                     exponents, rng);
   });
   if (num_chunks <= 1) {
--- sxt/multiexp/bucket_method/bucket_accumulation.t.cc
+++ sxt/multiexp/bucket_method/bucket_accumulation.t.cc
@@ -11,7 +11,7 @@ using namespace sxt::mtxbk;
 
 TEST_CASE("we can perform a bucket accumulation pass") {
   using E = bascrv::element97;
-  memmg::managed_array<E> bucket_sums(255 * 32);
+  memmg::managed_array<E> bucket_sums{255 * 32, memr::get_managed_device_resource()};
 
   SECTION("we handle the empty case") {
     bucket_sums.reset();
@@ -28,119 +28,6 @@ TEST_CASE("we can perform a bucket accumulation pass") {
     auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
-    for (auto val : bucket_sums) {
-      REQUIRE(val == 0);
-    }
-  }
-
-  SECTION("we handle a case with a single element of 1") {
-    uint8_t scalar[32] = {};
-    scalar[0] = 1;
-    const uint8_t* scalars[] = {
-      scalar
-    };
-    E generators[] = {7};
-    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
-    xens::get_scheduler().run();
-    REQUIRE(fut.ready());
-    for (size_t i=0; i<bucket_sums.size(); ++i) {
-      auto val = bucket_sums[i];
-      if (i == 0) {
-        REQUIRE(val == 7);
-      } else {
-        REQUIRE(val == 0);
-      }
-    }
-  }
-
-  SECTION("we handle two scalars of different values") {
-    uint8_t scalar_data[64] = {};
-    scalar_data[0] = 1;
-    scalar_data[32] = 2;
-    const uint8_t* scalars[] = {
-      scalar_data
-    };
-    E generators[] = {7, 5};
-    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
-    xens::get_scheduler().run();
-    REQUIRE(fut.ready());
-    for (size_t i=0; i<bucket_sums.size(); ++i) {
-      auto val = bucket_sums[i];
-      if (i == 0) {
-        REQUIRE(val == 7);
-      } else if (i == 1) {
-        REQUIRE(val == 5);
-      } else {
-        REQUIRE(val == 0);
-      }
-    }
-  }
-
-  SECTION("we handle two scalars of the same value") {
-    uint8_t scalar_data[64] = {};
-    scalar_data[0] = 2;
-    scalar_data[32] = 2;
-    const uint8_t* scalars[] = {
-      scalar_data
-    };
-    E generators[] = {7, 5};
-    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
-    xens::get_scheduler().run();
-    REQUIRE(fut.ready());
-    for (size_t i=0; i<bucket_sums.size(); ++i) {
-      auto val = bucket_sums[i];
-      if (i == 1) {
-        REQUIRE(val == 12);
-      } else {
-        REQUIRE(val == 0);
-      }
-    }
-  }
-
-  SECTION("we handle multiple outputs") {
-    bucket_sums = memmg::managed_array<E>(255 * 32 * 2);
-    uint8_t scalar_data1[32] = {};
-    scalar_data1[0] = 2;
-    uint8_t scalar_data2[32] = {};
-    scalar_data2[0] = 2;
-    const uint8_t* scalars[] = {
-      scalar_data1,
-      scalar_data2,
-    };
-    E generators[] = {7};
-    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
-    xens::get_scheduler().run();
-    REQUIRE(fut.ready());
-    for (size_t i=0; i<bucket_sums.size(); ++i) {
-      auto val = bucket_sums[i];
-      if (i == 1 || i == 255 * 32 + 1) {
-        REQUIRE(val == 7);
-      } else {
-        REQUIRE(val == 0);
-      }
-    }
-  }
-}
-
-TEST_CASE("we can perform a bucket accumulation pass 2") {
-  using E = bascrv::element97;
-  memmg::managed_array<E> bucket_sums{255 * 32, memr::get_managed_device_resource()};
-
-  SECTION("we handle the empty case") {
-    bucket_sums.reset();
-    auto fut = accumulate_buckets2<E>(bucket_sums, {}, {});    
-    REQUIRE(fut.ready());
-  }
-
-  SECTION("we handle a case with a single zero element") {
-    uint8_t scalar[32] = {};
-    const uint8_t* scalars[] = {
-      scalar
-    };
-    E generators[] = {7};
-    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
-    xens::get_scheduler().run();
-    REQUIRE(fut.ready());
     basdv::synchronize_device();
     for (auto val : bucket_sums) {
       REQUIRE(val == 0);
@@ -154,7 +41,7 @@ TEST_CASE("we can perform a bucket accumulation pass 2") {
       scalar
     };
     E generators[] = {7};
-    auto fut = accumulate_buckets2<E>(bucket_sums, generators, scalars);
+    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
@@ -176,7 +63,7 @@ TEST_CASE("we can perform a bucket accumulation pass 2") {
       scalar_data
     };
     E generators[] = {7, 5};
-    auto fut = accumulate_buckets2<E>(bucket_sums, generators, scalars);
+    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
@@ -200,7 +87,7 @@ TEST_CASE("we can perform a bucket accumulation pass 2") {
       scalar_data
     };
     E generators[] = {7, 5};
-    auto fut = accumulate_buckets2<E>(bucket_sums, generators, scalars);
+    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
@@ -225,7 +112,7 @@ TEST_CASE("we can perform a bucket accumulation pass 2") {
       scalar_data2,
     };
     E generators[] = {7};
-    auto fut = accumulate_buckets2<E>(bucket_sums, generators, scalars);
+    auto fut = accumulate_buckets<E>(bucket_sums, generators, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
