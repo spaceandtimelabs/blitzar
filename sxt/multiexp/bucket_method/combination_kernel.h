5a24f1ad18160952c853f8cdb2fee8b3e2fe0183
Ryan Burn <ryan.burn@gmail.com>
2023-10-04 15:13:01 -0700

    refactor

--- sxt/multiexp/bucket_method/combination_kernel.h
+++ sxt/multiexp/bucket_method/combination_kernel.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "sxt/algorithm/base/reducer.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/num/divide_up.h"
 
@@ -8,33 +7,6 @@ namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
 // combine_partial_bucket_sums
 //--------------------------------------------------------------------------------------------------
-template <algb::reducer Reducer>
-__global__ void combine_partial_bucket_sums(typename Reducer::value_type* out,
-                                            typename Reducer::value_type* partial_bucket_sums,
-                                            unsigned num_partials) {
-  using T = typename Reducer::value_type;
-  auto bucket_group_size = gridDim.x;
-  auto bucket_group_index = threadIdx.x;
-  auto num_bucket_groups = blockDim.x;
-  auto bucket_index = blockIdx.x;
-  auto output_index = blockIdx.y;
-
-  partial_bucket_sums += bucket_index + bucket_group_size * bucket_group_index +
-                         bucket_group_size * num_bucket_groups * num_partials * output_index;
-
-  out += bucket_index + bucket_group_size * bucket_group_index +
-         bucket_group_size * num_bucket_groups * output_index;
-
-  T sum = *partial_bucket_sums;
-  partial_bucket_sums += bucket_group_size * num_bucket_groups;
-  for (unsigned i = 1; i < num_partials; ++i) {
-    Reducer::accumulate_inplace(sum, *partial_bucket_sums);
-    partial_bucket_sums += bucket_group_size * num_bucket_groups;
-  }
-
-  *out = sum;
-}
-
 template <bascrv::element T>
 __global__ void combine_partial_bucket_sums(T* out, T* partial_bucket_sums, unsigned num_partials) {
   auto bucket_group_size = gridDim.x;
--- sxt/multiexp/bucket_method/combination_kernel.t.cc
+++ sxt/multiexp/bucket_method/combination_kernel.t.cc
@@ -1,6 +1,6 @@
 #include "sxt/multiexp/bucket_method/combination_kernel.h"
 
-#include "sxt/algorithm/reduction/test_reducer.h"
+#include "sxt/base/curve/example_element.h"
 #include "sxt/base/device/synchronization.h"
 #include "sxt/base/test/unit_test.h"
 #include "sxt/memory/management/managed_array.h"
@@ -9,44 +9,45 @@ using namespace sxt;
 using namespace sxt::mtxbk;
 
 TEST_CASE("we can combine partial bucket sums") {
-  memmg::managed_array<uint64_t> partial_bucket_sums(memr::get_managed_device_resource());
-  memmg::managed_array<uint64_t> bucket_sums(memr::get_managed_device_resource());
+  memmg::managed_array<bascrv::element97> partial_bucket_sums(memr::get_managed_device_resource());
+  memmg::managed_array<bascrv::element97> bucket_sums(memr::get_managed_device_resource());
 
   SECTION("we handle the case of a single bucket") {
-    partial_bucket_sums = {123};
-    bucket_sums = {0};
-    combine_partial_bucket_sums<algr::test_add_reducer>
-        <<<1, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 1);
+    partial_bucket_sums = {12u};
+    bucket_sums = {0u};
+    combine_partial_bucket_sums<<<1, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 1);
     basdv::synchronize_device();
-    REQUIRE(bucket_sums[0] == 123);
+    REQUIRE(bucket_sums[0] == 12u);
   }
 
   SECTION("we handle a bucket with two sums") {
-    partial_bucket_sums = {123, 456};
-    bucket_sums = {0};
-    combine_partial_bucket_sums<algr::test_add_reducer>
-        <<<1, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
+    partial_bucket_sums = {12u, 45u};
+    bucket_sums = {0u};
+    combine_partial_bucket_sums<<<1, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
     basdv::synchronize_device();
-    REQUIRE(bucket_sums[0] == 579);
+    REQUIRE(bucket_sums[0] == 57u);
   }
 
   SECTION("we handle a bucket group size > 1") {
-    partial_bucket_sums = {123, 456, 7, 3};
+    partial_bucket_sums = {12u, 45u, 7u, 3u};
     bucket_sums = {0, 0};
-    combine_partial_bucket_sums<algr::test_add_reducer>
-        <<<1, 2>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
+    combine_partial_bucket_sums<<<1, 2>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
     basdv::synchronize_device();
-    REQUIRE(bucket_sums[0] == 130);
-    REQUIRE(bucket_sums[1] == 459);
+    REQUIRE(bucket_sums[0] == 19u);
+    REQUIRE(bucket_sums[1] == 48u);
   }
 
   SECTION("we handle more than one bucket group") {
-    partial_bucket_sums = {123, 456, 7, 3};
+    partial_bucket_sums = {12u, 45u, 7u, 3u};
     bucket_sums = {0, 0};
-    combine_partial_bucket_sums<algr::test_add_reducer>
-        <<<2, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
+    combine_partial_bucket_sums<<<2, 1>>>(bucket_sums.data(), partial_bucket_sums.data(), 2);
     basdv::synchronize_device();
-    REQUIRE(bucket_sums[0] == 130);
-    REQUIRE(bucket_sums[1] == 459);
+    REQUIRE(bucket_sums[0] == 19u);
+    REQUIRE(bucket_sums[1] == 48u);
   }
 }
+
+TEST_CASE("we can reduce bucket groups") {
+}
+/* template <bascrv::element T, unsigned BucketGroupSize, unsigned NumBucketGroups> */
+/* __global__ void combine_bucket_groups(T* out, T* bucket_sums) { */
