5296466ca7ce4d1a1838969aeee54d3c9e494b96
Ryan Burn <ryan.burn@gmail.com>
2023-10-09 10:27:04 -0700

    integrate bucket method

--- sxt/multiexp/bucket_method/multiexponentiation.h
+++ sxt/multiexp/bucket_method/multiexponentiation.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <limits>
+
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/device/memory_utility.h"
@@ -12,6 +15,7 @@
 #include "sxt/memory/management/managed_array.h"
 #include "sxt/memory/resource/async_device_resource.h"
 #include "sxt/memory/resource/pinned_resource.h"
+#include "sxt/multiexp/base/exponent_sequence.h"
 #include "sxt/multiexp/bucket_method/bucket_accumulation.h"
 #include "sxt/multiexp/bucket_method/bucket_combination.h"
 #include "sxt/multiexp/bucket_method/combination_kernel.h"
@@ -54,5 +58,43 @@ xena::future<> multiexponentiate(basct::span<T> res, basct::cspan<T> generators,
 
   // combine buckets
   combine_buckets<T>(res, reduced_buckets);
+}
+
+//--------------------------------------------------------------------------------------------------
+// try_multiexponentiate 
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element Element>
+xena::future<memmg::managed_array<Element>>
+try_multiexponentiate(basct::cspan<Element> generators,
+                      basct::cspan<mtxb::exponent_sequence> exponents) noexcept {
+  auto num_outputs = exponents.size();
+  memmg::managed_array<Element> res;
+  uint64_t min_n = std::numeric_limits<uint64_t>::max();
+  uint64_t max_n = 0;
+  for (auto& exponent : exponents) {
+    if (exponent.element_nbytes != 32) {
+      co_return res;
+    }
+    min_n = std::min(min_n, exponent.n);
+    max_n = std::min(max_n, exponent.n);
+  }
+  if (min_n != max_n) {
+      co_return res;
+  }
+  auto n = max_n;
+  /* if (n < 10000) { */
+  /*     co_return res; */
+  /* } */
+  SXT_DEBUG_ASSERT(
+      generators.size() >= n
+  );
+  generators = generators.subspan(0, n);
+  memmg::managed_array<const uint8_t*> exponents_p(exponents.size());
+  for(size_t output_index=0; output_index<num_outputs; ++output_index) {
+    exponents_p[output_index] = exponents[output_index].data;
+  }
+  res.resize(num_outputs);
+  co_await multiexponentiate(res, generators, exponents_p);
+  co_return res;
 }
 } // namespace sxt::mtxbk
