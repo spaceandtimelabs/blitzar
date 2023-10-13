13041d69f909d86f3bb97fca51826418fb18ddcd
Ryan Burn <ryan.burn@gmail.com>
2023-10-12 17:18:08 -0700

    support chunking

--- sxt/base/iterator/index_range_utility.cc
+++ sxt/base/iterator/index_range_utility.cc
@@ -32,6 +32,8 @@ std::pair<index_range_iterator, index_range_iterator> split(const index_range& r
   SXT_DEBUG_ASSERT(n > 0);
   auto delta = rng.b() - rng.a();
   auto step = std::max(basn::divide_up(delta, n), size_t{1});
+  step = std::max(step, rng.min_chunk_size());
+  step = std::min(step, rng.max_chunk_size());
   index_range_iterator first{index_range{rng.a(), rng.b()}, step};
   index_range_iterator last{index_range{rng.b(), rng.b()}, step};
   return {first, last};
