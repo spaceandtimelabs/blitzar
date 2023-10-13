4b9d113b84a14007595af748d2b971a96f15011e
Ryan Burn <ryan.burn@gmail.com>
2023-10-12 17:03:45 -0700

    support chunking

--- sxt/base/iterator/index_range.cc
+++ sxt/base/iterator/index_range.cc
@@ -22,5 +22,40 @@ namespace sxt::basit {
 //--------------------------------------------------------------------------------------------------
 // constructor
 //--------------------------------------------------------------------------------------------------
-index_range::index_range(size_t a, size_t b) noexcept : a_{a}, b_{b} { SXT_DEBUG_ASSERT(a <= b); }
+index_range::index_range(size_t a, size_t b) noexcept
+    : index_range{a, b, 1, std::numeric_limits<size_t>::max()} {}
+
+index_range::index_range(size_t a, size_t b, size_t min_chunk_size, size_t max_chunk_size) noexcept
+    : a_{a}, b_{b}, min_chunk_size_{min_chunk_size}, max_chunk_size_{max_chunk_size} {
+  SXT_DEBUG_ASSERT(
+      // clang-format off
+      0 <= a && a <= b &&
+      0 < min_chunk_size_ && min_chunk_size_ <= max_chunk_size_
+      // clang-format on
+  );
+}
+
+//--------------------------------------------------------------------------------------------------
+// min_chunk_size
+//--------------------------------------------------------------------------------------------------
+index_range index_range::min_chunk_size(size_t val) const noexcept {
+  return {
+      a_,
+      b_,
+      val,
+      max_chunk_size_,
+  };
+}
+
+//--------------------------------------------------------------------------------------------------
+// max_chunk_size
+//--------------------------------------------------------------------------------------------------
+index_range index_range::max_chunk_size(size_t val) const noexcept {
+  return {
+      a_,
+      b_,
+      min_chunk_size_,
+      val,
+  };
+}
 } // namespace sxt::basit
