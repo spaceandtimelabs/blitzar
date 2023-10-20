df78077f2152307cebb494fa2dbe9f7203203a2c
rnburn <ryan.burn@gmail.com>
2023-10-20 05:24:35 +0000

    work on shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
-(pkgs.buildFHSEnv {
+(pkgs.buildFHSEnvChroot {
   name = "simple-env";
   targetPkgs = pkgs: [
     pkgs.python3
+    pkgs.glibc
+    pkgs.gcc13.libc
+    pkgs.gcc13
+    pkgs.clang
+    pkgs.bazel_6
   ];
   runScript = "bash";
 }).env
