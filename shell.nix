1f2aab70f1fa00d2d95cee0b2926060086eb06d3
rnburn <ryan.burn@gmail.com>
2023-10-20 17:41:58 +0000

    work on flake

--- shell.nix
+++ shell.nix
@@ -1,10 +1,19 @@
 { pkgs ? import <nixpkgs> { system = "x86_64-linux"; } }:
-(pkgs.buildFHSEnvChroot {
-  name = "simple-env";
-  targetPkgs = pkgs: [
+with pkgs;
+mkShell {
+  buildInputs = [
     pkgs.python3
-    pkgs.gcc13.libc
     pkgs.bazel_6
   ];
-  runScript = "bash";
-}).env
+  shellHook = ''
+  '';
+}
+# (pkgs.buildFHSEnvChroot {
+#   name = "simple-env";
+#   targetPkgs = pkgs: [
+#     pkgs.python3
+#     pkgs.gcc13.libc
+#     pkgs.bazel_6
+#   ];
+#   runScript = "bash";
+# }).env
