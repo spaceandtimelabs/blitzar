e1883063c8cfd05c1cad6deb328809a8fe18b259
rnburn <ryan.burn@gmail.com>
2023-11-06 09:27:26 -0800

    add min-max chunking

--- sxt/multiexp/bucket_method/accumulation.h
+++ sxt/multiexp/bucket_method/accumulation.h
@@ -28,6 +28,7 @@
 #include "sxt/base/iterator/index_range.h"
 #include "sxt/base/iterator/index_range_iterator.h"
 #include "sxt/base/iterator/index_range_utility.h"
+#include "sxt/base/num/divide_up.h"
 #include "sxt/execution/async/coroutine.h"
 #include "sxt/execution/async/future.h"
 #include "sxt/execution/device/device_viewable.h"
@@ -60,23 +61,21 @@ xena::future<> accumulate_buckets_impl(basct::span<T> bucket_sums, basct::cspan<
   static constexpr int num_bytes = 32; // hard code to 32 for now
 
   basdv::stream stream;
-
-  // partial_bucket_sums
   memr::async_device_resource resource{stream};
-  memmg::managed_array<T> partial_bucket_sums{bucket_sums.size() * num_blocks, &resource};
 
-  // generators_viewable
+  // make generators accessible to the active device
   memmg::managed_array<T> generators_viewable_data{&resource};
   auto generators_viewable = xendv::make_active_device_viewable(
       generators_viewable_data, generators.subspan(rng.a(), rng.size()));
 
-  // exponents_viewable
+  // make exponents accessible to the active device
   memmg::managed_array<uint8_t> exponents_viewable_data{&resource};
   auto exponents_viewable =
       make_exponents_viewable(exponents_viewable_data, exponents, rng, stream);
 
-  // partial bucket accumulation kernel
+  // accumulate generators into buckets of partial sums
   xendv::synchronize_event(stream, generators_viewable);
+  memmg::managed_array<T> partial_bucket_sums{bucket_sums.size() * num_blocks, &resource};
   bucket_accumulate<<<dim3(num_blocks, num_outputs, 1), num_bytes, 0, stream>>>(
       partial_bucket_sums.data(), generators_viewable.value().data(), exponents_viewable.data(), n);
   generators_viewable_data.reset();
@@ -105,7 +104,26 @@ xena::future<> accumulate_buckets_impl(basct::span<T> bucket_sums, basct::cspan<
       (bucket_sums.empty() || basdv::is_active_device_pointer(bucket_sums.data()))
       // clang-format on
   );
-  auto [first, last] = basit::split(basit::index_range{0, generators.size()}, split_factor);
+
+  // Pick some reasonable values for min and max chunk size so that
+  // we don't run out of GPU memory or split computations that are
+  // too small.
+  //
+  // Note: These haven't been informed by much benchmarking. I'm
+  // sure there are better values. This is just putting in some
+  // ballpark estimates to get started.
+  size_t min_chunk_size = 1ull << 10u;
+  size_t max_chunk_size = 1ull << 20u;
+  if (num_outputs > 0) {
+    max_chunk_size = basn::divide_up(max_chunk_size, num_outputs);
+    min_chunk_size *= num_outputs;
+    min_chunk_size = std::min(max_chunk_size, min_chunk_size);
+  }
+
+  auto [first, last] = basit::split(basit::index_range{0, generators.size()}
+                                        .min_chunk_size(min_chunk_size)
+                                        .max_chunk_size(max_chunk_size),
+                                    split_factor);
   auto num_chunks = std::distance(first, last);
 
   basdv::stream stream;
