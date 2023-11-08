a34bc3f38579ae80799334f6c70eb393069f1c32
Ryan Burn <ryan.burn@gmail.com>
2023-11-08 14:12:57 -0800

    feat: bucket method (PROOF-642) (#43)
    * work on bucket method
    
    * fill in bucket method
    
    * bucket
    
    * rework reducer concept
    
    * fix kernel
    
    * fix tests
    
    * fill in bucket method
    
    * work on bucket accumulation tests
    
    * fill in accumulate kernel
    
    * fill in bucket tests
    
    * fill in combination kernel
    
    * fill in combination kernel
    
    * fill in combination kernel
    
    * refactor
    
    * fill in combination kernel
    
    * test combination kernel
    
    * rework combination kernel
    
    * fill in testing
    
    * work on combination kernel
    
    * add stub for bucket accumulation
    
    * fill in stub for bucket accumulation
    
    * fill bucket accumulation
    
    * fill in bucket accumulation
    
    * fill in bucket algorithm
    
    * rework bucket code
    
    * rework accumulation kernel
    
    * fill in bucket method
    
    * fill in bucket method
    
    * fill in bucket method
    
    * fill in bucket method
    
    * fill in bucket method
    
    * fill in bucket method
    
    * fill in bucket accumulation
    
    * fill in bucket method
    
    * work on bucket accumulation
    
    * workaround for nvlink
    
    * fill in bucket testing
    
    * fill in bucket accumulation testing
    
    * fix combination step
    
    * fill in bucket testing
    
    * fill in bucket method
    
    * refactor
    
    * rework bucket accumulation
    
    * rework
    
    * rework bucket accumulation
    
    * rework bucket method
    
    * fill in testing
    
    * fill in bucket method
    
    * fill in bucket method
    
    * refactor
    
    * fill in bucket group reduction
    
    * fill out tests
    
    * fill in testing
    
    * fill in combination kernel testing
    
    * work on bucket combination
    
    * fill in bucket combination
    
    * fill in bucket combination
    
    * fill in bucket combination
    
    * fill in multiexp
    
    * fill in bucket testing
    
    * fill in bucket testing
    
    * fill in bucket testing
    
    * integrate bucket method
    
    * integrate bucket method
    
    * test
    
    * debug bucket method
    
    * fix bug
    
    * fix doubling bug
    
    * get tests to pass with bucket method
    
    * support chunking
    
    * support chunking
    
    * add chunking
    
    * chunking
    
    * add chunk tests
    
    * document
    
    * up
    
    * drill down on memory bug
    
    * clean up
    
    * clean up
    
    * clean up
    
    * time
    
    * undo
    
    * fix up
    
    * fix bug
    
    * add output chunking
    
    * refactor
    
    * reformat
    
    * document
    
    * rework combination kernel
    
    * reformat
    
    * undo
    
    * undo
    
    * undo
    
    * rename
    
    * rename
    
    * clean up
    
    * rework tests
    
    * add min-max chunking
    
    * comment
    
    * add test case
    
    * comment
    
    * reformat
    
    * undo format change
    
    * tweak path
    
    * add test case
    
    * fill in tests
    
    * fill in tests
    
    * comment

---
+++ sxt/multiexp/bucket_method/multiexponentiation.h
@@ -0,0 +1,134 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <algorithm>
+#include <limits>
+
+#include "sxt/base/container/span.h"
+#include "sxt/base/container/span_utility.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/base/device/memory_utility.h"
+#include "sxt/base/device/stream.h"
+#include "sxt/base/error/assert.h"
+#include "sxt/base/iterator/index_range.h"
+#include "sxt/base/iterator/index_range_utility.h"
+#include "sxt/base/num/divide_up.h"
+#include "sxt/execution/async/coroutine.h"
+#include "sxt/execution/async/future.h"
+#include "sxt/execution/device/synchronization.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/async_device_resource.h"
+#include "sxt/memory/resource/pinned_resource.h"
+#include "sxt/multiexp/base/exponent_sequence.h"
+#include "sxt/multiexp/bucket_method/accumulation.h"
+#include "sxt/multiexp/bucket_method/combination.h"
+
+namespace sxt::mtxbk {
+//--------------------------------------------------------------------------------------------------
+// multiexponentiate
+//--------------------------------------------------------------------------------------------------
+/**
+ * Compute a multi-exponentiation using a simple version of the bucket method based off of Algorithm
+ * 1 in
+ *
+ *    PipeMSM: Hardware Acceleration for Multi-Scalar Multiplication
+ *    https://eprint.iacr.org/2022/999.pdf
+ *
+ * For now, the bucket size and other parameters are fixed. It's not expected that this version
+ * of the algorithm will give the best performance, but it should give an improvement over naive
+ * versions of multi-exponentiation.
+ */
+template <bascrv::element T>
+xena::future<> multiexponentiate(basct::span<T> res, basct::cspan<T> generators,
+                                 basct::cspan<const uint8_t*> exponents) noexcept {
+  constexpr unsigned bucket_group_size = 255;
+  constexpr unsigned num_bucket_groups = 32;
+  auto num_outputs = exponents.size();
+  SXT_DEBUG_ASSERT(res.size() == num_outputs);
+  if (res.empty()) {
+    co_return;
+  }
+
+  basdv::stream stream;
+
+  // accumulate
+  memr::async_device_resource resource{stream};
+  memmg::managed_array<T> bucket_sums{bucket_group_size * num_bucket_groups * num_outputs,
+                                      &resource};
+  co_await accumulate_buckets<T>(bucket_sums, generators, exponents);
+
+  // reduce buckets
+  memmg::managed_array<T> reduced_buckets_dev{bucket_group_size * num_outputs, &resource};
+  static unsigned num_threads = 32;
+  dim3 block_dims(basn::divide_up(bucket_group_size, num_threads), num_outputs, 1);
+  combine_bucket_groups<bucket_group_size, num_bucket_groups>
+      <<<block_dims, num_threads, 0, stream>>>(reduced_buckets_dev.data(), bucket_sums.data());
+  memmg::managed_array<T> reduced_buckets{reduced_buckets_dev.size(), memr::get_pinned_resource()};
+  basdv::async_copy_device_to_host(reduced_buckets, reduced_buckets_dev, stream);
+  co_await xendv::await_stream(stream);
+  reduced_buckets_dev.reset();
+
+  // combine buckets
+  combine_buckets<T>(res, reduced_buckets);
+}
+
+//--------------------------------------------------------------------------------------------------
+// try_multiexponentiate
+//--------------------------------------------------------------------------------------------------
+/**
+ * Attempt to compute a multi-exponentiation using the bucket method if the problem dimensions
+ * suggest it will give a performance benefit; otherwise, return an empty array.
+ */
+template <bascrv::element Element>
+xena::future<memmg::managed_array<Element>>
+try_multiexponentiate(basct::cspan<Element> generators,
+                      basct::cspan<mtxb::exponent_sequence> exponents,
+                      size_t min_length = 10000) noexcept {
+  auto num_outputs = exponents.size();
+  memmg::managed_array<Element> res;
+  uint64_t min_n = std::numeric_limits<uint64_t>::max();
+  uint64_t max_n = 0;
+  for (auto& exponent : exponents) {
+    if (exponent.element_nbytes != 32) {
+      co_return res;
+    }
+    min_n = std::min(min_n, exponent.n);
+    max_n = std::max(max_n, exponent.n);
+  }
+  if (min_n != max_n || min_n < min_length) {
+    co_return res;
+  }
+  auto n = max_n;
+  SXT_DEBUG_ASSERT(generators.size() >= n);
+  generators = generators.subspan(0, n);
+  memmg::managed_array<const uint8_t*> exponents_p(num_outputs);
+  for (size_t output_index = 0; output_index < num_outputs; ++output_index) {
+    exponents_p[output_index] = exponents[output_index].data;
+  }
+  res.resize(num_outputs);
+  static constexpr size_t max_output_chunk = 8;
+  auto output_chunks =
+      basit::split(basit::index_range{0, num_outputs}.max_chunk_size(max_output_chunk), 1);
+  for (auto chunk_iter = output_chunks.first; chunk_iter != output_chunks.second; ++chunk_iter) {
+    auto chunk = *chunk_iter;
+    co_await multiexponentiate<Element>(basct::subspan(res, chunk.a(), chunk.size()), generators,
+                                        basct::subspan(exponents_p, chunk.a(), chunk.size()));
+  }
+  co_return res;
+}
+} // namespace sxt::mtxbk
---
+++ sxt/multiexp/bucket_method/multiexponentiation.t.cc
@@ -0,0 +1,201 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "sxt/multiexp/bucket_method/multiexponentiation.h"
+
+#include "sxt/base/curve/example_element.h"
+#include "sxt/base/device/synchronization.h"
+#include "sxt/base/test/unit_test.h"
+#include "sxt/curve21/operation/add.h"
+#include "sxt/curve21/operation/double.h"
+#include "sxt/curve21/operation/neg.h"
+#include "sxt/curve21/type/element_p3.h"
+#include "sxt/curve21/type/literal.h"
+#include "sxt/execution/schedule/scheduler.h"
+#include "sxt/field51/type/literal.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/managed_device_resource.h"
+
+using namespace sxt;
+using namespace sxt::mtxbk;
+using c21t::operator""_c21;
+using f51t::operator""_f51;
+
+TEST_CASE("we can compute a multiexponentiation") {
+  std::vector<bascrv::element97> res(1);
+  std::vector<bascrv::element97> generators;
+  std::vector<const uint8_t*> exponents;
+
+  SECTION("we can compute a multiexponentiation with no elements") {
+    res.clear();
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    REQUIRE(fut.ready());
+  }
+
+  SECTION("we can compute a multiexponentiation with a single zero element") {
+    uint8_t scalar_data[32] = {};
+    exponents.push_back(scalar_data);
+    generators = {12u};
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 0u);
+  }
+
+  SECTION("we can compute a multiexponentiation with a single element of 1") {
+    uint8_t scalar_data[32] = {};
+    scalar_data[0] = 1;
+    exponents.push_back(scalar_data);
+    generators = {12u};
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 12u);
+  }
+
+  SECTION("we can compute a multiexponentiation with a single element of 2") {
+    uint8_t scalar_data[32] = {};
+    scalar_data[0] = 2;
+    exponents.push_back(scalar_data);
+    generators = {12u};
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 24u);
+  }
+
+  SECTION("we can compute a multiexponentiation with a single element of 256") {
+    uint8_t scalar_data[32] = {};
+    scalar_data[1] = 1;
+    exponents.push_back(scalar_data);
+    generators = {12u};
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 256u * 12u);
+  }
+
+  SECTION("we can compute a multiexponentiation with 2 generators") {
+    uint8_t scalar_data[64] = {};
+    scalar_data[0] = 2;
+    scalar_data[32] = 3;
+    exponents.push_back(scalar_data);
+
+    generators = {12u, 34u};
+
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 2u * 12u + 3u * 34u);
+  }
+
+  SECTION("we can compute a multiexponetiation with multiple outputs") {
+    res.resize(2);
+
+    uint8_t scalar_data1[64] = {};
+    scalar_data1[0] = 2;
+    scalar_data1[32] = 3;
+    exponents.push_back(scalar_data1);
+
+    uint8_t scalar_data2[64] = {};
+    scalar_data2[0] = 7;
+    scalar_data2[32] = 4;
+    exponents.push_back(scalar_data2);
+
+    generators = {12u, 34u};
+
+    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 2u * 12u + 3u * 34u);
+    REQUIRE(res[1] == 7u * 12u + 4u * 34u);
+  }
+}
+
+TEST_CASE("we can compute multiexponentiations with curve-21") {
+  std::vector<c21t::element_p3> res(1);
+  std::vector<c21t::element_p3> generators;
+  std::vector<const uint8_t*> exponents;
+
+  SECTION("we can compute a multiexponentiation with a single element of 1") {
+    uint8_t scalar_data[32] = {};
+    scalar_data[0] = 1;
+    exponents.push_back(scalar_data);
+    generators = {0x123_c21};
+    auto fut = multiexponentiate<c21t::element_p3>(res, generators, exponents);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == 0x123_c21);
+  }
+}
+
+TEST_CASE("we can compute multiexponentiations with exponent sequences") {
+  std::vector<mtxb::exponent_sequence> exponents;
+  size_t min_length = 0;
+
+  SECTION("exponentiation fails if the exponent sequences are not 32 bytes") {
+    exponents.push_back({
+        .element_nbytes = 1,
+        .n = 1,
+    });
+    std::vector<bascrv::element97> generators = {2u};
+    auto fut = try_multiexponentiate<bascrv::element97>(generators, exponents, min_length);
+    REQUIRE(fut.ready());
+    REQUIRE(fut.value().empty());
+  }
+
+  SECTION("exponentiation fails if the exponent sequences are different lengths") {
+    exponents.push_back({
+        .element_nbytes = 32,
+        .n = 1,
+    });
+    exponents.push_back({
+        .element_nbytes = 32,
+        .n = 2,
+    });
+    std::vector<bascrv::element97> generators = {2u, 5u};
+    auto fut = try_multiexponentiate<bascrv::element97>(generators, exponents, min_length);
+    REQUIRE(fut.ready());
+    REQUIRE(fut.value().empty());
+  }
+
+  SECTION("exponentiation fails if the exponent sequences are too small") {
+    exponents.push_back({
+        .element_nbytes = 32,
+        .n = 1,
+    });
+    std::vector<bascrv::element97> generators = {2u};
+    auto fut = try_multiexponentiate<bascrv::element97>(generators, exponents, 2);
+    REQUIRE(fut.ready());
+    REQUIRE(fut.value().empty());
+  }
+
+  SECTION("exponentiation succeeds if preconditions are met") {
+    uint8_t scalar_data[32] = {};
+    scalar_data[0] = 12;
+    exponents.push_back({
+        .element_nbytes = 32,
+        .n = 1,
+        .data = scalar_data,
+    });
+    std::vector<bascrv::element97> generators = {2u};
+    auto fut = try_multiexponentiate<bascrv::element97>(generators, exponents, min_length);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(fut.value().size() == 1);
+    REQUIRE(fut.value()[0] == 2u * 12u);
+  }
+}
