32086665d86ed70f74ec59c1dde05478b7ee9b98
rnburn <ryan.burn@gmail.com>
2023-11-13 10:01:35 -0800

    drop dead code

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,7 +1,6 @@
 { pkgs }:
 let
   clang = import ./clang.nix { inherit pkgs; };
-  compiler-rt = import ./compiler_rt.nix { inherit pkgs; inherit clang; };
   cuda = import ./cuda.nix { inherit pkgs; };
   bazel = import ./bazel.nix { inherit pkgs; inherit clang; inherit cuda; };
 in
