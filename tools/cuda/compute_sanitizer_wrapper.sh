483592c22b70b30afa7e1ddc75235169532c8b8e
rnburn <ryan.burn@gmail.com>
2023-11-17 12:08:10 -0800

    fix command

--- tools/cuda/compute_sanitizer_wrapper.sh
+++ tools/cuda/compute_sanitizer_wrapper.sh
@@ -5,6 +5,9 @@ compute-sanitizer --log-file $outfile $1
 rc=$?
 output=$(<$outfile)
 rm $outfile
+
+# compute-sanitizer will fail if a test doesn't launch any kernels which we don't
+# want since we run it against all of our tests.
 if [[ $output =~ "Target application terminated before first instrumented API call" ]]; then
   exit 0
 fi
