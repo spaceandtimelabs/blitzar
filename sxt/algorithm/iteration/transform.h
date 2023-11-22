758c6cbe261827c3b3a9f4229f3b8791d8306123
rnburn <ryan.burn@gmail.com>
2023-11-22 10:37:20 -0800

    fill in transform

--- sxt/algorithm/iteration/transform.h
+++ sxt/algorithm/iteration/transform.h
@@ -2,9 +2,14 @@
 
 #include "sxt/algorithm/base/transform_functor.h"
 #include "sxt/base/container/span.h"
+#include "sxt/base/error/assert.h"
 #include "sxt/base/iterator/chunk_options.h"
+#include "sxt/base/iterator/index_range.h"
+#include "sxt/base/iterator/index_range_iterator.h"
+#include "sxt/base/iterator/index_range_utility.h"
 #include "sxt/base/type/value_type.h"
 #include "sxt/execution/async/future.h"
+#include "sxt/execution/device/for_each.h"
 
 namespace sxt::algi {
 //--------------------------------------------------------------------------------------------------
@@ -15,6 +20,16 @@ template <class F, class Arg1, class... ArgsRest>
 xena::future<> transform(basct::span<bast::value_type<Arg1>> res, F f,
                          basit::chunk_options chunk_options, const Arg1& x1,
                          const ArgsRest&... xrest) noexcept {
+  auto n = res.size();
+  SXT_DEBUG_ASSERT(
+      x1.size() == n && ((xrest.size() == n) && ...)
+  );
+  auto [first, last] = basit::split(basit::index_range{0, n}
+                                        .min_chunk_size(chunk_options.min_size)
+                                        .max_chunk_size(chunk_options.max_size),
+                                    chunk_options.split_factor);
+  xendv::concurrent_for_each(
+      first, last, [&](const basit::index_range& rng) noexcept -> xena::future<> { (void)rng; });
   return {};
 }
 } // namespace sxt::algi
--- sxt/base/iterator/chunk_options.h
+++ sxt/base/iterator/chunk_options.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <cstddef>
+#include <limits>
+
+#include "sxt/base/device/property.h"
 
 namespace sxt::basit {
 //--------------------------------------------------------------------------------------------------
 // chunk_options
 //--------------------------------------------------------------------------------------------------
 struct chunk_options {
-  size_t min_size;
-  size_t max_size;
+  size_t min_size = 1;
+  size_t max_size = std::numeric_limits<size_t>::max();
+  size_t split_factor = basdv::get_num_devices();
 };
 } // namespace sxt::basit
