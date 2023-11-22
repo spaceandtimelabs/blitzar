bc7db631474811bb04ca6f61485ff2ad844a66cf
rnburn <ryan.burn@gmail.com>
2023-11-22 09:27:37 -0800

    add chunk options

---
+++ sxt/base/iterator/chunk_options.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+
+namespace sxt::basit {
+//--------------------------------------------------------------------------------------------------
+// chunk_options
+//--------------------------------------------------------------------------------------------------
+struct chunk_options {
+  size_t min_size;
+  size_t max_size;
+};
+} // namespace sxt::basit
