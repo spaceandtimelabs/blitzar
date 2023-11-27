718acc845a3e52a3964d0afa8cd4b7a5f845ea53
rnburn <ryan.burn@gmail.com>
2023-11-27 01:45:13 -0800

    fill in chained resource

--- sxt/memory/resource/chained_resource.t.cc
+++ sxt/memory/resource/chained_resource.t.cc
@@ -1,2 +1,8 @@
 #include "sxt/memory/resource/chained_resource.h"
 
+#include "sxt/base/test/unit_test.h"
+using namespace sxt;
+using namespace sxt::memr;
+
+TEST_CASE("we can manage a chain of allocations") {
+}
