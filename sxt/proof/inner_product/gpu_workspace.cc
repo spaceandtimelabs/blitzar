97ddea5edff48c3ddafd396176bdaefa2f9c14e7
rnburn <ryan.burn@gmail.com>
2023-11-29 13:54:24 -0800

    rework inner product

--- sxt/proof/inner_product/gpu_workspace.cc
+++ sxt/proof/inner_product/gpu_workspace.cc
@@ -36,11 +36,16 @@ gpu_workspace::gpu_workspace() noexcept
 // ap_value
 //--------------------------------------------------------------------------------------------------
 xena::future<> gpu_workspace::ap_value(s25t::element& value) const noexcept {
-  SXT_DEBUG_ASSERT(this->a_vector.size() == 1);
-  memmg::managed_array<s25t::element> value_p{1, memr::get_pinned_resource()};
-  basdv::stream stream;
-  basdv::async_copy_device_to_host(value_p, this->a_vector, stream);
-  co_await xendv::await_and_own_stream(std::move(stream));
-  value = value_p[0];
+  if (use_new) {
+    value = this->a_vectorX[0];
+    co_return;
+  } else {
+    SXT_DEBUG_ASSERT(this->a_vector.size() == 1);
+    memmg::managed_array<s25t::element> value_p{1, memr::get_pinned_resource()};
+    basdv::stream stream;
+    basdv::async_copy_device_to_host(value_p, this->a_vector, stream);
+    co_await xendv::await_and_own_stream(std::move(stream));
+    value = value_p[0];
+  }
 }
 } // namespace sxt::prfip
