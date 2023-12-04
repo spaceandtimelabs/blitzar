08309ce0da46429e33b35863476e90d3607c3eaf
rnburn <ryan.burn@gmail.com>
2023-12-04 09:26:47 -0800

    work on docker

--- flake.nix
+++ flake.nix
@@ -35,10 +35,29 @@
               coreutils
               bashInteractive
             ];
-            pathsToLink = ["/bin"];
           };
 
           config = {
+            extraCommands = 
+              # taken from https://github.com/nix-community/docker-nixpkgs/blob/master/images/devcontainer/default.nix
+              # # create the Nix DB
+              # export NIX_REMOTE=local?root=$PWD
+              # export USER=nobody
+              # ${nix}/bin/nix-store --load-db < ${closureInfo { rootPaths = [ profile ]; }}/registration
+
+              # # set the user profile
+              # ${profile}/bin/nix-env --profile nix/var/nix/profiles/default --set ${profile}
+
+              ''
+              # minimal
+              mkdir -p bin /usr/bin
+              ln -s /nix/var/nix/profiles/default/bin/sh bin/sh
+              ln -s /nix/var/nix/profiles/default/bin/sh bin/bash
+              ln -s /nix/var/nix/profiles/default/bin/env usr/bin/env
+
+              # make sure /tmp exists
+              mkdir -m 0777 tmp
+            '';
             Cmd = [
               "/bin/bash"
             ];
