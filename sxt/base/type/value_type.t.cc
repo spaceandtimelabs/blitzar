f52b6f006eb7c8fd1252f92e3356470271ebd9d7
rnburn <ryan.burn@gmail.com>
2023-12-04 09:58:56 -0800

    fill in value_type

--- sxt/base/type/value_type.t.cc
+++ sxt/base/type/value_type.t.cc
@@ -25,5 +25,11 @@ using namespace sxt;
 using namespace sxt::bast;
 
 TEST_CASE("we can get the value type of containers") {
-  REQUIRE(std::is_same_v<value_type_t<std::vector<int>>, int>);
+  SECTION("we handle a simple container") {
+    REQUIRE(std::is_same_v<value_type_t<std::vector<int>>, int>);
+  }
+
+  SECTION("we handle an array") {
+    REQUIRE(std::is_same_v<value_type_t<int[6]>, int>);
+  }
 }
