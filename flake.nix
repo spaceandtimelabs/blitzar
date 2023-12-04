74355a4db46b876e34f1c6c580da2d1fcef81bfa
rnburn <ryan.burn@gmail.com>
2023-12-04 12:11:42 -0800

    work on docker image

--- flake.nix
+++ flake.nix
@@ -27,39 +27,48 @@
         with pkgs;
         pkgs.dockerTools.buildImage {
           name = "blitzar";
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            paths = [
-              bazel
-              binutils
-              coreutils
-              bashInteractive
-            ];
-          };
+          copyToRoot = with pkgs; [
+            bazel
+            binutils
+            coreutils
+            dockerTools.usrBinEnv
+            dockerTools.binSh
+            dockerTools.caCertificates
+            dockerTools.fakeNss
+          ];
+          # pkgs.buildEnv {
+          #   name = "image-root";
+          #   paths = [
+          #     bazel
+          #     binutils
+          #     coreutils
+          #     bashInteractive
+          #   ];
+          # };
 
           config = {
-            extraCommands = 
-              # taken from https://github.com/nix-community/docker-nixpkgs/blob/master/images/devcontainer/default.nix
-              # # create the Nix DB
-              # export NIX_REMOTE=local?root=$PWD
-              # export USER=nobody
-              # ${nix}/bin/nix-store --load-db < ${closureInfo { rootPaths = [ profile ]; }}/registration
+            # extraCommands = 
+            #   # taken from https://github.com/nix-community/docker-nixpkgs/blob/master/images/devcontainer/default.nix
+            #   # # create the Nix DB
+            #   # export NIX_REMOTE=local?root=$PWD
+            #   # export USER=nobody
+            #   # ${nix}/bin/nix-store --load-db < ${closureInfo { rootPaths = [ profile ]; }}/registration
 
-              # # set the user profile
-              # ${profile}/bin/nix-env --profile nix/var/nix/profiles/default --set ${profile}
+            #   # # set the user profile
+            #   # ${profile}/bin/nix-env --profile nix/var/nix/profiles/default --set ${profile}
 
-              ''
-              # minimal
-              mkdir -p bin /usr/bin
-              ln -s /nix/var/nix/profiles/default/bin/sh bin/sh
-              ln -s /nix/var/nix/profiles/default/bin/sh bin/bash
-              ln -s /nix/var/nix/profiles/default/bin/env usr/bin/env
+            #   ''
+            #   # minimal
+            #   mkdir -p bin /usr/bin
+            #   ln -s /nix/var/nix/profiles/default/bin/sh bin/sh
+            #   ln -s /nix/var/nix/profiles/default/bin/sh bin/bash
+            #   ln -s /nix/var/nix/profiles/default/bin/env usr/bin/env
 
-              # make sure /tmp exists
-              mkdir -m 0777 tmp
-            '';
+            #   # make sure /tmp exists
+            #   mkdir -m 0777 tmp
+            # '';
             Cmd = [
-              "/bin/bash"
+              "/bin/sh"
             ];
           };
         };
