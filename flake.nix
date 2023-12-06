f049171f654ae3e715e8015d7fed0227bdf603c3
Ryan Burn <ryan.burn@gmail.com>
2023-12-06 12:42:57 -0800

    ci: partial nix docker support (PROOF-537) (#65)
    * work on docker image
    
    * work on docker flake
    
    * fill in env
    
    * work on docker
    
    * work on docker image
    
    * work on docker
    
    * work on docker image
    
    * work on nvidia support
    
    * comment

--- flake.nix
+++ flake.nix
@@ -13,11 +13,55 @@
         config.allowUnfree = true;
         config.cudaSupport = true;
       };
+      shell = import ./nix/shell.nix { inherit pkgs; };
     in
     {
       formatter.${system} = pkgs.nixpkgs-fmt;
-      devShells.${system}.default = import ./nix/shell.nix {
-        inherit pkgs;
-      };
+      devShells.${system}.default = shell;
+
+      # WIP: Set up a docker image
+      #
+      # To build the image, run
+      #   > nix build .#docker
+      #   > sudo docker load <result
+      # The image can be run with
+      #   > docker 
+      #   > sudo docker run -v /home/rnburn/proj/blitzar:/src -w /src --rm --runtime=nvidia --gpus all -it blitzar:<hash>
+      # TODO(rnburn):
+      #   * Invoking 
+      #        bazel build //sxt/...
+      #     in the container should work but the full build
+      #        bazel build //...
+      #     needs a bit of work to get the benchmarks to build
+      #   * When running
+      #        bazel test //sxt/...
+      #     the GPU tests will fail -- some steps are missing in the docker environment to get the
+      #     GPU drivers to appear
+      packages.${system}.docker = 
+        let
+          clang = import ./nix/clang.nix { inherit pkgs; };
+          cuda = import ./nix/cuda.nix { inherit pkgs; };
+          bazel = import ./nix/bazel.nix { inherit pkgs; inherit clang; inherit cuda; };
+        in
+        with pkgs;
+        pkgs.dockerTools.buildImage {
+          name = "blitzar";
+          copyToRoot = with pkgs; [
+            bazel
+            binutils
+            coreutils
+            dockerTools.usrBinEnv
+            dockerTools.binSh
+            dockerTools.caCertificates
+            dockerTools.fakeNss
+            pkgs.nvidia-docker
+          ];
+
+          config = {
+            Cmd = [
+              "/bin/sh"
+            ];
+          };
+        };
     };
 }
