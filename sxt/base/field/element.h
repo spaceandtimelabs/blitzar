7db90022422c37e2533eb1e0fe9c883d1cb50369
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2023-12-13 09:55:14 -0500

    fix: rebasing issue

--- sxt/base/field/element.h
+++ sxt/base/field/element.h
@@ -20,7 +20,7 @@
 
 namespace sxt::basfld {
 //--------------------------------------------------------------------------------------------------
-// modifiable
+// element
 //--------------------------------------------------------------------------------------------------
 template <class T>
 concept element = requires(T& eref, const T& ecref) {
@@ -29,14 +29,6 @@ concept element = requires(T& eref, const T& ecref) {
   { ecref[0] } noexcept -> std::same_as<const uint64_t&>;
   { eref.data() } noexcept -> std::same_as<uint64_t*>;
   { ecref.data() } noexcept -> std::same_as<const uint64_t*>;
-};
-
-//--------------------------------------------------------------------------------------------------
-// element
-//--------------------------------------------------------------------------------------------------
-template <class T>
-concept element = requires {
-  requires T::num_limbs_v > 0 && std::equality_comparable<T> && modifiable<T>;
   { T::modulus() } noexcept -> std::same_as<T>;
 };
 } // namespace sxt::basfld
