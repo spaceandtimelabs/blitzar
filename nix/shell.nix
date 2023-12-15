b4683e3fa36ed567d6b201220437af9a1ebf47bd
rnburn <ryan.burn@gmail.com>
2023-12-14 21:53:06 -0800

    tweak

--- nix/shell.nix
+++ nix/shell.nix
@@ -10,7 +10,7 @@ mkShell {
     bazel-buildtools
     python3
     llvmPackages.libclang.lib
-    rust-bin.nightly."2023-12-01".default
+    rust-bin.nightly."2023-12-01".complete
     # cargo
     # rust-bindgen
     # rustfmt
@@ -26,6 +26,6 @@ mkShell {
     cudaDrivers
   ];
   shellHook = ''
-    export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
+    # export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
   '';
 }
