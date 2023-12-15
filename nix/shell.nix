9235f939e9840fbd5a8f1db25036b3d2bb0c982f
rnburn <ryan.burn@gmail.com>
2023-12-14 22:18:14 -0800

    tweak

--- nix/shell.nix
+++ nix/shell.nix
@@ -12,7 +12,7 @@ mkShell {
     llvmPackages.libclang.lib
     rust-bin.nightly."2023-12-01".default
     # cargo
-    # rust-bindgen
+    rust-bindgen
     # rustfmt
     patchelf
     nodejs
@@ -27,12 +27,5 @@ mkShell {
   ];
   shellHook = ''
     export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
-    export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
-      $(< ${stdenv.cc}/nix-support/libc-cflags) \
-      $(< ${stdenv.cc}/nix-support/cc-cflags) \
-      $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
-      ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
-      ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
-    "
   '';
 }
