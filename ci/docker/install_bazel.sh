ba13d9d4870217c4d6ad0f29e133fb0777a54e25
rnburn <ryan.burn@gmail.com>
2023-12-20 00:09:19 -0800

    drop cruft

--- ci/docker/install_bazel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-BAZELISK_VERSION=v1.1.0
-
-wget -O /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/$BAZELISK_VERSION/bazelisk-linux-amd64
-chmod +x /usr/local/bin/bazel
