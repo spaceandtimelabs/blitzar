2a1b67af7d20cc397bb954d8d83070bf9216a9f2
Ryan Burn <ryan.burn@gmail.com>
2023-12-20 16:51:13 -0800

    chore: update docs and remove cruft (PROOF-537) (#77)
    * update readme
    
    * drop cruft
    
    * fix build
    
    * fix typo

--- ci/docker/run_docker_cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-1
-
-# If you don't have a GPU instance configured
-docker run --rm -v "$PWD":/src -w /src --privileged -it "$IMAGE" /bin/bash -l
--- ci/docker/run_docker_gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-1
-
-# If you have a GPU instance configured in your machine
-docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE" /bin/bash -l
