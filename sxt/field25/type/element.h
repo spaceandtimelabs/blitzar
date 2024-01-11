9af52abab9696233422c58fe42e041e1e942c5ef
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-01-11 09:35:23 -0500

    feat: add field25 type package (PROOF-775) (#82)
    * copy field12/type package into field25 package
    
    * update include paths and namespace names
    
    * update field25 type package to support four limbs

---
+++ sxt/field25/type/element.h
@@ -0,0 +1,67 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cstdint>
+#include <iosfwd>
+
+#include "sxt/base/macro/cuda_callable.h"
+
+namespace sxt::f25t {
+//--------------------------------------------------------------------------------------------------
+// element
+//--------------------------------------------------------------------------------------------------
+class element {
+public:
+  static constexpr size_t num_limbs_v = 4;
+
+  element() noexcept = default;
+
+  CUDA_CALLABLE constexpr element(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4) noexcept
+      : data_{x1, x2, x3, x4} {}
+
+  CUDA_CALLABLE constexpr element(const uint64_t x[4]) noexcept : data_{x[0], x[1], x[2], x[3]} {}
+
+  CUDA_CALLABLE constexpr const uint64_t& operator[](int index) const noexcept {
+    return data_[index];
+  }
+
+  CUDA_CALLABLE constexpr uint64_t& operator[](int index) noexcept { return data_[index]; }
+
+  CUDA_CALLABLE constexpr const uint64_t* data() const noexcept { return data_; }
+
+  CUDA_CALLABLE constexpr uint64_t* data() noexcept { return data_; }
+
+private:
+  uint64_t data_[num_limbs_v];
+};
+
+//--------------------------------------------------------------------------------------------------
+// operator<<
+//--------------------------------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& out, const element& e) noexcept;
+
+//--------------------------------------------------------------------------------------------------
+// operator==
+//--------------------------------------------------------------------------------------------------
+CUDA_CALLABLE bool operator==(const element& lhs, const element& rhs) noexcept;
+
+//--------------------------------------------------------------------------------------------------
+// operator!=
+//--------------------------------------------------------------------------------------------------
+inline bool operator!=(const element& lhs, const element& rhs) noexcept { return !(lhs == rhs); }
+} // namespace sxt::f25t
---
+++ sxt/field25/type/literal.t.cc
@@ -0,0 +1,62 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "sxt/field25/type/literal.h"
+
+#include <sstream>
+
+#include "sxt/base/test/unit_test.h"
+
+using namespace sxt::f25t;
+
+TEST_CASE("literal element printing") {
+  std::ostringstream oss;
+
+  SECTION("of zero prints 0x0_f25") {
+    oss << 0x0_f25;
+    REQUIRE(oss.str() == "0x0_f25");
+  }
+
+  SECTION("of one prints 0x1_f25") {
+    oss << 0x1_f25;
+    REQUIRE(oss.str() == "0x1_f25");
+  }
+
+  SECTION("of 10 prints 0xa_f25") {
+    oss << 0xa_f25;
+    REQUIRE(oss.str() == "0xa_f25");
+  }
+
+  SECTION("of 16 prints 0x10_f25") {
+    oss << 0x10_f25;
+    REQUIRE(oss.str() == "0x10_f25");
+  }
+
+  SECTION("of the modulus prints 0x0_f25") {
+    oss << 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47_f25;
+    REQUIRE(oss.str() == "0x0_f25");
+  }
+
+  SECTION("of the modulus minus one prints a pre-computed value") {
+    oss << 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd46_f25;
+    REQUIRE(oss.str() == "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd46_f25");
+  }
+
+  SECTION("of the modulus plus one prints as one") {
+    oss << 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd48_f25;
+    REQUIRE(oss.str() == "0x1_f25");
+  }
+}
