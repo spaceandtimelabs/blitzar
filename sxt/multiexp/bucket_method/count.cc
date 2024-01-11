9e8790f1ed6ff66e72990c4d1ffae89c84330397
rnburn <ryan.burn@gmail.com>
2024-01-11 03:25:18 -0800

    rework

--- sxt/multiexp/bucket_method/count.cc
+++ sxt/multiexp/bucket_method/count.cc
@@ -8,6 +8,7 @@
 #include "sxt/memory/resource/async_device_resource.h"
 #include "sxt/memory/resource/chained_resource.h"
 #include "sxt/memory/resource/pinned_resource.h"
+#include "sxt/multiexp/base/digit_utility.h"
 
 namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
@@ -21,9 +22,9 @@ static __global__ void count_bucket_entries_kernel(unsigned* count_array,
   unsigned bucket_group_index = threadIdx.y;
   unsigned partition_index = blockIdx.x;
   unsigned num_partitions = gridDim.x;
-  unsigned num_buckets_per_group = 1u << bit_width;
+  unsigned num_buckets_per_group = (1u << bit_width) - 1;
   unsigned num_bucket_groups = blockDim.y;
-  auto output_scalars = scalars[output_index];
+  auto output_scalars = scalars[output_index] + partition_index;
   auto bucket_counts = count_array;
   bucket_counts += output_index * num_buckets_per_group * num_buckets_per_group * num_partitions;
   bucket_counts += bucket_group_index * num_buckets_per_group * num_partitions;
@@ -49,11 +50,14 @@ void count_bucket_entries(memmg::managed_array<unsigned>& count_array, const bas
                           basct::cspan<uint8_t*> scalars, unsigned n, unsigned element_num_bytes,
                           unsigned bit_width, unsigned num_partitions) noexcept {
   SXT_DEBUG_ASSERT(
-      basdv::is_active_device_pointer(scalars.data())
+      // clang-format off
+      basdv::is_active_device_pointer(scalars.data()) &&
+      num_partitions <= n
+      // clang-format on
   );
   auto num_bucket_groups = basn::divide_up(element_num_bytes * 8u, bit_width);
   auto num_outputs = scalars.size();
-  auto num_buckets_per_group = 1u << bit_width;
+  auto num_buckets_per_group = (1u << bit_width) - 1;
   count_array.resize(num_outputs * num_bucket_groups * num_buckets_per_group * num_partitions);
   SXT_DEBUG_ASSERT(basdv::is_active_device_pointer(count_array.data()));
   memr::async_device_resource resource{stream};
