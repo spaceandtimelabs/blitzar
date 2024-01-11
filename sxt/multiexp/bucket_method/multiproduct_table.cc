73237184290aa6866dc2f064cb2e4de2fc8f4de4
rnburn <ryan.burn@gmail.com>
2024-01-10 23:29:49 -0800

    rework bucket method

--- sxt/multiexp/bucket_method/multiproduct_table.cc
+++ sxt/multiexp/bucket_method/multiproduct_table.cc
@@ -1,1 +1,22 @@
 #include "sxt/multiexp/bucket_method/multiproduct_table.h"
+
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/execution/async/future.h"
+
+namespace sxt::mtxbk {
+//--------------------------------------------------------------------------------------------------
+// compute_multiproduct_table
+//--------------------------------------------------------------------------------------------------
+xena::future<void> compute_multiproduct_table(memmg::managed_array<bucket_descriptor>& table,
+                                              memmg::managed_array<unsigned>& indexes,
+                                              basct::cspan<uint8_t> scalars,
+                                              unsigned element_num_bytes,
+                                              unsigned bit_width) noexcept {
+  (void)table;
+  (void)indexes;
+  (void)scalars;
+  (void)element_num_bytes;
+  (void)bit_width;
+  return {};
+}
+} // namespace sxt::mtxbk
--- sxt/multiexp/bucket_method/multiproduct_table.h
+++ sxt/multiexp/bucket_method/multiproduct_table.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+#include "sxt/memory/management/managed_array_fwd.h"
+#include "sxt/execution/async/future_fwd.h"
+
 namespace sxt::mtxbk {
+struct bucket_descriptor;
+
 //--------------------------------------------------------------------------------------------------
 // compute_multiproduct_table
 //--------------------------------------------------------------------------------------------------
-  /* xena::future<void> compute_multiproduct_table( */
-
+xena::future<void> compute_multiproduct_table(memmg::managed_array<bucket_descriptor>& table,
+                                              memmg::managed_array<unsigned>& indexes,
+                                              basct::cspan<uint8_t> scalars,
+                                              unsigned element_num_bytes,
+                                              unsigned bit_width) noexcept;
 } // namespace sxt::mtxbk
