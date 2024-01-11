fe772fdaa514b9d71f22be7406174a09b6b1cf18
rnburn <ryan.burn@gmail.com>
2024-01-11 03:33:07 -0800

    rework bucket method

--- sxt/multiexp/bucket_method/count.cc
+++ sxt/multiexp/bucket_method/count.cc
@@ -24,13 +24,28 @@ static __global__ void count_bucket_entries_kernel(unsigned* count_array,
   unsigned num_partitions = gridDim.x;
   unsigned num_buckets_per_group = (1u << bit_width) - 1;
   unsigned num_bucket_groups = blockDim.y;
-  auto output_scalars = scalars[output_index] + partition_index;
   auto bucket_counts = count_array;
   bucket_counts += output_index * num_buckets_per_group * num_buckets_per_group * num_partitions;
   bucket_counts += bucket_group_index * num_buckets_per_group * num_partitions;
   bucket_counts += partition_index * num_buckets_per_group;
   for (unsigned count_index = 0; count_index < num_buckets_per_group; ++count_index) {
     bucket_counts[count_index] = 0;
+  }
+  auto output_scalars = scalars[output_index];
+  auto byte_width = basn::divide_up(bit_width, 8u);
+  for (unsigned i = partition_index; i < n; i += num_partitions) {
+    basct::cspan<uint8_t> scalar{
+        output_scalars + i * element_num_bytes,
+        element_num_bytes,
+    };
+    uint16_t digit = 0;
+    mtxb::extract_digit({reinterpret_cast<uint8_t*>(&digit), byte_width}, scalar, bit_width,
+                        bucket_group_index);
+    (void)digit;
+    /* CUDA_CALLABLE void extract_digit(basct::span<uint8_t> digit, basct::cspan<uint8_t> e, */
+    /*                                  size_t radix_log2, size_t digit_index) noexcept; */
+    (void)scalar;
+    (void)i;
   }
   (void)output_scalars;
   (void)output_index;
@@ -52,7 +67,8 @@ void count_bucket_entries(memmg::managed_array<unsigned>& count_array, const bas
   SXT_DEBUG_ASSERT(
       // clang-format off
       basdv::is_active_device_pointer(scalars.data()) &&
-      num_partitions <= n
+      num_partitions <= n &&
+      bit_width <= 16
       // clang-format on
   );
   auto num_bucket_groups = basn::divide_up(element_num_bytes * 8u, bit_width);
