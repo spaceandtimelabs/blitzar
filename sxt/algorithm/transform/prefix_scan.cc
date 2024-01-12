95823fcd5f2cae49bebcdf27da2cbf393ad3a7d1
rnburn <ryan.burn@gmail.com>
2024-01-11 22:01:35 -0800

    compilation bug

---
+++ sxt/algorithm/transform/prefix_scan.cc
@@ -0,0 +1,8 @@
+#include "sxt/algorithm/transform/prefix_scan.h"
+
+// #include <exception>
+
+#include "cub/cub.cuh"
+
+namespace sxt::algtr {
+} // namespace sxt::algtr
---
+++ sxt/algorithm/transform/prefix_scan.t.cc
@@ -0,0 +1,5 @@
+#include "sxt/algorithm/transform/prefix_scan.h"
+
+#include "sxt/base/test/unit_test.h"
+using namespace sxt;
+using namespace sxt::algtr;
