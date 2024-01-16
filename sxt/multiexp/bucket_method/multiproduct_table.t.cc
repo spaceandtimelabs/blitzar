9c7fd8c8e2423bb1f733ad2bed0087a2ddf2e58f
rnburn <ryan.burn@gmail.com>
2024-01-15 16:57:00 -0800

    fill in multiproduct table

--- sxt/multiexp/bucket_method/multiproduct_table.t.cc
+++ sxt/multiexp/bucket_method/multiproduct_table.t.cc
@@ -1,2 +1,6 @@
 #include "sxt/multiexp/bucket_method/multiproduct_table.h"
 
+#include "sxt/base/test/unit_test.h"
+
+TEST_CASE("we can compute the multiproduct table used for the bucket method") {
+}
