18501d3f23c77b435745cb34e393ae8110eeff52
rnburn <ryan.burn@gmail.com>
2024-01-20 20:52:03 -0800

    fill in reduction

--- sxt/multiexp/bucket_method/reduction.h
+++ sxt/multiexp/bucket_method/reduction.h
@@ -22,29 +22,6 @@ namespace sxt::mtxbk {
 template <bascrv::element T>
 CUDA_CALLABLE void reduce_bucket_group(T* __restrict__ reductions,
                                        const T* __restrict__ bucket_sums, unsigned bit_width,
-                                       unsigned num_buckets_per_output, unsigned reduction_width,
-                                       unsigned reduction_index) noexcept {
-  auto num_reductions_per_output = basn::divide_up(num_buckets_per_output, reduction_width);
-  auto output_index = reduction_index / num_reductions_per_output;
-  auto bucket_first = reduction_index % num_reductions_per_output;
-  auto bucket_last = std::min(bucket_first + reduction_width, num_buckets_per_output);
-  bucket_first += output_index * num_buckets_per_output;
-  bucket_last += output_index * num_buckets_per_output;
-  assert(bucket_last - bucket_first > 1 && "there must be at least two buckets to reduce");
-  T res = bucket_sums[--bucket_last];
-  while (bucket_first != bucket_last) {
-    for (unsigned i = 0; i < bit_width; ++i) {
-      double_element(res, res);
-    }
-    auto e = bucket_sums[--bucket_last];
-    add_inplace(res, e);
-  }
-  reductions[reduction_index] = res;
-}
-
-template <bascrv::element T>
-CUDA_CALLABLE void reduce_bucket_group(T* __restrict__ reductions,
-                                       const T* __restrict__ bucket_sums, unsigned bit_width,
                                        unsigned reduction_width,
                                        unsigned reduction_index) noexcept {
   auto num_buckets_per_group = (1u << bit_width) - 1u;
@@ -55,47 +32,20 @@ CUDA_CALLABLE void reduce_bucket_group(T* __restrict__ reductions,
   auto bucket_first = bucket_first_group + reduction_group_index * reduction_width;
   auto bucket_last =
       std::min(bucket_first + reduction_width, bucket_first_group + num_buckets_per_group);
-  auto res = bucket_sums[--bucket_last];
+  auto sum = bucket_sums[--bucket_last];
+  auto partial = sum;
   while (bucket_last != bucket_first) {
-    add_inplace(res, res, bucket_sums[--bucket_last]);
+    auto g = bucket_sums[--bucket_last];
+    add_inplace(sum, g);
+    add(partial, partial, sum);
   }
-  reductions[reduction_index] = res;
+  reductions[2u * reduction_index] = sum;
+  reductions[2u * reduction_index + 1u] = partial;
 }
 
 //--------------------------------------------------------------------------------------------------
 // compute_partial_reduction_device
 //--------------------------------------------------------------------------------------------------
-template <bascrv::element T>
-void compute_partial_reduction_device(basct::span<T> reductions, const basdv::stream& stream,
-                                      basct::cspan<T> bucket_sums, unsigned bit_width,
-                                      unsigned num_outputs, unsigned reduction_width) noexcept {
-  auto num_buckets = bucket_sums.size();
-  auto num_buckets_per_output = num_buckets / num_outputs;
-  auto num_reductions_per_output = basn::divide_up(num_buckets_per_output, reduction_width);
-  auto num_reductions = num_reductions_per_output * num_outputs;
-  if (reduction_width == 1) {
-    basdv::async_copy_device_to_host(reductions, bucket_sums, stream);
-    return;
-  }
-  memr::async_device_resource resource{stream};
-  memmg::managed_array<T> reductions_dev{num_reductions, &resource};
-  auto f = [
-               // clang-format off
-    reductions = reductions_dev.data(),
-    bucket_sums = bucket_sums,
-    bit_width = bit_width,
-    num_buckets_per_output = num_buckets_per_output,
-    reduction_width = reduction_width
-               // clang-format on
-  ] __device__
-           __host__(unsigned /*num_reductions*/, unsigned reduction_index) noexcept {
-             reduce_bucket_group(reductions, bucket_sums, bit_width, num_buckets_per_output,
-                                 reduction_width, reduction_index);
-           };
-  algi::launch_for_each_kernel(stream, f, num_reductions);
-  basdv::async_copy_device_to_host(reductions, reductions_dev, stream);
-}
-
 template <bascrv::element T>
 void compute_partial_reduction_device(basct::span<T> reductions, const basdv::stream& stream,
                                       basct::cspan<T> bucket_sums, unsigned bit_width,
@@ -147,6 +97,11 @@ unsigned plan_reduction(unsigned num_buckets, unsigned num_outputs) noexcept;
 template <bascrv::element T>
 xena::future<> compute_reduction(basct::span<T> reductions, basct::cspan<T> bucket_sums,
                                  unsigned bit_width, unsigned reduction_width = 0) noexcept {
+  (void)reductions;
+  (void)bucket_sums;
+  (void)bit_width;
+  (void)reduction_width;
+#if 0
   auto num_buckets = bucket_sums.size();
   auto num_outputs = reductions.size();
   auto num_buckets_per_output = num_buckets / num_outputs;
@@ -166,5 +121,6 @@ xena::future<> compute_reduction(basct::span<T> reductions, basct::cspan<T> buck
   co_await xendv::await_stream(std::move(stream));
   compute_partial_reduction_host(reductions, partial_reductions, bit_width, num_outputs,
                                  num_reductions_p / num_outputs);
+#endif
 }
 } // namespace sxt::mtxbk
