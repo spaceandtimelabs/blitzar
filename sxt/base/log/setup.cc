ff8d1b46c64561030b506c31e0e608925fb3d577
rnburn <ryan.burn@gmail.com>
2024-01-29 02:36:41 -0800

    reformat

--- sxt/base/log/setup.cc
+++ sxt/base/log/setup.cc
@@ -1,13 +1,29 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2024-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "sxt/base/log/setup.h"
 
-#include <cstdlib>
 #include <cctype>
+#include <cstdlib>
 
 #include "spdlog/spdlog.h"
 
 namespace sxt::basl {
 //--------------------------------------------------------------------------------------------------
-// set_log_level 
+// set_log_level
 //--------------------------------------------------------------------------------------------------
 static void set_log_level() noexcept {
   auto level = std::getenv("BLITZAR_LOG_LEVEL");
@@ -37,7 +53,7 @@ static void set_log_level() noexcept {
 }
 
 //--------------------------------------------------------------------------------------------------
-// setup_logger 
+// setup_logger
 //--------------------------------------------------------------------------------------------------
 void setup_logger() noexcept {
   static thread_local bool is_initialized = [] {
