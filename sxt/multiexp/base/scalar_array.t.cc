da4f08afbaa30a7628e2a417188fb372534aa224
rnburn <ryan.burn@gmail.com>
2024-01-29 15:58:08 -0800

    fill in transpose testing

--- sxt/multiexp/base/scalar_array.t.cc
+++ sxt/multiexp/base/scalar_array.t.cc
@@ -32,7 +32,7 @@ TEST_CASE("we can copy scalars to a device array") {
 TEST_CASE("we can copy transpose scalar arrays to device memory") {
   memmg::managed_array<uint8_t> array{memr::get_managed_device_resource()};
 
-  SECTION("we can transpose a single scalar with 32 elements") {
+  SECTION("we can transpose a single scalar of 32 bytes") {
     std::vector<uint8_t> scalars1(32);
     std::iota(scalars1.begin(), scalars1.end(), 0);
     array.resize(32);
@@ -42,5 +42,21 @@ TEST_CASE("we can copy transpose scalar arrays to device memory") {
     REQUIRE(fut.ready());
     REQUIRE(array[0] == 0u);
     REQUIRE(array[31u] == 31u);
+  }
+
+  SECTION("we can transpose two scalars of 32 bytes") {
+    std::vector<uint8_t> scalars1(64);
+    std::iota(scalars1.begin(), scalars1.end(), 0);
+    array.resize(64);
+    std::vector<const uint8_t*> scalars = {scalars1.data()};
+    auto fut = sxt::mtxb::make_transposed_device_scalar_array(array, scalars, 32, 2);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(array[0] == 0u);
+    REQUIRE(array[1] == 32u);
+    REQUIRE(array[2] == 1u);
+    REQUIRE(array[3] == 33u);
+    REQUIRE(array[62] == 31u);
+    REQUIRE(array[63] == 63u);
   }
 }
