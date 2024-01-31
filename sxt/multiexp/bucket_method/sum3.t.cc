9aae6dbc390d30f6299a0382e5c33733f0062904
rnburn <ryan.burn@gmail.com>
2024-01-31 13:28:31 -0800

    rework

--- sxt/multiexp/bucket_method/sum3.t.cc
+++ sxt/multiexp/bucket_method/sum3.t.cc
@@ -10,9 +10,98 @@ using namespace sxt::mtxbk;
 
 using E = bascrv::element97;
 
-TEST_CASE("we accumulate buckets") {
+TEST_CASE("we accumulate buckets with a bit width of 8") {
   unsigned element_num_bytes = 32;
   unsigned bit_width = 8;
+  unsigned num_buckets_per_output = ((1u << bit_width) - 1u) * element_num_bytes;
+
+  std::vector<E> sums;
+  std::vector<E> generators;
+  std::vector<const uint8_t*> scalars;
+
+  sums.resize(num_buckets_per_output);
+
+  std::vector<E> expected(num_buckets_per_output, 0u);
+
+  SECTION("we can accumulate a single element of zero") {
+    generators = {33u};
+    std::vector<uint8_t> scalars1(32);
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(sums == expected);
+  }
+
+  SECTION("we can accumulate a single element of one") {
+    generators = {33u};
+    std::vector<uint8_t> scalars1(32);
+    scalars1[0] = 1u;
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[0] = 33u;
+    REQUIRE(sums == expected);
+  }
+
+  SECTION("we can accumulate a single element of two") {
+    generators = {33u};
+    std::vector<uint8_t> scalars1(32);
+    scalars1[0] = 2u;
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[1] = 33u;
+    REQUIRE(sums == expected);
+  }
+
+  SECTION("we can accumulate two elements into the same bucket") {
+    generators = {33u, 44u};
+    std::vector<uint8_t> scalars1(64);
+    scalars1[0] = 1u;
+    scalars1[32] = 1u;
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width, 1u);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[0] = 77u;
+    REQUIRE(sums == expected);
+  }
+
+  SECTION("we can accumulate three elements into the same bucket") {
+    generators = {33u, 44u, 55u};
+    std::vector<uint8_t> scalars1(32u * 3u);
+    scalars1[0] = 1u;
+    scalars1[32] = 1u;
+    scalars1[64] = 1u;
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width, 1u);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[0] = 33u + 44u + 55u;
+    REQUIRE(sums == expected);
+  }
+
+  SECTION("we can accumulate multiple elements in different buckets") {
+    generators = {33u, 44u};
+    std::vector<uint8_t> scalars1(64);
+    scalars1[0] = 1u;
+    scalars1[32] = 2u;
+    scalars = {scalars1.data()};
+    auto fut = compute_bucket_sums3<E>(sums, generators, scalars, element_num_bytes, bit_width, 1u);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[0] = 33u;
+    expected[1] = 44u;
+    REQUIRE(sums == expected);
+  }
+}
+
+TEST_CASE("we accumulate buckets with a bit width of 4") {
+  unsigned element_num_bytes = 32;
+  unsigned bit_width = 4;
   unsigned num_buckets_per_output = ((1u << bit_width) - 1u) * element_num_bytes;
 
   std::vector<E> sums;
