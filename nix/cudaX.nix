4503c714f113dd77e55f301495bb8a374b65ab79
rnburn <ryan.burn@gmail.com>
2024-02-01 09:29:50 -0800

    work on cuda derivation

--- nix/cudaX.nix
+++ nix/cudaX.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 with pkgs;
 pkgs.stdenvNoCC.mkDerivation {
-  name = "cudaXWrapped";
+  name = "cudaX1Wrapped";
   src = fetchurl {
     url = "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run";
     sha256 = "24b2afc9f770d8cf43d6fa7adc2ebfd47c4084db01bdda1ce3ce0a4d493ba65b";
@@ -23,6 +23,13 @@ pkgs.stdenvNoCC.mkDerivation {
         (cd $dir/lib64 && find . -type d -exec mkdir -p $out/lib64/\{} \;)
         (cd $dir/lib64 && find . \( -type f -o -type l \) -exec mv \{} $out/lib64/\{} \;)
       fi
+
+      # mv pkg/builds/cuda_sanitizer_api $out/cuda_sanitizer_api
+      # ln -s $out/cuda_sanitizer_api/compute-sanitizer/compute-sanitizer $out/bin/compute-sanitizer
+
+      # mv pkg/builds/nsight_systems/target-linux-x64 $out/target-linux-x64
+      # mv pkg/builds/nsight_systems/host-linux-x64 $out/host-linux-x64
+      # rm $out/host-linux-x64/libstdc++.so*
     done
   '';
 }
