4999250f10579740435ac32df2a68b182e3a6079
rnburn <ryan.burn@gmail.com>
2024-02-01 10:31:17 -0800

    set up env

--- nix/cudaX.nix
+++ nix/cudaX.nix
@@ -6,11 +6,15 @@ pkgs.stdenvNoCC.mkDerivation {
     url = "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run";
     sha256 = "24b2afc9f770d8cf43d6fa7adc2ebfd47c4084db01bdda1ce3ce0a4d493ba65b";
   };
+  patches = [
+    ./cuda_host_defines.patch
+  ];
   unpackPhase = ''
     sh $src --keep --noexec
   '';
   installPhase = ''
     mkdir -p $out/bin $out/lib64 $out/include
+    cp -r pkg/builds $out/builds
     for dir in pkg/builds/*; do
       if [ -d $dir/bin ]; then
         mv $dir/bin/* $out/bin
