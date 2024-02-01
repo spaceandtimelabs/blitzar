dacb5982752e0d8561ba0fa5b010800910ebe08c
rnburn <ryan.burn@gmail.com>
2024-02-01 09:39:42 -0800

    custom cuda

--- nix/cudaX.nix
+++ nix/cudaX.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 with pkgs;
 pkgs.stdenvNoCC.mkDerivation {
-  name = "cudaX1Wrapped";
+  name = "cudaWrapped";
   src = fetchurl {
     url = "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run";
     sha256 = "24b2afc9f770d8cf43d6fa7adc2ebfd47c4084db01bdda1ce3ce0a4d493ba65b";
@@ -24,12 +24,13 @@ pkgs.stdenvNoCC.mkDerivation {
         (cd $dir/lib64 && find . \( -type f -o -type l \) -exec mv \{} $out/lib64/\{} \;)
       fi
 
-      # mv pkg/builds/cuda_sanitizer_api $out/cuda_sanitizer_api
-      # ln -s $out/cuda_sanitizer_api/compute-sanitizer/compute-sanitizer $out/bin/compute-sanitizer
-
-      # mv pkg/builds/nsight_systems/target-linux-x64 $out/target-linux-x64
-      # mv pkg/builds/nsight_systems/host-linux-x64 $out/host-linux-x64
-      # rm $out/host-linux-x64/libstdc++.so*
     done
+    mv pkg/builds/cuda_nvcc/nvvm $out/nvvm
+    mv pkg/builds/cuda_sanitizer_api $out/cuda_sanitizer_api
+    ln -s $out/cuda_sanitizer_api/compute-sanitizer/compute-sanitizer $out/bin/compute-sanitizer
+
+    mv pkg/builds/nsight_systems/target-linux-x64 $out/target-linux-x64
+    mv pkg/builds/nsight_systems/host-linux-x64 $out/host-linux-x64
+    rm $out/host-linux-x64/libstdc++.so*
   '';
 }
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,8 +1,8 @@
 { pkgs }:
 let
   clang = import ./clang.nix { inherit pkgs; };
-  cuda = import ./cuda.nix { inherit pkgs; };
-  cudaX = import ./cudaX.nix { inherit pkgs; };
+  # cuda = import ./cuda.nix { inherit pkgs; };
+  cuda = import ./cudaX.nix { inherit pkgs; };
   bazel = import ./bazel.nix { inherit pkgs; inherit clang; inherit cuda; };
 in
 with pkgs;
@@ -18,7 +18,6 @@ mkShell {
     bazel
     clang
     cuda
-    cudaX
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath [
     "/usr/lib/wsl"
