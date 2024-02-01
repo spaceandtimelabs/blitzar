127718db74b55c66c56e6637ebc6a08ab90abc5c
rnburn <ryan.burn@gmail.com>
2024-01-31 22:21:51 -0800

    bump to cuda 12.3

--- nix/cuda.nix
+++ nix/cuda.nix
@@ -1,8 +1,8 @@
 { pkgs }:
 with pkgs;
 let
-  toolkit = cudaPackages_12_2.cudatoolkit;
-  toolkit-lib = cudaPackages_12_2.cudatoolkit.lib;
+  toolkit = cudaPackages_12_3.cudatoolkit;
+  toolkit-lib = cudaPackages_12_3.cudatoolkit.lib;
 in
 pkgs.stdenvNoCC.mkDerivation {
   name = "cudaWrapped";
@@ -12,22 +12,51 @@ pkgs.stdenvNoCC.mkDerivation {
     toolkit-lib
   ];
 
+  src = [
+    ./host_defines.h
+  ];
+
   unpackPhase = "true";
   buildPhase = "";
 
-  installPhase = ''
+  installPhase = 
+  let
+    hostDefines = src."./host_defines.h";
+  in
+  ''
     mkdir $out
     for f in `ls -1 ${toolkit}`
     do
-      if [[ $f != "lib64" && $f != "lib" ]]; then
+      if [[ $f != "lib64" && $f != "include" && $f != "lib" ]]; then
         ln -s ${toolkit}/$f $out/$f
       fi
     done
+
+    mkdir $out/include
+    for f in `ls -1 ${toolkit}/include`
+    do
+      if [[ $f != "crt" ]]; then
+        ln -s ${toolkit}/include/$f $out/include/$f
+      fi
+    done
+    mkdir $out/include/crt
+    for f in `ls -1 ${toolkit}/include/crt`
+    do
+      if [[ $f != "host_defines.h" ]]; then
+        ln -s ${toolkit}/include/crt/$f $out/include/crt/$f
+      fi
+    done
+
     mkdir $out/lib64
     for f in `ls -1 ${toolkit}/lib`
     do
       ln -s ${toolkit}/lib/$f $out/lib64/$f
     done
     ln -s ${toolkit-lib}/lib/libcudart_static.a $out/lib64/libcudart_static.a
+    mkdir $out/patched
+    for srcFile in $src; do
+      cp $srcFile $out/patched/$(stripHash $srcFile)
+    done
+    ln -s $out/patched/host_defines.h $out/include/crt/host_defines.h
   '';
 }
