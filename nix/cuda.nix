951f01327e7d845fcbcdd9fe56dda5dec2d04f7c
rnburn <ryan.burn@gmail.com>
2024-02-01 10:34:49 -0800

    comment

--- nix/cuda.nix
+++ nix/cuda.nix
@@ -1,12 +1,17 @@
+# Set up a custom install for cuda toolkit
+#
+# This is derived from
+#  https://github.com/NixOS/nixpkgs/blob/72db73af3db9392554b89eb9aeb3187ae9dca78e/pkgs/development/cuda-modules/cudatoolkit/default.nix
 { pkgs }:
 with pkgs;
 pkgs.stdenvNoCC.mkDerivation {
-  name = "cudaWrapped";
+  name = "cudatoolkit";
   src = fetchurl {
     url = "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run";
     sha256 = "24b2afc9f770d8cf43d6fa7adc2ebfd47c4084db01bdda1ce3ce0a4d493ba65b";
   };
   patches = [
+    # patch host_defines.h to work with libc++
     ./cuda_host_defines.patch
   ];
   unpackPhase = ''
