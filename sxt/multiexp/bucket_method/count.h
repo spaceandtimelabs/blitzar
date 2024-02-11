c9cf6f89fe3cdea4fdd2f44b91255ef52df87083
rnburn <ryan.burn@gmail.com>
2024-02-11 09:08:37 -0800

    rework count

--- sxt/multiexp/bucket_method/count.h
+++ sxt/multiexp/bucket_method/count.h
@@ -14,4 +14,9 @@ void inclusive_prefix_count_buckets(basct::span<unsigned> counts, const basdv::s
                                     basct::cspan<uint8_t> digits, unsigned element_num_bytes,
                                     unsigned bit_width, unsigned num_outputs, unsigned tile_size,
                                     unsigned n) noexcept;
+
+void inclusive_prefix_count_buckets(basct::span<unsigned> counts, basct::span<uint16_t> tile_counts,
+                                    const basdv::stream& stream, basct::cspan<uint8_t> digits,
+                                    unsigned element_num_bytes, unsigned bit_width,
+                                    unsigned num_outputs, unsigned tile_size, unsigned n) noexcept;
 } // namespace sxt::mtxbk
