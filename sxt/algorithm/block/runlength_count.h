e3a8d6903b0e6a5612b8d023ac1a83118e964174
rnburn <ryan.burn@gmail.com>
2024-02-11 13:07:38 -0800

    fill in runlength count

--- sxt/algorithm/block/runlength_count.h
+++ sxt/algorithm/block/runlength_count.h
@@ -1,4 +1,30 @@
 #pragma once
 
+#include "cub/cub.cuh"
+
 namespace sxt::algbk {
+//--------------------------------------------------------------------------------------------------
+// runlength_count 
+//--------------------------------------------------------------------------------------------------
+template <class T, class CounterT, unsigned NumThreads, unsigned NumBins>
+class runlength_count {
+  using Discontinuity = cub::BlockDiscontinuity<T, NumThreads>;
+public:
+  struct temp_storage {
+    typename Discontinuity::TempStorage discontinuity;
+    CounterT run_begin[NumBins];
+    CounterT run_end[NumBins];
+  };
+
+  explicit runlength_count(temp_storage& storage) noexcept
+      : storage_{storage}, discontinuity_{storage.discontinuity} {}
+
+  template <unsigned ItemsPerThread>
+  void count(CounterT counts[NumBins], T (&items)[ItemsPerThread]) noexcept {
+  } 
+
+private:
+  temp_storage& storage_;
+  Discontinuity discontinuity_;
+};
 } // namespace sxt::algbk
