2507bb6b6ee2ffbcd4c5d2eb8d2e4e59220794a9
rnburn <ryan.burn@gmail.com>
2024-02-11 01:04:28 -0800

    fill in count

--- sxt/multiexp/bucket_method/count.cc
+++ sxt/multiexp/bucket_method/count.cc
@@ -1,6 +1,9 @@
 #include "sxt/multiexp/bucket_method/count.h"
 
+#include "sxt/base/device/memory_utility.h"
 #include "sxt/base/device/stream.h"
+#include "sxt/base/error/assert.h"
+#include "sxt/base/num/divide_up.h"
 
 namespace sxt::mtxbk {
 //--------------------------------------------------------------------------------------------------
@@ -10,6 +13,18 @@ void inclusive_prefix_count_buckets(basct::span<unsigned> counts, const basdv::s
                                     basct::cspan<uint8_t> digits, unsigned element_num_bytes,
                                     unsigned bit_width, unsigned num_outputs, unsigned n,
                                     unsigned num_tiles) noexcept {
+  auto num_buckets_per_digit = (1u << bit_width) - 1u;
+  auto num_digits = basn::divide_up(element_num_bytes, 8u * bit_width);
+  auto num_buckets_per_output = num_buckets_per_digit * num_digits;
+  auto num_buckets_total = num_buckets_per_digit * num_outputs;
+  SXT_DEBUG_ASSERT(
+      // clang-format off
+      counts.size() == num_buckets_total * num_tiles &&
+      digits.size() == num_digits * n * num_outputs &&
+      basdv::is_active_device_pointer(counts.data()) &&
+      basdv::is_active_device_pointer(digits.data())
+      // clang-format on
+  );
   (void)counts;
   (void)stream;
   (void)digits;
