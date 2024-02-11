91926989ec428a08e9b2574dc894c21f3cb5d4d7
rnburn <ryan.burn@gmail.com>
2024-02-11 00:07:02 -0800

    fill in transpose scalars

--- sxt/multiexp/base/scalar_array.t.cc
+++ sxt/multiexp/base/scalar_array.t.cc
@@ -1,5 +1,46 @@
 #include "sxt/multiexp/base/scalar_array.h"
 
+#include <vector>
+#include <numeric>
+
+#include "sxt/base/device/stream.h"
+#include "sxt/base/device/synchronization.h"
 #include "sxt/base/test/unit_test.h"
+#include "sxt/execution/async/future.h"
+#include "sxt/execution/schedule/scheduler.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/managed_device_resource.h"
 using namespace sxt;
 using namespace sxt::mtxb;
+
+TEST_CASE("we can copy transpose scalar arrays to device memory") {
+  memmg::managed_array<uint8_t> array{memr::get_managed_device_resource()};
+
+  SECTION("we can transpose a single scalar of 32 bytes") {
+    std::vector<uint8_t> scalars1(32);
+    std::iota(scalars1.begin(), scalars1.end(), 0);
+    array.resize(32);
+    std::vector<const uint8_t*> scalars = {scalars1.data()};
+    auto fut = transpose_scalars_to_device(array, scalars, 32, 8, 1);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(array[0] == 0u);
+    REQUIRE(array[31u] == 31u);
+  }
+
+  SECTION("we can transpose two scalars of 32 bytes") {
+    std::vector<uint8_t> scalars1(64);
+    std::iota(scalars1.begin(), scalars1.end(), 0);
+    array.resize(64);
+    std::vector<const uint8_t*> scalars = {scalars1.data()};
+    auto fut = transpose_scalars_to_device(array, scalars, 32, 8, 2);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(array[0] == 0u);
+    REQUIRE(array[1] == 32u);
+    REQUIRE(array[2] == 1u);
+    REQUIRE(array[3] == 33u);
+    REQUIRE(array[62] == 31u);
+    REQUIRE(array[63] == 63u);
+  }
+}
