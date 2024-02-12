74c8239e75206f0d44c3aeabb80b7a4e47617ea6
rnburn <ryan.burn@gmail.com>
2024-02-11 20:10:50 -0800

    fill in multiproduct table tests

--- sxt/multiexp/bucket_method/multiproduct_table.t.cc
+++ sxt/multiexp/bucket_method/multiproduct_table.t.cc
@@ -1,7 +1,78 @@
 #include "sxt/multiexp/bucket_method/multiproduct_table.h"
 
+#include <algorithm>
+#include <vector>
+
+#include "sxt/base/device/synchronization.h"
 #include "sxt/base/test/unit_test.h"
+#include "sxt/execution/async/future.h"
+#include "sxt/execution/schedule/scheduler.h"
+#include "sxt/memory/resource/managed_device_resource.h"
 using namespace sxt;
 using namespace sxt::mtxbk;
 
-TEST_CASE("t") {}
+TEST_CASE("we can construct the multi-product table for the bucket method") {
+  std::pmr::vector<uint16_t> bucket_prefix_counts{memr::get_managed_device_resource()};
+  std::pmr::vector<uint16_t> indexes{memr::get_managed_device_resource()};
+  std::vector<const uint8_t*> scalars;
+  const unsigned element_num_bytes = 32;
+  const unsigned bit_width = 8;
+  const unsigned num_buckets_per_digit = 255;
+  const unsigned num_digits = 32;
+  bucket_prefix_counts.resize(num_buckets_per_digit * num_digits);
+  unsigned n = 0;
+
+  std::pmr::vector<uint16_t> expected_counts(bucket_prefix_counts.size());
+  std::pmr::vector<uint16_t> expected_indexes;
+
+  SECTION("we handle the case of a single scalar of zeros") {
+    n = 1;
+    std::vector<uint8_t> scalars1(32 * n);
+    scalars = {scalars1.data()};
+    indexes.resize(num_digits * n);
+    auto fut = make_multiproduct_table(bucket_prefix_counts, indexes, scalars, element_num_bytes,
+                                       bit_width, n);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected_indexes.resize(indexes.size());
+    basdv::synchronize_device();
+    REQUIRE(bucket_prefix_counts == expected_counts);
+    REQUIRE(indexes == expected_indexes);
+  }
+
+  SECTION("we handle the case of a single scalar of one") {
+    n = 1;
+    std::vector<uint8_t> scalars1(32 * n);
+    scalars1[0] = 1;
+    scalars = {scalars1.data()};
+    indexes.resize(num_digits * n);
+    auto fut = make_multiproduct_table(bucket_prefix_counts, indexes, scalars, element_num_bytes,
+                                       bit_width, n);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    std::fill_n(expected_counts.begin(), num_buckets_per_digit, 1u);
+    expected_indexes.resize(indexes.size());
+    basdv::synchronize_device();
+    REQUIRE(bucket_prefix_counts == expected_counts);
+    REQUIRE(indexes == expected_indexes);
+  }
+
+  SECTION("we handle two scalars of zero and one") {
+    n = 2;
+    std::vector<uint8_t> scalars1(32 * n);
+    scalars1[0] = 0;
+    scalars1[element_num_bytes] = 1;
+    scalars = {scalars1.data()};
+    indexes.resize(num_digits * n);
+    auto fut = make_multiproduct_table(bucket_prefix_counts, indexes, scalars, element_num_bytes,
+                                       bit_width, n);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    std::fill_n(expected_counts.begin(), num_buckets_per_digit, 1u);
+    expected_indexes.resize(indexes.size());
+    expected_indexes[0] = 1;
+    basdv::synchronize_device();
+    REQUIRE(bucket_prefix_counts == expected_counts);
+    REQUIRE(indexes == expected_indexes);
+  }
+}
