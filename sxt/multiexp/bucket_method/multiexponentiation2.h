ae821359ab18787ad0e07050c5f62b277b80c08e
rnburn <ryan.burn@gmail.com>
2024-02-14 14:26:59 -0800

    add stub

---
+++ sxt/multiexp/bucket_method/multiexponentiation2.h
@@ -0,0 +1,4 @@
+#pragma once
+
+namespace sxt::mtxbk {
+} // namespace sxt::mtxbk
---
+++ sxt/multiexp/bucket_method/multiexponentiation2.t.cc
@@ -0,0 +1,5 @@
+#include "sxt/multiexp/bucket_method/multiexponentiation2.h"
+
+#include "sxt/base/test/unit_test.h"
+
+TEST_CASE("t") {}
