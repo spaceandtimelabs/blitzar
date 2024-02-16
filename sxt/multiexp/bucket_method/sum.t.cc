22367bce499672130f9aaa7b3f12130c3c227212
rnburn <ryan.burn@gmail.com>
2024-02-16 10:13:59 -0800

    fill in tests

--- sxt/multiexp/bucket_method/sum.t.cc
+++ sxt/multiexp/bucket_method/sum.t.cc
@@ -1,7 +1,37 @@
 #include "sxt/multiexp/bucket_method/sum.h"
 
+#include <vector>
+
+#include "sxt/base/curve/example_element.h"
 #include "sxt/base/test/unit_test.h"
+#include "sxt/execution/schedule/scheduler.h"
 using namespace sxt;
 using namespace sxt::mtxbk;
 
-TEST_CASE("todo") {}
+TEST_CASE("we can compute the bucket sums for a chunk") {
+  using E = bascrv::element97;
+  const unsigned element_num_bytes = 32;
+  const unsigned bit_width = 8;
+
+  std::vector<E> sums(255 * 32);
+  std::vector<E> generators = {3u};
+  std::vector<const uint8_t*> scalars;
+
+  std::vector<E> expected(sums.size());
+
+  SECTION("we can compute bucket sums for a single exponent of zero") {
+    std::vector<uint8_t> scalar1(32);
+    scalars = {scalar1.data()};
+    auto fut =
+        sum_buckets_chunk<E>(sums, generators, scalars, element_num_bytes, bit_width);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(sums == expected);
+  }
+
+
+}
+/* template <bascrv::element T> */
+/* xena::future<> sum_buckets_chunk(basct::span<T> sums, basct::cspan<T> generators, */
+/*                                  basct::cspan<const uint8_t*> exponents, unsigned element_num_bytes, */
+/*                                  unsigned bit_width) noexcept { */
