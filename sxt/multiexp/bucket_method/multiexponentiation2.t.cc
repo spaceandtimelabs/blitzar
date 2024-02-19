ae1706ed67f273ab3cf3a8ec97022ad8a7ffd3d4
rnburn <ryan.burn@gmail.com>
2024-02-19 02:59:49 -0800

    fill in mx2 testing

--- sxt/multiexp/bucket_method/multiexponentiation2.t.cc
+++ sxt/multiexp/bucket_method/multiexponentiation2.t.cc
@@ -40,18 +40,18 @@ TEST_CASE("we can compute a multiexponentiation") {
     REQUIRE(res[0] == 0u);
   }
 
-#if 0
   SECTION("we can compute a multiexponentiation with a single element of 1") {
-    uint8_t scalar_data[32] = {};
-    scalar_data[0] = 1;
-    exponents.push_back(scalar_data);
+    std::vector<uint8_t> scalars1(32);
+    scalars1[0] = 1;
+    scalars = {scalars1.data()};
     generators = {12u};
-    auto fut = multiexponentiate<bascrv::element97>(res, generators, exponents);
+    auto fut = multiexponentiate2<bascrv::element97>(res, generators, scalars, element_num_bytes);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     REQUIRE(res[0] == 12u);
   }
 
+#if 0
   SECTION("we can compute a multiexponentiation with a single element of 2") {
     uint8_t scalar_data[32] = {};
     scalar_data[0] = 2;
