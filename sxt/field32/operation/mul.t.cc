ea1257fd4160b01e9557d8031f30fe37ed47856f
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-02-23 15:13:33 -0500

    small changes

--- sxt/field32/operation/mul.t.cc
+++ sxt/field32/operation/mul.t.cc
@@ -59,7 +59,6 @@ TEST_CASE("we can multiply finite field elements") {
   SECTION("another one") {
     auto e = 0x123_f32;
     auto inv = 0x5e9208cc18a1de9208cc18a1de9208cc18a1de9208cc18a1de9208cc18a1de84_f32;
-    f32t::element ei;
     mul(res, e, inv);
 
     // p_v + 1
