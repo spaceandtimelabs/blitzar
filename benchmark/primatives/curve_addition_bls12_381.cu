63d3e4f9e2ca6332ee820a7dbb02278dab315c75
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-02-29 15:48:02 -0500

    updates to improve benchmarks

--- benchmark/primatives/curve_addition_bls12_381.cu
+++ benchmark/primatives/curve_addition_bls12_381.cu
@@ -17,6 +17,7 @@
 #include <iostream>
 #include <chrono>
 
+#include "sxt/base/num/divide_up.h"
 #include "sxt/base/num/fast_random_number_generator.h"
 #include "sxt/curve_g1/operation/add.h"
 #include "sxt/curve_g1/random/element_p2.h"
@@ -30,9 +31,12 @@ namespace sxt {
 //--------------------------------------------------------------------------------------------------
 // vector_add_impl 
 //--------------------------------------------------------------------------------------------------
-__global__ void vector_add_impl(cg1t::element_p2* vec_a, cg1t::element_p2* vec_b, cg1t::element_p2* vec_r, size_t n_elments) {
+__global__ void vector_add_impl(const cg1t::element_p2* __restrict__ vec_a,
+                                const cg1t::element_p2* __restrict__ vec_b, 
+                                cg1t::element_p2* __restrict__ vec_r,
+                                unsigned n_elements) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < n_elments) {
+    if (tid < n_elements) {
       cg1o::add(vec_r[tid], vec_a[tid], vec_b[tid]);
     }
 }
@@ -40,22 +44,22 @@ __global__ void vector_add_impl(cg1t::element_p2* vec_a, cg1t::element_p2* vec_b
 //--------------------------------------------------------------------------------------------------
 // vector_add 
 //--------------------------------------------------------------------------------------------------
-void vector_add(cg1t::element_p2* vec_b, cg1t::element_p2* vec_a, cg1t::element_p2* vec_result, size_t n_elments) {
-    const unsigned int num_blocks = static_cast<unsigned int>(ceil(static_cast<float>(n_elments) / MAX_THREADS_PER_BLOCK));
-    const unsigned int threads_per_block = MAX_THREADS_PER_BLOCK;
+void vector_add(cg1t::element_p2* vec_b, cg1t::element_p2* __restrict__ vec_a, cg1t::element_p2* vec_result, unsigned n_elements) {
+    const unsigned threads_per_block = MAX_THREADS_PER_BLOCK;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
 
-    vector_add_impl<<<num_blocks, threads_per_block>>>(vec_a, vec_b, vec_result, n_elments);
+    vector_add_impl<<<num_blocks, threads_per_block>>>(vec_a, vec_b, vec_result, n_elements);
 }
 
 //--------------------------------------------------------------------------------------------------
 // init_random_array_impl 
 //--------------------------------------------------------------------------------------------------
-__global__ static void init_random_array_impl(cg1t::element_p2* rand, size_t n_elments) {
+__global__ static void init_random_array_impl(cg1t::element_p2* __restrict__ rand, unsigned n_elements) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < n_elments)
+    if (tid < n_elements)
     {
         basn::fast_random_number_generator rng{static_cast<uint64_t>(tid + 1),
-                                               static_cast<uint64_t>(n_elments + 1)};
+                                               static_cast<uint64_t>(n_elements + 1)};
         cg1rn::generate_random_element(rand[tid], rng);
     }
 }
@@ -63,11 +67,11 @@ __global__ static void init_random_array_impl(cg1t::element_p2* rand, size_t n_e
 //--------------------------------------------------------------------------------------------------
 // init_random_array
 //--------------------------------------------------------------------------------------------------
-void init_random_array(cg1t::element_p2* rand, size_t n_elments) {
-    const unsigned int num_blocks = static_cast<unsigned int>(ceil(static_cast<float>(n_elments) / MAX_THREADS_PER_BLOCK));
-    const unsigned int threads_per_block = MAX_THREADS_PER_BLOCK;
+void init_random_array(cg1t::element_p2* rand, unsigned n_elements) {
+    const unsigned threads_per_block = MAX_THREADS_PER_BLOCK;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
 
-    init_random_array_impl<<<num_blocks, threads_per_block>>>(rand, n_elments);
+    init_random_array_impl<<<num_blocks, threads_per_block>>>(rand, n_elements);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -95,11 +99,11 @@ void add(size_t vector_size, size_t repetitions) noexcept {
 
   // Benchmarking loop
   std::cout << "Starting benchmarking" << std::endl;
-  auto start_time = std::chrono::high_resolution_clock::now();
+  auto start_time = std::chrono::steady_clock::now();
   for (int i = 0; i < repetitions; i++) {
     vector_add(a.data(), b.data(), ret.data(), vector_size);
   }
-  auto end_time = std::chrono::high_resolution_clock::now();
+  auto end_time = std::chrono::steady_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 
   // Report data
