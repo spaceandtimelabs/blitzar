7c343ab7cf04becdde41fec5b5363347cdce789f
rnburn <ryan.burn@gmail.com>
2024-03-05 19:38:53 -0800

    fill in

--- sxt/multiexp/pippenger2/product_kernel.t.cc
+++ sxt/multiexp/pippenger2/product_kernel.t.cc
@@ -8,4 +8,16 @@
 using namespace sxt;
 using namespace sxt::mtxpp2;
 
-TEST_CASE("we can compute products with a lookup table") {}
+TEST_CASE("we can compute products with a lookup table") {
+  std::pmr::vector<bascrv::element97> products{memr::get_managed_device_resource()};
+  std::pmr::vector<bascrv::element97> table{memr::get_managed_device_resource()};
+  std::pmr::vector<uint16_t> bitsets{memr::get_managed_device_resource()};
+
+  (void)products;
+  (void)table;
+  (void)bitsets;
+/* template <unsigned ItemsPerThreadLg2, bascrv::element T> */
+/* __global__ void product_kernel(T* __restrict__ products, const uint16_t* __restrict__ bitsets, */
+/*                                const T* __restrict__ table, unsigned num_products, */
+/*                                unsigned n) { */
+}
