207ce10a52f026a108eaac8b9776cb679a5ddb74
rnburn <ryan.burn@gmail.com>
2024-03-05 19:34:07 -0800

    fill in product kernel

--- sxt/multiexp/pippenger2/product_kernel.t.cc
+++ sxt/multiexp/pippenger2/product_kernel.t.cc
@@ -1,5 +1,11 @@
 #include "sxt/multiexp/pippenger2/product_kernel.h"
 
-#include "sxt/base/test/unit_test.h"
+#include <vector>
 
-TEST_CASE("t") {}
+#include "sxt/base/curve/example_element.h"
+#include "sxt/base/test/unit_test.h"
+#include "sxt/memory/resource/managed_device_resource.h"
+using namespace sxt;
+using namespace sxt::mtxpp2;
+
+TEST_CASE("we can compute products with a lookup table") {}
