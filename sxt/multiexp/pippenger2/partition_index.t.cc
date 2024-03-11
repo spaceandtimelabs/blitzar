f8716459b92183c4f8f9e0c3e1f6bcc9a88b7d77
rnburn <ryan.burn@gmail.com>
2024-03-11 09:59:46 -0700

    fill in partition index

--- sxt/multiexp/pippenger2/partition_index.t.cc
+++ sxt/multiexp/pippenger2/partition_index.t.cc
@@ -12,18 +12,29 @@ using namespace sxt::mtxpp2;
 TEST_CASE("we can compute the partition indexes that correspond to a multiexponentiation") {
 
   std::pmr::vector<uint16_t> indexes{memr::get_managed_device_resource()};
+  indexes.resize(256);
   std::vector<const uint8_t*> scalars;
 
   std::pmr::vector<uint16_t> expected;
+  expected.resize(256);
 
-  SECTION("we handle the case of a single element") {
+  SECTION("we handle the case of a single element of zero") {
     std::vector<uint8_t> scalars1(32);
     scalars = {scalars1.data()};
-    indexes.resize(256u);
     auto fut = fill_partition_indexes(indexes, scalars, 32, 1);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
-    expected.resize(indexes.size());
+    REQUIRE(indexes == expected);
+  }
+
+  SECTION("we handle the case of a single element of one") {
+    std::vector<uint8_t> scalars1(32);
+    scalars1[0] = 1;
+    scalars = {scalars1.data()};
+    auto fut = fill_partition_indexes(indexes, scalars, 32, 1);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected[0] = 1;
     REQUIRE(indexes == expected);
   }
 }
