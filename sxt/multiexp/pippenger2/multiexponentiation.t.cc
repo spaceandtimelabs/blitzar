0e63a1683177f0a940e8ce9f9c34e370aeb4b965
rnburn <ryan.burn@gmail.com>
2024-03-13 12:20:41 -0700

    fill in mx testing

--- sxt/multiexp/pippenger2/multiexponentiation.t.cc
+++ sxt/multiexp/pippenger2/multiexponentiation.t.cc
@@ -1,6 +1,40 @@
 #include "sxt/multiexp/pippenger2/multiexponentiation.h"
 
-#include "sxt/base/test/unit_test.h"
-using namespace sxt;
+#include <vector>
 
-TEST_CASE("t") {}
+#include "sxt/base/curve/example_element.h"
+#include "sxt/base/test/unit_test.h"
+#include "sxt/execution/schedule/scheduler.h"
+using namespace sxt;
+using namespace sxt::mtxpp2;
+
+TEST_CASE("we can compute multiexponentiations") {
+  using E = bascrv::element97;
+
+  std::vector<E> res(1);
+  std::vector<E> partition_table(1u << 16u);
+  std::vector<const uint8_t*> scalars;
+
+  std::vector<E> expected(1);
+
+  SECTION("we handle a single element of zero") {
+    std::vector<uint8_t> scalars1(32);
+    scalars = {scalars1.data()};
+    auto fut = multiexponentiate<E>(res, partition_table, scalars, 32, 1);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res == expected);
+  }
+
+  SECTION("we handle a single element of one") {
+    std::vector<uint8_t> scalars1(32);
+    scalars1[0] = 1u;
+    scalars = {scalars1.data()};
+    partition_table[1] = 123u;
+    auto fut = multiexponentiate<E>(res, partition_table, scalars, 32, 1);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    expected = {123u};
+    REQUIRE(res == expected);
+  }
+}
