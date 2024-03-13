f1ae8d296c1eeb51cb718d74602244e427a7005d
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-03-13 10:58:53 -0400

    add bls12-381 primitive benchmarks

---
+++ benchmark/primitives/curve_ops_bls12_381.cu
@@ -0,0 +1,134 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "curve_ops_bls12_381.h"
+
+#include <chrono>
+#include <print>
+
+#include "stats.h"
+#include "sxt/base/num/divide_up.h"
+#include "sxt/base/num/fast_random_number_generator.h"
+#include "sxt/curve_g1/operation/add.h"
+#include "sxt/curve_g1/random/element_p2.h"
+#include "sxt/curve_g1/type/element_p2.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/device_resource.h"
+
+namespace sxt {
+//--------------------------------------------------------------------------------------------------
+// vector_add_impl 
+//--------------------------------------------------------------------------------------------------
+__global__ void vector_add_impl(cg1t::element_p2* __restrict__ vec_ret,
+                                const cg1t::element_p2* __restrict__ vec_a,
+                                unsigned n_elements,
+                                unsigned repetitions) {
+  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+  if (tid < n_elements) {
+    cg1t::element_p2 x = vec_a[tid];
+    auto res = x;
+    for (unsigned i = 0; i < repetitions; ++i) {
+      cg1o::add(res, res, x);
+    }
+    vec_ret[tid] = res;
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// vector_add
+//--------------------------------------------------------------------------------------------------
+void vector_add(cg1t::element_p2* vec_result, const cg1t::element_p2* vec_a, unsigned n_elements, unsigned repetitions, unsigned n_threads) {
+    const unsigned threads_per_block = n_threads;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
+
+    vector_add_impl<<<num_blocks, threads_per_block>>>(vec_result, vec_a, n_elements, repetitions);
+}
+
+
+//--------------------------------------------------------------------------------------------------
+// init_random_array_impl 
+//--------------------------------------------------------------------------------------------------
+__global__ void init_random_array_impl(cg1t::element_p2* __restrict__ rand, unsigned n_elements) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tid < n_elements)
+    {
+        basn::fast_random_number_generator rng{static_cast<uint64_t>(tid + 1),
+                                               static_cast<uint64_t>(n_elements + 1)};
+        cg1rn::generate_random_element(rand[tid], rng);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+// init_random_array
+//--------------------------------------------------------------------------------------------------
+void init_random_array(cg1t::element_p2* rand, unsigned n_elements, unsigned n_threads) {
+    const unsigned threads_per_block = n_threads;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
+
+    init_random_array_impl<<<num_blocks, threads_per_block>>>(rand, n_elements);
+}
+
+//--------------------------------------------------------------------------------------------------
+// curve_ops_bls12_381
+//--------------------------------------------------------------------------------------------------
+void curve_ops_bls12_381(unsigned n_elements, unsigned repetitions, unsigned n_threads, unsigned n_executions) noexcept {
+  std::println("curve_ops_bls12_381 add");
+
+  // Allocate memory for the input and output vectors
+  memmg::managed_array<cg1t::element_p2> a(n_elements, memr::get_device_resource());
+  memmg::managed_array<cg1t::element_p2> ret(n_elements, memr::get_device_resource());
+
+  // Populate the input vectors with random curve elements
+  init_random_array(a.data(), n_elements, n_threads);
+
+  // Warm-up loop
+  vector_add(ret.data(), a.data(), n_elements, repetitions, n_threads);
+  cudaDeviceSynchronize();
+
+  // Report any errors from the warmup loop
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    std::println("CUDA error: {}", cudaGetErrorString(err));
+  }
+
+  // Benchmarking loop
+  std::vector<double> elapsed_times;
+  for (unsigned i = 0; i < n_executions; ++i) {
+    auto start_time = std::chrono::steady_clock::now();
+    vector_add(ret.data(), a.data(), n_elements, repetitions, n_threads);
+    cudaDeviceSynchronize();
+    auto end_time = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    elapsed_times.push_back(duration.count());
+
+    // Report any errors from the benchmark loop
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+      std::println("CUDA error: {}", cudaGetErrorString(err));
+    }
+  }
+
+  // Report data
+  std::println("Final benchmarks over {} executions", n_executions);
+  std::println("...Median : {} ms", sxt::median(elapsed_times));
+  std::println("...Min    : {} ms", sxt::min(elapsed_times));
+  std::println("...Max    : {} ms", sxt::max(elapsed_times));
+  std::println("...Mean   : {} ms", sxt::mean(elapsed_times));
+  std::println("...STD    : {} ms", sxt::std_dev(elapsed_times));
+  std::println("...Performance : {} Giga Curve Additions Per Second", sxt::gmps(elapsed_times, repetitions, n_elements));
+  std::println("");
+}
+}
---
+++ benchmark/primitives/stats.h
@@ -0,0 +1,82 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+namespace sxt {
+//--------------------------------------------------------------------------------------------------
+// min
+//--------------------------------------------------------------------------------------------------
+template <class T> static T min(const std::vector<T>& data) {
+  return *std::min_element(data.begin(), data.end());
+}
+
+//--------------------------------------------------------------------------------------------------
+// max
+//--------------------------------------------------------------------------------------------------
+template <class T> static T max(const std::vector<T>& data) {
+  return *std::max_element(data.begin(), data.end());
+}
+
+//--------------------------------------------------------------------------------------------------
+// mean
+//--------------------------------------------------------------------------------------------------
+template <class T> static T mean(const std::vector<T>& data) {
+  return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+}
+
+//--------------------------------------------------------------------------------------------------
+// std_dev
+//--------------------------------------------------------------------------------------------------
+template <class T> static T std_dev(const std::vector<T>& data) {
+  const T mean = sxt::mean(data);
+  const T variance = std::accumulate(data.begin(), data.end(), 0.0,
+                                     [mean](T accumulator, T val) {
+                                       return accumulator + (val - mean) * (val - mean);
+                                     }) /
+                     data.size();
+  return std::sqrt(variance);
+}
+
+//--------------------------------------------------------------------------------------------------
+// median
+//--------------------------------------------------------------------------------------------------
+template <class T> static T median(const std::vector<T>& data) {
+  std::vector<T> sorted_data = data;
+  std::sort(sorted_data.begin(), sorted_data.end());
+  if (sorted_data.size() % 2 == 0) {
+    return (sorted_data[sorted_data.size() / 2 - 1] + sorted_data[sorted_data.size() / 2]) / 2;
+  } else {
+    return sorted_data[sorted_data.size() / 2];
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// gmps
+//--------------------------------------------------------------------------------------------------
+/**
+ * Giga operations per second. The data vector must be in milliseconds.
+ */
+template <class T>
+static T gmps(const std::vector<T>& data, unsigned repetitions, unsigned n_elements) {
+  const T median = sxt::median(data);
+  return 1.0e-9 * repetitions * n_elements / (1.0e-3 * median);
+}
+} // namespace sxt
