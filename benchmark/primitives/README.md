f755977fd17dbe8a755752dd8238721cd7fa4e9d
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-03-14 15:16:17 -0400

    bench: add bls12-381 primitive benchmarks (PROOF-804) (#106)
    * add bls12-381 primitive benchmarks
    
    * update simple standard deviation and median calculation

---
+++ benchmark/primitives/README.md
@@ -0,0 +1,21 @@
+# Benchmarking for primitives on the GPU
+Currently only supports benchmarks for addition with `bls12-381` curve elements. Creates two vectors of size `<n_elements>` and does vector addition with them `<repetitions>` number of times. Timing results are output in milliseconds.
+
+This benchmark is similar to the Icicle's [example/multiply](https://github.com/ingonyama-zk/icicle/tree/40309329fbf6c5fc7e77d629c72b4a3d28036444/examples/c%2B%2B/multiply), but updated to measure curve addition, field addition, and field multiplication.
+
+## Usages
+`bazel run -c opt //benchmark/primitives:benchmark <curve> <op> <n_elements> <repetitions> <optional - n_threads> <optional - n_executions>`
+
+- `curve`: specifies the curve. Currently only supports `bls12_381`
+- `op`: specifies what operations to target, either `curve` or `field`
+- `n_elements`: specifies the number of elements in the vector
+- `repetitions`: specifies the number of operations executed for each element of the vector
+- `n_threads`: specifies the max number of threads per block
+- `n_executions`: specifies the number of executions used when measuring benchmark statistics
+
+## Example
+Run curve operations using `bls12-381` elements.  
+`bazel run -c opt //benchmark/primitives:benchmark bls12_381 curve 10000 1000 256 10`
+
+Run field operations (addition and multiplication) using `bls12-381` elements.  
+`bazel run -c opt //benchmark/primitives:benchmark bls12_381 field 1000000 1000 256 10`
---
+++ benchmark/primitives/field_ops_bls12_381.cu
@@ -0,0 +1,173 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "field_ops_bls12_381.h"
+
+#include <chrono>
+#include <format>
+#include <print>
+
+#include "stats.h"
+#include "sxt/base/num/divide_up.h"
+#include "sxt/base/num/fast_random_number_generator.h"
+#include "sxt/field12/operation/add.h"
+#include "sxt/field12/operation/mul.h"
+#include "sxt/field12/random/element.h"
+#include "sxt/field12/type/element.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/device_resource.h"
+
+namespace sxt {
+//--------------------------------------------------------------------------------------------------
+// vector_add_impl 
+//--------------------------------------------------------------------------------------------------
+template <typename T>
+__global__ void vector_add_impl(T* __restrict__ vec_ret,
+                                const T* __restrict__ vec_a,
+                                unsigned n_elements,
+                                unsigned repetitions) {
+  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+  if (tid < n_elements) {
+    T x = vec_a[tid];
+    auto res = x;
+    for (unsigned i = 0; i < repetitions; ++i) {
+      f12o::add(res, res, x);
+    }
+    vec_ret[tid] = res;
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// vector_mul_impl 
+//--------------------------------------------------------------------------------------------------
+template <typename T>
+__global__ void vector_mul_impl(T* __restrict__ vec_ret,
+                                const T* __restrict__ vec_a,
+                                unsigned n_elements,
+                                unsigned repetitions) {
+  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+  if (tid < n_elements) {
+    T x = vec_a[tid];
+    auto res = x;
+    for (unsigned i = 0; i < repetitions; ++i) {
+      f12o::mul(res, res, x);
+    }
+    vec_ret[tid] = res;
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// vector_add 
+//--------------------------------------------------------------------------------------------------
+void vector_add(f12t::element* vec_result, const f12t::element* vec_a, unsigned n_elements, unsigned repetitions, unsigned n_threads) {
+    const unsigned threads_per_block = n_threads;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
+
+    vector_add_impl<<<num_blocks, threads_per_block>>>(vec_result, vec_a, n_elements, repetitions);
+}
+
+//--------------------------------------------------------------------------------------------------
+// vector_mul
+//--------------------------------------------------------------------------------------------------
+void vector_mul(f12t::element* vec_result, const f12t::element* vec_a, unsigned n_elements, unsigned repetitions, unsigned n_threads) {
+    const unsigned threads_per_block = n_threads;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
+
+    vector_mul_impl<<<num_blocks, threads_per_block>>>(vec_result, vec_a, n_elements, repetitions);
+}
+
+//--------------------------------------------------------------------------------------------------
+// init_random_array_impl 
+//--------------------------------------------------------------------------------------------------
+__global__ void init_random_array_impl(f12t::element* __restrict__ rand, unsigned n_elements) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tid < n_elements)
+    {
+        basn::fast_random_number_generator rng{static_cast<uint64_t>(tid + 1),
+                                               static_cast<uint64_t>(n_elements + 1)};
+        f12rn::generate_random_element(rand[tid], rng);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+// init_random_array
+//--------------------------------------------------------------------------------------------------
+void init_random_array(f12t::element* rand, unsigned n_elements, unsigned n_threads) {
+    const unsigned threads_per_block = n_threads;
+    const unsigned num_blocks = basn::divide_up(n_elements, threads_per_block);
+
+    init_random_array_impl<<<num_blocks, threads_per_block>>>(rand, n_elements);
+}
+
+//--------------------------------------------------------------------------------------------------
+// field_ops_bls12_381
+//--------------------------------------------------------------------------------------------------
+void field_ops_bls12_381(std::string op, unsigned n_elements, unsigned repetitions, unsigned n_threads, unsigned n_executions) noexcept {
+  std::println("field_ops_bls12_381 for {}", op);
+
+  // Allocate memory for the input and output vectors
+  memmg::managed_array<f12t::element> a(n_elements, memr::get_device_resource());
+  memmg::managed_array<f12t::element> ret(n_elements, memr::get_device_resource());
+
+  // Populate the input vectors with random curve elements
+  init_random_array(a.data(), n_elements, n_threads);
+
+  // Warm-up loop
+  if (op == "add") {
+    vector_add(ret.data(), a.data(), n_elements, repetitions, n_threads);
+  } else {
+    vector_mul(ret.data(), a.data(), n_elements, repetitions, n_threads);
+  }
+  cudaDeviceSynchronize();
+
+  // Report any errors from the warm up loop
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    std::println("CUDA error: {}", cudaGetErrorString(err));
+  }
+
+  // Benchmarking loop
+  std::vector<double> elapsed_times;
+  for (unsigned i = 0; i < n_executions; ++i) {
+    auto start_time = std::chrono::steady_clock::now();
+    if (op == "add") {
+      vector_add(ret.data(), a.data(), n_elements, repetitions, n_threads);
+    } else {
+      vector_mul(ret.data(), a.data(), n_elements, repetitions, n_threads);
+    }
+    cudaDeviceSynchronize();
+    auto end_time = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    elapsed_times.push_back(duration.count());
+
+    // Report any errors from the benchmark loop
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+      std::println("CUDA error: {}", cudaGetErrorString(err));
+    }
+  }
+
+  // Report data
+  std::println("Final benchmarks over {} executions", n_executions);
+  std::println("...Median : {} ms", sxt::median(elapsed_times));
+  std::println("...Min    : {} ms", sxt::min(elapsed_times));
+  std::println("...Max    : {} ms", sxt::max(elapsed_times));
+  std::println("...Mean   : {} ms", sxt::mean(elapsed_times));
+  std::println("...STD    : {} ms", sxt::std_dev(elapsed_times));
+  std::println("...Performance : {} Giga Field {} Per Second", sxt::gmps(elapsed_times, repetitions, n_elements), op);
+  std::println("");
+}
+}
