828349be0ea11a7fa66c334d9df2dc9463d9b57c
rnburn <ryan.burn@gmail.com>
2024-03-27 21:13:54 -0700

    fill in generator accessor

--- sxt/multiexp/base/generator_accessor.h
+++ sxt/multiexp/base/generator_accessor.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
+#include "sxt/execution/async/future_fwd.h"
 
 namespace sxt::mtxb {
 //--------------------------------------------------------------------------------------------------
@@ -11,5 +13,7 @@ class generator_accessor {
  public:
    virtual ~generator_accessor() noexcept = default;
 
+   virtual xena::future<> copy_precomputed_sums_to_device(basct::span<T> dest,
+                                                          unsigned first) const noexcept = 0;
 };
 } // namespace sxt::mtxb
--- sxt/multiexp/base/generator_accessor.t.cc
+++
@@ -1,6 +0,0 @@
-#include "sxt/multiexp/base/generator_accessor.h"
-
-#include "sxt/base/test/unit_test.h"
-using namespace sxt;
-
-TEST_CASE("todo") {}
