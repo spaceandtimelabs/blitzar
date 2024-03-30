0ff80e6603add71eaf3d61961ff7aed4ba93717e
rnburn <ryan.burn@gmail.com>
2024-03-29 20:01:54 -0700

    fill in accessor

--- sxt/multiexp/pippenger2/in_memory_partition_table_accessor.h
+++ sxt/multiexp/pippenger2/in_memory_partition_table_accessor.h
@@ -5,9 +5,11 @@
 #include <fstream>
 #include <string_view>
 
+#include "sxt/base/error/assert.h"
 #include "sxt/base/error/panic.h"
-#include "sxt/multiexp/pippenger2/partition_table_accessor.h"
 #include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/pinned_resource.h"
+#include "sxt/multiexp/pippenger2/partition_table_accessor.h"
 
 namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
@@ -16,7 +18,8 @@ namespace sxt::mtxpp2 {
 template <bascrv::element T>
 class in_memory_partition_table_accessor final : public partition_table_accessor<T> {
  public:
-   explicit in_memory_partition_table_accessor(std::string_view filename) noexcept {
+   explicit in_memory_partition_table_accessor(std::string_view filename) noexcept
+       : table_{memr::get_pinned_resource()} {
      std::ifstream in{filename, std::ios::binary};
      if (!in.good()) {
        baser::panic("failed to open {}: {}", filename, std::strerror(errno));
@@ -25,8 +28,9 @@ class in_memory_partition_table_accessor final : public partition_table_accessor
      in.seekg(0, std::ios::end);
      auto size = in.tellg() - pos;
      in.seekg(pos);
-     (void)size;
-     (void)filename;
+     SXT_RELEASE_ASSERT(size % sizeof(T) == 0);
+     table_.resize(size / sizeof(T));
+     in.read(reinterpret_cast<char*>(table_.data()), size);
    }
 
    void async_copy_precomputed_sums_to_device(basct::span<T> dest, bast::raw_stream_t stream,
