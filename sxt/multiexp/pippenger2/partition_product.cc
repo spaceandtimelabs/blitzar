361bc0a6142e4a22ebfdaa9c2e242b04bb6305f4
rnburn <ryan.burn@gmail.com>
2024-04-03 21:00:42 -0700

    add stub for partition product

---
+++ sxt/multiexp/pippenger2/partition_product.cc
@@ -0,0 +1,1 @@
+#include "sxt/multiexp/pippenger2/partition_product.h"
---
+++ sxt/multiexp/pippenger2/partition_product.t.cc
@@ -0,0 +1,5 @@
+#include "sxt/multiexp/pippenger2/partition_product.h"
+
+#include "sxt/base/test/unit_test.h"
+
+TEST_CASE("todo") {}
