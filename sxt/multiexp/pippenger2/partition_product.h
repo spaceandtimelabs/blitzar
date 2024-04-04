b377956a7b5d44dc170d4708fbed1b7634976840
rnburn <ryan.burn@gmail.com>
2024-04-03 22:38:14 -0700

    fill in partition product

--- sxt/multiexp/pippenger2/partition_product.h
+++ sxt/multiexp/pippenger2/partition_product.h
@@ -4,9 +4,12 @@
 
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
+#include "sxt/base/device/stream.h"
+#include "sxt/base/device/memory_utility.h"
 #include "sxt/base/error/assert.h"
 #include "sxt/base/num/divide_up.h"
 #include "sxt/execution/async/future.h"
+#include "sxt/execution/device/synchronization.h"
 #include "sxt/memory/management/managed_array.h"
 #include "sxt/memory/resource/device_resource.h"
 #include "sxt/multiexp/pippenger2/partition_table_accessor.h"
@@ -21,15 +24,21 @@ xena::future<> partition_product(basct::span<T> products,
                                  basct::cspan<uint8_t> scalars, unsigned offset) noexcept {
   auto num_products = products.size();
   auto n = scalars.size() * 8u / num_products;
-  auto num_partition_tables = basn::divide_up(n, 16u);
+  auto num_partitions = basn::divide_up(n, 16u);
   auto num_table_entries = 1u << 16u;
   SXT_DEBUG_ASSERT(
       offset % 16u == 0
   );
 
+  // scalars_dev
   memmg::managed_array<uint8_t> scalars_dev{scalars.size(), memr::get_device_resource()};
+  auto scalars_fut = [&]() noexcept -> xena::future<> {
+    basdv::stream stream;
+    basdv::async_copy_host_to_device(scalars_dev, scalars, stream);
+    co_await xendv::await_stream(stream);
+  };
 
-  memmg::managed_array<T> partition_table{num_partition_tables * num_table_entries,
+  memmg::managed_array<T> partition_table{num_partitions * num_table_entries,
                                           memr::get_device_resource()};
   (void)products;
   (void)accessor;
