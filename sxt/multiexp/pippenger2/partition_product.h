4b68d75e0eb201a4ed20883410752843b783b2c1
rnburn <ryan.burn@gmail.com>
2024-04-03 21:05:34 -0700

    add stub for partition product function

--- sxt/multiexp/pippenger2/partition_product.h
+++ sxt/multiexp/pippenger2/partition_product.h
@@ -1,4 +1,24 @@
 #pragma once
 
+#include <cstdint>
+
+#include "sxt/base/container/span.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/execution/async/future.h"
+#include "sxt/multiexp/pippenger2/partition_table_accessor.h"
+
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// partition_product 
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element T>
+xena::future<> partition_product(basct::span<T> products,
+                                 const partition_table_accessor<T>& accessor,
+                                 basct::cspan<uint8_t> scalars, unsigned offset) noexcept {
+  (void)products;
+  (void)accessor;
+  (void)scalars;
+  (void)offset;
+  return {};
+}
 } // namespace sxt::mtxpp2
