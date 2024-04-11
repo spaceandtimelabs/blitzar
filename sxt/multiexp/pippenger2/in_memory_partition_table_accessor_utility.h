99e27ad41413d4bbf7041b252b18a8517cf439ea
rnburn <ryan.burn@gmail.com>
2024-04-11 11:42:49 -0700

    fill in partition table accessor

--- sxt/multiexp/pippenger2/in_memory_partition_table_accessor_utility.h
+++ sxt/multiexp/pippenger2/in_memory_partition_table_accessor_utility.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include <memory>
+
+#include "sxt/base/container/span.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/multiexp/pippenger2/in_memory_partition_table_accessor.h"
+
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// make_in_memory_partition_table_accessor
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element T>
+std::unique_ptr<partition_table_accessor<T>>
+make_in_memory_partition_table_accessor(basct::cspan<T> generators) noexcept {
+  (void)generators;
+  return {};
+}
 } // namespace sxt:mtxpp2
