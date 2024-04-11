5c354c5309f6f7c4d0e782e6d506bb76f3410f73
rnburn <ryan.burn@gmail.com>
2024-04-11 12:02:01 -0700

    make in memory accessor

--- sxt/multiexp/pippenger2/in_memory_partition_table_accessor_utility.h
+++ sxt/multiexp/pippenger2/in_memory_partition_table_accessor_utility.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
+#include "sxt/base/num/divide_up.h"
 #include "sxt/multiexp/pippenger2/in_memory_partition_table_accessor.h"
 
 namespace sxt::mtxpp2 {
@@ -13,6 +15,15 @@ namespace sxt::mtxpp2 {
 template <bascrv::element T>
 std::unique_ptr<partition_table_accessor<T>>
 make_in_memory_partition_table_accessor(basct::cspan<T> generators) noexcept {
+  auto n = generators.size();
+  std::vector<T> generators_data;
+  if (n % 16 != 0) {
+    n = basn::divide_up(n, size_t{16}) * 16u;
+    generators_data.resize(n);
+    auto iter = std::copy(generators.begin(), generators.end(), generators_data.begin());
+    std::fill(iter, generators_data.end(), T::identity());
+    generators = generators_data;
+  }
   (void)generators;
   return {};
 }
