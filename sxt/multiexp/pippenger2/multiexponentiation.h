94c6702cf0de4cff0ed03fe1ce907f74cd6b333b
rnburn <ryan.burn@gmail.com>
2024-04-11 10:50:56 -0700

    fill in multiexponentiation

--- sxt/multiexp/pippenger2/multiexponentiation.h
+++ sxt/multiexp/pippenger2/multiexponentiation.h
@@ -18,7 +18,12 @@
 
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
+#include "sxt/base/error/assert.h"
 #include "sxt/execution/async/future.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/async_device_resource.h"
+#include "sxt/memory/resource/device_resource.h"
+#include "sxt/multiexp/pippenger2/partition_product.h"
 #include "sxt/multiexp/pippenger2/partition_table_accessor.h"
 
 namespace sxt::mtxpp2 {
@@ -27,10 +32,29 @@ namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
 template <bascrv::element T>
 xena::future<> multiexponentiate(basct::span<T> res, const partition_table_accessor<T>& accessor,
+                                 unsigned element_num_bytes,
                                  basct::cspan<uint8_t> scalars) noexcept {
-  (void)res;
-  (void)accessor;
-  (void)scalars;
-  return {};
+  auto num_outputs = res.size();
+  auto num_products = num_outputs * element_num_bytes * 8u;
+  SXT_DEBUG_ASSERT(
+      // clang-format off
+      scalars.size() % (num_outputs * element_num_bytes) == 0
+      // clang-format on
+  );
+  
+  // compute bitwise products
+  memmg::managed_array<T> products(num_products, memr::get_device_resource());
+  co_await partition_product<T>(products, accessor, scalars, 0);
+
+  // reduce products
+  basdv::stream stream;
+  memr::async_device_resource resource{stream};
+  memmg::managed_array<T> res_dev{num_outputs, stream};
+  reduce_products<T>(res_dev, stream, products);
+
+  // copy result
+  products.reset();
+  basdv::async_copy_device_to_host(res, stream, res_dev);
+  co_await xendv::await_stream(stream);
 }
 } // namespace sxt::mtxpp2
