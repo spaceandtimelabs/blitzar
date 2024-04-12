974300cef183a2b23cd401460445bfa4eefdd977
rnburn <ryan.burn@gmail.com>
2024-04-12 10:14:10 -0700

    fill in testing

--- sxt/multiexp/pippenger2/multiexponentiation.t.cc
+++ sxt/multiexp/pippenger2/multiexponentiation.t.cc
@@ -30,7 +30,7 @@ using namespace sxt::mtxpp2;
 TEST_CASE("we can compute multiexponentiations using a precomputed table of partition sums") {
   using E = bascrv::element97;
 
-  std::vector<E> generators(10);
+  std::vector<E> generators(32);
   std::mt19937 rng{0};
   for (auto& g : generators) {
     g = std::uniform_int_distribution<unsigned>{0, 96}(rng);
@@ -41,14 +41,14 @@ TEST_CASE("we can compute multiexponentiations using a precomputed table of part
   std::vector<uint8_t> scalars(1);
   std::vector<E> res(1);
 
-  SECTION("we can compute a sum with a zero scalar") {
+  SECTION("we can compute a multiexponentiation with a zero scalar") {
     auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     REQUIRE(res[0] == E::identity());
   }
 
-  SECTION("we can compute a sum sum with a scalar of one") {
+  SECTION("we can compute a multiexponentiation multiexponentiation with a scalar of one") {
     scalars[0] = 1;
     auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
     xens::get_scheduler().run();
@@ -56,7 +56,7 @@ TEST_CASE("we can compute multiexponentiations using a precomputed table of part
     REQUIRE(res[0] == generators[0]);
   }
 
-  SECTION("we can compute a sum with a scalar of two") {
+  SECTION("we can compute a multiexponentiation with a scalar of two") {
     scalars[0] = 2;
     auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
     xens::get_scheduler().run();
@@ -64,11 +64,31 @@ TEST_CASE("we can compute multiexponentiations using a precomputed table of part
     REQUIRE(res[0] == 2u * generators[0].value);
   }
 
-  SECTION("we can compute a sum with a scalar of three") {
+  SECTION("we can compute a multiexponentiation with a scalar of three") {
     scalars[0] = 3;
     auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     REQUIRE(res[0] == 3u * generators[0].value);
+  }
+
+  SECTION("we can compute a multiexponentiation with two scalars") {
+    scalars.resize(2);
+    scalars[0] = 1;
+    scalars[1] = 1;
+    auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == generators[0].value + generators[1].value);
+  }
+
+  SECTION("we can compute a multiexponentiation with more than 16 scalars") {
+    scalars.resize(17);
+    scalars[0] = 1;
+    scalars[16] = 1;
+    auto fut = multiexponentiate<E>(res, *accessor, 1, scalars);
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    REQUIRE(res[0] == generators[0].value + generators[16].value);
   }
 }
