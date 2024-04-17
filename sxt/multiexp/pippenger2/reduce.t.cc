3e92dab5f9b1f1bef40c026c121273ea1dcc0a5b
rnburn <ryan.burn@gmail.com>
2024-04-16 20:26:47 -0700

    add reduction component

---
+++ sxt/multiexp/pippenger2/reduce.t.cc
@@ -0,0 +1,58 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2024-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "sxt/multiexp/pippenger2/reduce.h"
+
+#include <vector>
+
+#include "sxt/base/curve/example_element.h"
+#include "sxt/base/device/synchronization.h"
+#include "sxt/base/test/unit_test.h"
+#include "sxt/memory/resource/managed_device_resource.h"
+
+using namespace sxt;
+using namespace sxt::mtxpp2;
+
+TEST_CASE("we can reduce products") {
+  using E = bascrv::element97;
+
+  std::pmr::vector<E> outputs{memr::get_managed_device_resource()};
+  std::pmr::vector<E> products{memr::get_managed_device_resource()};
+  basdv::stream stream;
+
+  std::pmr::vector<E> expected;
+
+  SECTION("we handle a single element reduction") {
+    outputs.resize(1);
+    products.resize(1);
+    products[0] = 123u;
+    reduce_products<E>(outputs, stream, products);
+    basdv::synchronize_stream(stream);
+    expected = {123u};
+    REQUIRE(outputs == expected);
+  }
+
+  SECTION("we handle a reduction with two elements") {
+    outputs.resize(1);
+    products.resize(2);
+    products[0] = 123u;
+    products[1] = 456u;
+    reduce_products<E>(outputs, stream, products);
+    basdv::synchronize_stream(stream);
+    expected = {123u + 2u * 456u};
+    REQUIRE(outputs == expected);
+  }
+}
