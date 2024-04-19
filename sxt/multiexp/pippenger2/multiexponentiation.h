8e2f80aee50223f622651f4bf740ad58470c2e81
rnburn <ryan.burn@gmail.com>
2024-04-18 21:47:33 -0700

    chunking

--- sxt/multiexp/pippenger2/multiexponentiation.h
+++ sxt/multiexp/pippenger2/multiexponentiation.h
@@ -16,6 +16,8 @@
  */
 #pragma once
 
+#include <iterator>
+
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/device/property.h"
@@ -31,6 +33,37 @@
 #include "sxt/multiexp/pippenger2/reduce.h"
 
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// multiexponentiate_no_chunks 
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element T>
+xena::future<>
+multiexponentiate_no_chunks(basct::span<T> res, const partition_table_accessor<T>& accessor,
+                            unsigned element_num_bytes, basct::cspan<uint8_t> scalars) noexcept {
+  auto num_outputs = res.size();
+  auto num_products = num_outputs * element_num_bytes * 8u;
+  SXT_DEBUG_ASSERT(
+      // clang-format off
+      scalars.size() % (num_outputs * element_num_bytes) == 0
+      // clang-format on
+  );
+
+  // compute bitwise products
+  memmg::managed_array<T> products(num_products, memr::get_device_resource());
+  co_await partition_product<T>(products, accessor, scalars, 0);
+
+  // reduce products
+  basdv::stream stream;
+  memr::async_device_resource resource{stream};
+  memmg::managed_array<T> res_dev{num_outputs, &resource};
+  reduce_products<T>(res_dev, stream, products);
+  products.reset();
+
+  // copy result
+  basdv::async_copy_device_to_host(res, res_dev, stream);
+  co_await xendv::await_stream(stream);
+}
+
 //--------------------------------------------------------------------------------------------------
 // multiexponentiate
 //--------------------------------------------------------------------------------------------------
@@ -53,7 +86,12 @@ xena::future<> multiexponentiate(basct::span<T> res, const partition_table_acces
       // clang-format on
   );
 
-  auto [chunk_first, chunk_last] = basit::split(basit::index_range{0, n}, basdv::get_num_devices());
+  auto [chunk_first, chunk_last] =
+      basit::split(basit::index_range{0, n}.chunk_multiple(16), basdv::get_num_devices());
+  if (std::distance(chunk_first, chunk_last) == 1) {
+    multiexponentiate_no_chunks(res, accessor, element_num_bytes, scalars);
+    co_return;
+  }
   (void)n;
   (void)chunk_first;
   (void)chunk_last;
