c4cff166a1abe0f55f9d681b2c8c63491bc5a99c
rnburn <ryan.burn@gmail.com>
2024-04-22 21:55:49 -0700

    fill in combination

--- sxt/multiexp/pippenger2/combination.t.cc
+++ sxt/multiexp/pippenger2/combination.t.cc
@@ -1,5 +1,12 @@
 #include "sxt/multiexp/pippenger2/combination.h"
 
+#include "sxt/base/curve/example_element.h"
+#include "sxt/base/device/stream.h"
 #include "sxt/base/test/unit_test.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/managed_device_resource.h"
+using namespace sxt;
+using namespace sxt::mtxpp2;
 
-TEST_CASE("todo") {}
+TEST_CASE("todo") {
+}
