d2671546635eb34910151a0519e4bfe9f149b238
rnburn <ryan.burn@gmail.com>
2024-05-01 22:37:26 -0700

    fill in curve id utility

--- sxt/cbindings/base/curve_id_utility.h
+++ sxt/cbindings/base/curve_id_utility.h
@@ -1,4 +1,16 @@
 #pragma once
 
+#include "sxt/cbindings/base/curve_id.h"
+#include "sxt/curve21/type/element_p3.h"
+#include "sxt/curve_g1/type/element_p2.h"
+#include "sxt/curve_bng1/type/element_p2.h"
+
 namespace sxt::cbnb {
+//--------------------------------------------------------------------------------------------------
+// switch_curve_type
+//--------------------------------------------------------------------------------------------------
+template <class F> void switch_curve_type(curve_id_t curve, F f) {
+  (void)curve;
+  (void)f;
+}
 } // namespace sxt::cbnb
