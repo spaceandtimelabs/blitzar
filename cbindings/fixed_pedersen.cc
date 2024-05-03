eac3417ee4f8fa0e43ad2da9cbbe7503d1e4447f
rnburn <ryan.burn@gmail.com>
2024-05-03 10:13:25 -0700

    fill in c api

--- cbindings/fixed_pedersen.cc
+++ cbindings/fixed_pedersen.cc
@@ -1,1 +1,13 @@
 #include "cbindings/fixed_pedersen.h"
+
+struct multiexp_handle* sxt_multiexp_handle_new(unsigned curve_id, const void* generators,
+                                                unsigned n) {
+  (void)curve_id;
+  (void)generators;
+  (void)n;
+  return nullptr;
+}
+
+void sxt_multiexp_handle_free(struct multiexp_handle* handle) {
+  (void)handle;
+}
