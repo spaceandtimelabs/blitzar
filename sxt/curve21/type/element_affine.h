5d8f7b16e20417d73e2bf8a97c09e43919a3a962
rnburn <ryan.burn@gmail.com>
2024-06-04 20:13:07 -0700

    fill in affine point

--- sxt/curve21/type/element_affine.h
+++ sxt/curve21/type/element_affine.h
@@ -1,4 +1,13 @@
 #pragma once
 
+#include "sxt/field51/type/element.h"
+
 namespace sxt::c21t {
+//--------------------------------------------------------------------------------------------------
+// element_affine
+//--------------------------------------------------------------------------------------------------
+struct element_affine {
+  f51t::element X;
+  f51t::element Y;
+};
 } // namespace sxt::c21t
