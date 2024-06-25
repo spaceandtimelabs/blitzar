34520fb8b3c21e6970f01e975ca4fd330cb7e26b
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-06-25 14:57:32 -0400

    chore: remove CUDA_CALLABLE from constexpr functions

--- sxt/field12/type/element.h
+++ sxt/field12/type/element.h
@@ -31,22 +31,19 @@ public:
 
   element() noexcept = default;
 
-  CUDA_CALLABLE constexpr element(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4, uint64_t x5,
-                                  uint64_t x6) noexcept
+  constexpr element(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4, uint64_t x5,
+                    uint64_t x6) noexcept
       : data_{x1, x2, x3, x4, x5, x6} {}
 
-  CUDA_CALLABLE constexpr element(const uint64_t x[6]) noexcept
-      : data_{x[0], x[1], x[2], x[3], x[4], x[5]} {}
+  constexpr element(const uint64_t x[6]) noexcept : data_{x[0], x[1], x[2], x[3], x[4], x[5]} {}
 
-  CUDA_CALLABLE constexpr const uint64_t& operator[](int index) const noexcept {
-    return data_[index];
-  }
+  constexpr const uint64_t& operator[](int index) const noexcept { return data_[index]; }
 
-  CUDA_CALLABLE constexpr uint64_t& operator[](int index) noexcept { return data_[index]; }
+  constexpr uint64_t& operator[](int index) noexcept { return data_[index]; }
 
-  CUDA_CALLABLE constexpr const uint64_t* data() const noexcept { return data_; }
+  constexpr const uint64_t* data() const noexcept { return data_; }
 
-  CUDA_CALLABLE constexpr uint64_t* data() noexcept { return data_; }
+  constexpr uint64_t* data() noexcept { return data_; }
 
 private:
   uint64_t data_[num_limbs_v];
