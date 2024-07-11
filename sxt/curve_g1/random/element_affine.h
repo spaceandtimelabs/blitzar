db722fc9baa38d5a0f6e4793866e48219fec4b22
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-07-11 13:21:13 -0400

    chore: update Pedersen test component to use random elements in bn254 and bls12-381 curve tests (PROOF-891) (#152)
    * feat: add random affine element generation
    
    * chore: update pedersen tests to use random element generator

---
+++ sxt/curve_g1/random/element_affine.h
@@ -0,0 +1,38 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2023-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cstring>
+
+#include "sxt/base/macro/cuda_callable.h"
+#include "sxt/curve_g1/random/element_p2.h"
+#include "sxt/curve_g1/type/conversion_utility.h"
+#include "sxt/curve_g1/type/element_affine.h"
+#include "sxt/curve_g1/type/element_p2.h"
+
+namespace sxt::cg1rn {
+//--------------------------------------------------------------------------------------------------
+// generate_random_element
+//--------------------------------------------------------------------------------------------------
+CUDA_CALLABLE
+inline void generate_random_element(cg1t::element_affine& a,
+                                    basn::fast_random_number_generator& rng) noexcept {
+  cg1t::element_p2 p;
+  generate_random_element(p, rng);
+  cg1t::to_element_affine(a, p);
+}
+} // namespace sxt::cg1rn
