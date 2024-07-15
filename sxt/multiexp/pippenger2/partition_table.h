9e86874f4c7afb833612031e2532c492f0cd936c
rnburn <ryan.burn@gmail.com>
2024-07-15 09:51:37 -0700

    rework partition table code

--- sxt/multiexp/pippenger2/partition_table.h
+++ sxt/multiexp/pippenger2/partition_table.h
@@ -66,6 +66,48 @@ CUDA_CALLABLE void compute_partition_table_slice(U* __restrict__ sums,
   }
 }
 
+template <class U, bascrv::element T>
+  requires requires(const U& u, const T& e) {
+    static_cast<U>(e);
+    T{u};
+  }
+CUDA_CALLABLE void compute_partition_table_slice(U* __restrict__ sums, unsigned window_width,
+                                                 const T* __restrict__ generators) noexcept {
+  (void)sums;
+  (void)window_width;
+  (void)generators;
+#if 0
+  sums[0] = static_cast<U>(T::identity());
+
+  // single entry sums
+  for (unsigned i = 0; i < 16; ++i) {
+    sums[1 << i] = static_cast<U>(generators[i]);
+  }
+
+  // multi-entry sums
+  for (unsigned k = 2; k <= 16; ++k) {
+    unsigned partition = std::numeric_limits<uint16_t>::max() >> (16u - k);
+    auto partition_last = partition << (16u - k);
+
+    // iterate over all possible permutations with k bits set to 1
+    // until we reach partition_last
+    while (true) {
+      // compute the k bit sum from a (k-1) bit sum and a 1 bit sum
+      auto rest = partition & (partition - 1u);
+      auto t = partition ^ rest;
+      T sum{sums[rest]};
+      T e{sums[t]};
+      add_inplace(sum, e);
+      sums[partition] = static_cast<U>(sum);
+      if (partition == partition_last) {
+        break;
+      }
+      partition = basbt::next_permutation(partition);
+    }
+  }
+#endif
+}
+
 //--------------------------------------------------------------------------------------------------
 // compute_partition_table
 //--------------------------------------------------------------------------------------------------
@@ -103,10 +145,16 @@ void compute_partition_table(basct::span<U> sums, unsigned window_width,
   auto table_size = 1u << window_width;
   SXT_DEBUG_ASSERT(
       // clang-format off
-     sums.size() == partition_table_size_v * generators.size() / window_width &&
+     sums.size() == table_size * generators.size() / window_width &&
      generators.size() % window_width == 0
       // clang-format on
   );
+  auto n = generators.size() / window_width;
+  for (unsigned i = 0; i < n; ++i) {
+    auto sums_slice = sums.subspan(i * table_size, table_size);
+    auto generators_slice = generators.subspan(i * window_width, window_width);
+    compute_partition_table_slice(sums_slice.data(), window_width, generators_slice.data());
+  }
 }
 
 template <bascrv::element T>
