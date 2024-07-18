9eb4eee94137037f1b5817b1dd48fbf3d39f907e
Ryan Burn <ryan.burn@gmail.com>
2024-07-18 11:02:11 -0700

    fix: fix size calculation in bindings (PROOF-877) (#156)
    * fix c bindingins
    
    * refactor
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Jacob Trombetta <jacob.trombetta@spaceandtime.io>

--- sxt/cbindings/backend/gpu_backend.cc
+++ sxt/cbindings/backend/gpu_backend.cc
@@ -20,7 +20,7 @@
 #include <vector>
 
 #include "sxt/base/error/assert.h"
-#include "sxt/base/num/round_up.h"
+#include "sxt/base/num/divide_up.h"
 #include "sxt/cbindings/base/curve_id_utility.h"
 #include "sxt/curve21/operation/add.h"
 #include "sxt/curve21/operation/double.h"
@@ -221,18 +221,18 @@ void gpu_backend::fixed_multiexponentiation(void* res, cbnb::curve_id_t curve_id
                                             const mtxpp2::partition_table_accessor_base& accessor,
                                             const unsigned* output_bit_table, unsigned num_outputs,
                                             unsigned n, const uint8_t* scalars) const noexcept {
-  cbnb::switch_curve_type(
-      curve_id, [&]<class U, class T>(std::type_identity<U>, std::type_identity<T>) noexcept {
-        basct::span<T> res_span{static_cast<T*>(res), num_outputs};
-        basct::cspan<unsigned> output_bit_table_span{output_bit_table, num_outputs};
-        auto output_num_bytes =
-            basn::round_up(std::accumulate(output_bit_table, output_bit_table + num_outputs, 0), 8);
-        basct::cspan<uint8_t> scalars_span{scalars, output_num_bytes * n};
-        auto fut = mtxpp2::async_multiexponentiate<T>(
-            res_span, static_cast<const mtxpp2::partition_table_accessor<U>&>(accessor),
-            output_bit_table_span, scalars_span);
-        xens::get_scheduler().run();
-      });
+  cbnb::switch_curve_type(curve_id, [&]<class U, class T>(std::type_identity<U>,
+                                                          std::type_identity<T>) noexcept {
+    basct::span<T> res_span{static_cast<T*>(res), num_outputs};
+    basct::cspan<unsigned> output_bit_table_span{output_bit_table, num_outputs};
+    auto output_num_bytes =
+        basn::divide_up(std::accumulate(output_bit_table, output_bit_table + num_outputs, 0), 8);
+    basct::cspan<uint8_t> scalars_span{scalars, output_num_bytes * n};
+    auto fut = mtxpp2::async_multiexponentiate<T>(
+        res_span, static_cast<const mtxpp2::partition_table_accessor<U>&>(accessor),
+        output_bit_table_span, scalars_span);
+    xens::get_scheduler().run();
+  });
 }
 
 //--------------------------------------------------------------------------------------------------
