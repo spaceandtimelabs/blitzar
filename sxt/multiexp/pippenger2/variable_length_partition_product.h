27c4b47f1eefb12455c82e11c4f0550d2918786f
rnburn <ryan.burn@gmail.com>
2024-07-25 19:22:22 -0700

    fill in varying length partition product

--- sxt/multiexp/pippenger2/variable_length_partition_product.h
+++ sxt/multiexp/pippenger2/variable_length_partition_product.h
@@ -19,10 +19,14 @@ template <bascrv::element T, class U>
   requires std::constructible_from<T, U>
 xena::future<> async_partition_product(basct::span<T> products,
                                        const partition_table_accessor<U>& accessor,
-                                       basct::cspan<uint8_t> scalars, unsigned offset) noexcept {
+                                       basct::cspan<uint8_t> scalars, 
+                                       basct::cspan<unsigned> lengths,
+                                       unsigned offset) noexcept {
   (void)products;
   (void)accessor;
   (void)scalars;
+  (void)lengths;
   (void)offset;
+  return {};
 }
 } // namespace sxt::mtxpp2
