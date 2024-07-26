66a4791b3eb12dfe94ad697284d5fc00a8516b1e
rnburn <ryan.burn@gmail.com>
2024-07-25 19:20:28 -0700

    add stub for new function

--- sxt/multiexp/pippenger2/variable_length_partition_product.h
+++ sxt/multiexp/pippenger2/variable_length_partition_product.h
@@ -1,4 +1,28 @@
 #pragma once
 
+#include <concepts>
+
+#include "sxt/base/container/span.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/execution/async/coroutine.h"
+#include "sxt/multiexp/pippenger2/partition_table_accessor.h"
+
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// async_partition_product
+//--------------------------------------------------------------------------------------------------
+/**
+ * Compute the multiproduct for the bits of an array of scalars using an accessor to
+ * precomputed sums for each group of generators.
+ */
+template <bascrv::element T, class U>
+  requires std::constructible_from<T, U>
+xena::future<> async_partition_product(basct::span<T> products,
+                                       const partition_table_accessor<U>& accessor,
+                                       basct::cspan<uint8_t> scalars, unsigned offset) noexcept {
+  (void)products;
+  (void)accessor;
+  (void)scalars;
+  (void)offset;
+}
 } // namespace sxt::mtxpp2
