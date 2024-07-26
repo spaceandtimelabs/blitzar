18f43f93a6b6abc8bac2bcecb762effce0850021
rnburn <ryan.burn@gmail.com>
2024-07-25 20:06:08 -0700

    add function stubs

--- sxt/multiexp/pippenger2/variable_length_computation.cc
+++ sxt/multiexp/pippenger2/variable_length_computation.cc
@@ -1,1 +1,13 @@
 #include "sxt/multiexp/pippenger2/variable_length_computation.h"
+
+namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// compute_offset_table 
+//--------------------------------------------------------------------------------------------------
+void compute_offset_table(basct::span<unsigned> offset_table, basct::cspan<unsigned> bit_table,
+                          basct::cspan<unsigned> length_table) noexcept {
+  (void)offset_table;
+  (void)bit_table;
+  (void)length_table;
+}
+} // namespace sxt::mtxpp2
--- sxt/multiexp/pippenger2/variable_length_computation.h
+++ sxt/multiexp/pippenger2/variable_length_computation.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+
 namespace sxt::mtxpp2 {
-} // namespace mtxpp2
+//--------------------------------------------------------------------------------------------------
+// compute_offset_table 
+//--------------------------------------------------------------------------------------------------
+void compute_offset_table(basct::span<unsigned> offset_table, basct::cspan<unsigned> bit_table,
+                          basct::cspan<unsigned> length_table) noexcept;
+} // namespace sxt::mtxpp2
