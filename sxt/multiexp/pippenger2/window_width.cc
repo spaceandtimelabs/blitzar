3ca4b0f1010eb2d5fb0384711bc2cf6feb11d346
rnburn <ryan.burn@gmail.com>
2024-07-29 10:39:47 -0700

    fill in window width option

--- sxt/multiexp/pippenger2/window_width.cc
+++ sxt/multiexp/pippenger2/window_width.cc
@@ -1,10 +1,38 @@
 #include "sxt/multiexp/pippenger2/window_width.h"
 
+#include <cstdlib>
+#include <cstring>
+#include <charconv>
+
+#include "sxt/base/error/panic.h"
+
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// get_default_window_width_impl 
+//--------------------------------------------------------------------------------------------------
+static unsigned get_default_window_width_impl() noexcept {
+  auto s = std::getenv("BLITZAR_PARTITION_WINDW_WIDTH");
+  if (s == nullptr) {
+    return 16;
+  }
+  unsigned width;
+  auto parse_result = std::from_chars(s, s + std::strlen(s), width);
+  if (parse_result.ec != std::errc{}) {
+    baser::panic("failed to parse partition window width {}", s);
+  }
+  if (width == 0) {
+    baser::panic("partition window width cannot be zero");
+  }
+  return width;
+}
+
 //--------------------------------------------------------------------------------------------------
 // get_default_window_width
 //--------------------------------------------------------------------------------------------------
 unsigned get_default_window_width() noexcept {
-  return 16;
+  static auto res = []() noexcept {
+    return get_default_window_width_impl();
+  }();
+  return res;
 }
 } // sxt::mtxpp2
