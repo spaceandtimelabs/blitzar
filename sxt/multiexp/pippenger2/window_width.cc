fbf3ead34fe5be4d410175c30061cc7998e04496
rnburn <ryan.burn@gmail.com>
2024-07-29 09:22:49 -0700

    expose option for window width

---
+++ sxt/multiexp/pippenger2/window_width.cc
@@ -0,0 +1,10 @@
+#include "sxt/multiexp/pippenger2/window_width.h"
+
+namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// get_default_window_width
+//--------------------------------------------------------------------------------------------------
+unsigned get_default_window_width() noexcept {
+  return 16;
+}
+} // sxt::mtxpp2
---
+++ sxt/multiexp/pippenger2/window_width.t.cc
@@ -0,0 +1,5 @@
+#include "sxt/multiexp/pippenger2/window_width.h"
+
+#include "sxt/base/test/unit_test.h"
+
+TEST_CASE("t") {}
