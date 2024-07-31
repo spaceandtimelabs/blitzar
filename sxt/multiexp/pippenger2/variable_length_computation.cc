cfa937701b126e285c33d0647297f2f2c2f64e2f
rnburn <ryan.burn@gmail.com>
2024-07-31 10:38:32 -0700

    fill in variable length computation

--- sxt/multiexp/pippenger2/variable_length_computation.cc
+++ sxt/multiexp/pippenger2/variable_length_computation.cc
@@ -1,13 +1,32 @@
 #include "sxt/multiexp/pippenger2/variable_length_computation.h"
 
+#include <algorithm>
+
+#include "sxt/base/error/assert.h"
+
 namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
 // compute_product_length_table 
 //--------------------------------------------------------------------------------------------------
-void compute_product_length_table(basct::span<unsigned>& lengths, basct::cspan<unsigned> bit_widths,
+void compute_product_length_table(basct::span<unsigned>& product_lengths, basct::cspan<unsigned> bit_widths,
                                   basct::cspan<unsigned> output_lengths, unsigned first,
                                   unsigned length) noexcept {
-  (void)lengths;
+  auto num_products = product_lengths.size();
+  auto num_outputs = bit_widths.size();
+  SXT_DEBUG_ASSERT(
+      // clang-format off
+      num_products >= num_outputs &&
+      product_lengths.size() == num_products &&
+      bit_widths.size() == num_outputs &&
+      output_lengths.size() == num_outputs
+      // clang-format on
+  );
+
+  // find first output with longer than <length>
+  auto output_first =
+      std::count_if(output_lengths.begin(), output_lengths.end(),
+                    [&](double output_length) noexcept { return output_length <= length; });
+  (void)output_first;
   (void)bit_widths;
   (void)output_lengths;
   (void)first;
--- sxt/multiexp/pippenger2/variable_length_computation.h
+++ sxt/multiexp/pippenger2/variable_length_computation.h
@@ -6,7 +6,7 @@ namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
 // compute_product_length_table 
 //--------------------------------------------------------------------------------------------------
-void compute_product_length_table(basct::span<unsigned>& lengths, basct::cspan<unsigned> bit_widths,
+void compute_product_length_table(basct::span<unsigned>& product_lengths, basct::cspan<unsigned> bit_widths,
                                   basct::cspan<unsigned> output_lengths, unsigned first,
                                   unsigned length) noexcept;
 } // namespace sxt::mtxpp2
