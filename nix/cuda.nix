49315987e7cb205941ab27c9b68495c7f1324ef6
rnburn <ryan.burn@gmail.com>
2024-08-23 18:30:04 -0700

    bump cuda toolkit

--- nix/cuda.nix
+++ nix/cuda.nix
@@ -7,8 +7,9 @@ with pkgs;
 pkgs.stdenvNoCC.mkDerivation {
   name = "cudatoolkit";
   src = fetchurl {
-    url = "https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run";
-    sha256 = "24b2afc9f770d8cf43d6fa7adc2ebfd47c4084db01bdda1ce3ce0a4d493ba65b";
+    url = "https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run";
+    hash = "sha256-MasEOU5psU3YZW4rRMKHfbGg6Jjf+KdUakxihDgQG5Q=";
+
   };
   patches = [
     # patch host_defines.h to work with libc++
