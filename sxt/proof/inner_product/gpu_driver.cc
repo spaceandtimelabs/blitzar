841d1fcc36e90eb9a7eb7f618364837233ede7ac
rnburn <ryan.burn@gmail.com>
2024-09-24 16:46:24 -0700

    fix asan

--- sxt/proof/inner_product/gpu_driver.cc
+++ sxt/proof/inner_product/gpu_driver.cc
@@ -45,11 +45,6 @@
 #include "sxt/scalar25/operation/inv.h"
 
 namespace sxt::prfip {
-static int* f() {
-  int res = 123;
-  return &res;
-}
-
 //--------------------------------------------------------------------------------------------------
 // commit_to_fold_partial
 //--------------------------------------------------------------------------------------------------
@@ -58,7 +53,6 @@ static xena::future<void> commit_to_fold_partial(rstt::compressed_element& commi
                                                  const c21t::element_p3& q_value,
                                                  basct::cspan<s25t::element> u_vector,
                                                  basct::cspan<s25t::element> v_vector) noexcept {
-  std::cout << *f() << std::endl;
   auto u_exponents = mtxb::to_exponent_sequence(u_vector);
   auto u_commit_fut = mtxcrv::async_compute_multiexponentiation<c21t::element_p3>(
       g_vector.subspan(0, u_vector.size()), u_exponents);
