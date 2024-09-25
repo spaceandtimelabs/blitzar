ce3988ea3bb97005cb85e6faf2fbbe7eab7588cb
rnburn <ryan.burn@gmail.com>
2024-09-25 15:53:28 -0700

    add stub for sumcheck

---
+++ sxt/proof/sumcheck/round_sum.cc
@@ -0,0 +1,1 @@
+#include "sxt/proof/sumcheck/round_sum.h"
---
+++ sxt/proof/sumcheck/round_sum.t.cc
@@ -0,0 +1,7 @@
+#include "sxt/proof/sumcheck/round_sum.h"
+
+#include "sxt/base/test/unit_test.h"
+using namespace sxt;
+using namespace sxt::prfsk;
+
+TEST_CASE("t") {}
