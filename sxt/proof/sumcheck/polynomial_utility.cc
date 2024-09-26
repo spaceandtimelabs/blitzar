9c8a58eabf1b29343e623b154d58a8a09d218834
rnburn <ryan.burn@gmail.com>
2024-09-26 15:01:52 -0700

    fill in polynomial utility

--- sxt/proof/sumcheck/polynomial_utility.cc
+++ sxt/proof/sumcheck/polynomial_utility.cc
@@ -2,6 +2,7 @@
 
 #include "sxt/scalar25/type/element.h"
 #include "sxt/scalar25/operation/add.h"
+#include "sxt/scalar25/operation/muladd.h"
 
 namespace sxt::prfsk {
 //--------------------------------------------------------------------------------------------------
@@ -14,6 +15,23 @@ void sum_polynomial_01(s25t::element& e, basct::cspan<s25t::element> polynomial)
   e = polynomial[0];
   for (unsigned i=1; i<polynomial.size(); ++i) {
     s25o::add(e, e, polynomial[i]);
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// evaluate_polynomial
+//--------------------------------------------------------------------------------------------------
+void evaluate_polynomial(s25t::element& e, basct::cspan<s25t::element> polynomial,
+                         const s25t::element& x) noexcept {
+  if (polynomial.empty()) {
+    e = s25t::element{};
+  }
+  auto i = polynomial.size();
+  --i;
+  e = polynomial[i];
+  while (i > 0) {
+    --i;
+    s25o::muladd(e, e, x, polynomial[i]);
   }
 }
 } // namespace sxt::prfsk
--- sxt/proof/sumcheck/polynomial_utility.h
+++ sxt/proof/sumcheck/polynomial_utility.h
@@ -9,4 +9,10 @@ namespace sxt::prfsk {
 // sum_polynomial_01 
 //--------------------------------------------------------------------------------------------------
 void sum_polynomial_01(s25t::element& e, basct::cspan<s25t::element> polynomial) noexcept;
+
+//--------------------------------------------------------------------------------------------------
+// evaluate_polynomial
+//--------------------------------------------------------------------------------------------------
+void evaluate_polynomial(s25t::element& e, basct::cspan<s25t::element> polynomial,
+                         const s25t::element& x) noexcept;
 } // namespace sxt::prfsk
