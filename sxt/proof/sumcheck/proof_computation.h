25c333dc5aea0ae09505c560ca78932ae1c26199
rnburn <ryan.burn@gmail.com>
2024-09-25 20:56:08 -0700

    add proof stub

--- sxt/proof/sumcheck/proof_computation.h
+++ sxt/proof/sumcheck/proof_computation.h
@@ -1,4 +1,24 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+#include "sxt/execution/async/future.h"
+/* #include "sxt/proof/transcript/transcript_utility.h" */
+
+namespace sxt::prft { class transcript; }
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// prove_sum 
+//--------------------------------------------------------------------------------------------------
+template <class Scalar>
+xena::future<> prove_sum(basct::span<Scalar> polynomials, prft::transcript& transcript,
+                         basct::cspan<Scalar> mles, basct::cspan<unsigned> product_table,
+                         basct::cspan<unsigned> product_lengths) noexcept {
+  (void)polynomials;
+  (void)transcript;
+  (void)mles;
+  (void)product_table;
+  (void)product_lengths;
+  return {};
+}
 } // namespace sxt::prfsk
