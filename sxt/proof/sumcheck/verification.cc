6aedb32378c3d1f156f6a7177b713f3dd4617c91
rnburn <ryan.burn@gmail.com>
2024-09-26 13:54:49 -0700

    add verification stub

--- sxt/proof/sumcheck/verification.cc
+++ sxt/proof/sumcheck/verification.cc
@@ -1,1 +1,19 @@
 #include "sxt/proof/sumcheck/verification.h"
+
+#include "sxt/scalar25/type/element.h"
+#include "verification.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// verify_sumcheck_no_evaluation 
+//--------------------------------------------------------------------------------------------------
+bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
+                                   basct::span<s25t::element> evaluation_point,
+                                   prft::transcript& transcript, unsigned round_degree) noexcept {
+  (void)expected_sum;
+  (void)evaluation_point;
+  (void)transcript;
+  (void)round_degree;
+  return true;
+}
+} // namespace sxt::prfsk
--- sxt/proof/sumcheck/verification.h
+++ sxt/proof/sumcheck/verification.h
@@ -1,4 +1,15 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+
+namespace sxt::prft { class transcript; }
+namespace sxt::s25t { class element; }
+
 namespace sxt::prfsk {
-}
+//--------------------------------------------------------------------------------------------------
+// verify_sumcheck_no_evaluation 
+//--------------------------------------------------------------------------------------------------
+bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
+                                   basct::span<s25t::element> evaluation_point,
+                                   prft::transcript& transcript, unsigned round_degree) noexcept;
+} // namespace sxt::prfsk
