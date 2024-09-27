c33fa27d941124cfe26531c76ca6c5eae5d7d33e
rnburn <ryan.burn@gmail.com>
2024-09-27 14:40:41 -0700

    fill in transcript utility

--- sxt/proof/sumcheck/transcript_utility.cc
+++ sxt/proof/sumcheck/transcript_utility.cc
@@ -1,1 +1,15 @@
 #include "sxt/proof/sumcheck/transcript_utility.h"
+
+#include "sxt/proof/transcript/transcript_utility.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// init_transcript 
+//--------------------------------------------------------------------------------------------------
+void init_transcript(prft::transcript& transcript, unsigned num_variables,
+                     unsigned round_degree) noexcept {
+  prft::set_domain(transcript, "sumcheck proof v1");
+  prft::append_value(transcript, "n", num_variables);
+  prft::append_value(transcript, "k", round_degree);
+}
+} // namespace sxt::prfsk
--- sxt/proof/sumcheck/transcript_utility.h
+++ sxt/proof/sumcheck/transcript_utility.h
@@ -1,4 +1,11 @@
 #pragma once
 
+namespace sxt::prft { class transcript; }
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// init_transcript 
+//--------------------------------------------------------------------------------------------------
+void init_transcript(prft::transcript& transcript, unsigned num_variables,
+                     unsigned round_degree) noexcept;
 } // namespace sxt::prfsk
