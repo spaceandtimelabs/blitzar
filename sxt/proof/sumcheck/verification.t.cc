60e7b897f6a791d4b95c8c4075c8405e9b1d0cde
rnburn <ryan.burn@gmail.com>
2024-09-27 20:07:47 -0700

    fill in verification

--- sxt/proof/sumcheck/verification.t.cc
+++ sxt/proof/sumcheck/verification.t.cc
@@ -12,13 +12,20 @@ using sxt::s25t::operator""_s25;
 
 TEST_CASE("we can verify a sumcheck proof up to the polynomial evaluation") {
   s25t::element expected_sum = 0x0_s25;
-  std::vector<s25t::element> evaluation_point(1);
+  std::vector<s25t::element> evaluation_point = {0x0_s25};
   prft::transcript transcript{"abc"};
-  std::vector<s25t::element> round_polynomials(2);
+  std::vector<s25t::element> round_polynomials = {0x0_s25, 0x0_s25};
 
   SECTION("verification fails if dimensions don't match") {
     auto res = sxt::prfsk::verify_sumcheck_no_evaluation(expected_sum, evaluation_point, transcript,
                                                          round_polynomials, 2);
     REQUIRE(!res);
+  }
+
+  SECTION("we can verify a single round") {
+    auto res = sxt::prfsk::verify_sumcheck_no_evaluation(expected_sum, evaluation_point, transcript,
+                                                         round_polynomials, 1);
+    REQUIRE(res);
+    REQUIRE(evaluation_point[0] != 0x0_s25);
   }
 }
