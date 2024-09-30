caf7abfd6d60c943907da7f48dfecf8b66fee2b2
rnburn <ryan.burn@gmail.com>
2024-09-30 13:50:08 -0700

    fill in prover stub

--- sxt/proof/sumcheck/proof_computation.cc
+++ sxt/proof/sumcheck/proof_computation.cc
@@ -1,1 +1,19 @@
 #include "sxt/proof/sumcheck/proof_computation.h"
+
+#include "sxt/scalar25/type/element.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// prove_sum 
+//--------------------------------------------------------------------------------------------------
+xena::future<> prove_sum(basct::span<s25t::element> polynomials, prft::transcript& transcript,
+                         basct::cspan<s25t::element> mles, basct::cspan<unsigned> product_terms,
+                         basct::cspan<std::pair<s25t::element, unsigned>> product_table) noexcept {
+  (void)polynomials;
+  (void)transcript;
+  (void)mles;
+  (void)product_terms;
+  (void)product_table;
+  return {};
+}
+} // namespace sxt::prfsk
