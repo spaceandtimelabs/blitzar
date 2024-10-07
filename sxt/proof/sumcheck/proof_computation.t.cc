94fdfd1055b032289073b3ff5e280ae560654602
rnburn <ryan.burn@gmail.com>
2024-10-07 15:20:50 -0700

    fill in sumcheck tests

--- sxt/proof/sumcheck/proof_computation.t.cc
+++ sxt/proof/sumcheck/proof_computation.t.cc
@@ -8,6 +8,7 @@
 #include "sxt/execution/async/future.h"
 #include "sxt/proof/sumcheck/cpu_driver.h"
 #include "sxt/proof/transcript/transcript.h"
+#include "sxt/scalar25/operation/overload.h"
 #include "sxt/scalar25/type/element.h"
 #include "sxt/scalar25/type/literal.h"
 using namespace sxt;
@@ -31,9 +32,8 @@ TEST_CASE("we can create a sumcheck proof") {
   SECTION("we can prove a sum with a single term") {
     auto fut = prove_sum(polynomials, evaluation_point, transcript, drv, mles, product_table,
                          product_terms, 2);
-    for (auto& s : polynomials) {
-      std::cout << s << "\n";
-    }
+    REQUIRE(polynomials[0] == mles[0]);
+    REQUIRE(polynomials[1] == mles[1] - mles[0]);
     std::cout << "*********\n";
     for (auto& r : evaluation_point) {
       std::cout << r << "\n";
