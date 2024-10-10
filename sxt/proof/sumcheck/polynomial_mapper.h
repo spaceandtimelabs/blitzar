d2de7f316f6ea1176e6a9aa4261dbacf02682b01
rnburn <ryan.burn@gmail.com>
2024-10-10 14:58:52 -0700

    fill in polynomial_mapper

--- sxt/proof/sumcheck/polynomial_mapper.h
+++ sxt/proof/sumcheck/polynomial_mapper.h
@@ -4,6 +4,7 @@
 #include <utility>
 
 #include "sxt/base/macro/cuda_callable.h"
+#include "sxt/proof/sumcheck/polynomial_utility.h"
 #include "sxt/scalar25/operation/add.h"
 #include "sxt/scalar25/type/element.h"
 
@@ -15,15 +16,24 @@ template <unsigned MaxDegree> struct polynomial_mapper {
   using value_type = std::array<s25t::element, MaxDegree + 1u>;
 
   CUDA_CALLABLE
-  value_type map_index(unsigned index) const noexcept {
-    (void)index;
-    return {};
-  }
-
-  CUDA_CALLABLE
   void map_index(value_type& p, unsigned index) const noexcept {
+    // zero
+    for (auto& pi : p) { 
+      pi = {};
+    }
+
+    // expand products
+/* void expand_products(basct::span<s25t::element> p, const s25t::element* mles, unsigned n, */
+/*                      unsigned step, basct::cspan<unsigned> terms) noexcept; */
     (void)p;
     (void)index;
   }
+
+  const s25t::element* __restrict__ mles;
+  const std::pair<s25t::element, unsigned>* __restrict__ product_table;
+  const unsigned* __restrict__ product_terms;
+  unsigned num_products;
+  unsigned mid;
+  unsigned n;
 };
 } // namespace sxt::prfsk
