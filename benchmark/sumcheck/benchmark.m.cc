4a632f1133e93999d9e7840a1e318a6ee955bc82
rnburn <ryan.burn@gmail.com>
2024-10-14 20:31:28 -0700

    fill in sumcheck benchmark

--- benchmark/sumcheck/benchmark.m.cc
+++ benchmark/sumcheck/benchmark.m.cc
@@ -4,7 +4,9 @@
 
 #include "sxt/base/container/span.h"
 #include "sxt/base/error/panic.h"
+#include "sxt/base/num/fast_random_number_generator.h"
 #include "sxt/memory/management/managed_array.h"
+#include "sxt/scalar25/random/element.h"
 #include "sxt/scalar25/type/element.h"
 using namespace sxt;
 
@@ -47,17 +49,15 @@ static void read_params(params& p, int argc, char* argv[]) noexcept {
   }
 }
 
-void fill_mles(basct::span<s25t::element> mles) noexcept {
-  (void)mles;
-}
-
 int main(int argc, char* argv[]) {
   params p;
   read_params(p, argc, argv);
 
+  basn::fast_random_number_generator rng{1, 2};
+
   // mles
   memmg::managed_array<s25t::element> mles(p.n * p.degree * p.num_products);
-  fill_mles(mles);
+  s25rn::generate_random_elements(mles, rng);
 
   return 0;
 }
