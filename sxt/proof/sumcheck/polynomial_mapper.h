56491beda2268ebd21c5a2d9567b2016f7f3d952
rnburn <ryan.burn@gmail.com>
2024-10-31 17:21:32 -0700

    fill in polynomial_mapper

--- sxt/proof/sumcheck/polynomial_mapper.h
+++ sxt/proof/sumcheck/polynomial_mapper.h
@@ -17,11 +17,13 @@
 #pragma once
 
 #include <array>
+#include <cassert>
 #include <utility>
 
 #include "sxt/base/macro/cuda_callable.h"
 #include "sxt/proof/sumcheck/polynomial_utility.h"
 #include "sxt/scalar25/operation/add.h"
+#include "sxt/scalar25/operation/mul.h"
 #include "sxt/scalar25/operation/muladd.h"
 #include "sxt/scalar25/type/element.h"
 
@@ -41,21 +43,24 @@ template <unsigned MaxDegree> struct polynomial_mapper {
 
   CUDA_CALLABLE
   void map_index(value_type& p, unsigned index) const noexcept {
-    // zero
-    for (auto& pi : p) {
-      pi = {};
-    }
-
-    // expand products
     auto mle_data = mles + index;
     auto terms_data = product_terms;
     s25t::element prod[MaxDegree + 1u];
-    for (unsigned product_index = 0; product_index < num_products; ++product_index) {
-      auto [mult, num_terms] = product_table[product_index];
 
+    // first iteration
+    assert(num_products > 0);
+    auto [mult, num_terms] = product_table[0];
+    expand_products({prod, num_terms + 1u}, mle_data, n, mid, {terms_data, num_terms});
+    terms_data += num_terms;
+    for (unsigned i = 0; i < num_terms + 1; ++i) {
+      s25o::mul(p[i], mult, prod[i]);
+    }
+
+    // rest
+    for (unsigned product_index = 1; product_index < num_products; ++product_index) {
+      auto [mult, num_terms] = product_table[product_index];
       expand_products({prod, num_terms + 1u}, mle_data, n, mid, {terms_data, num_terms});
       terms_data += num_terms;
-
       for (unsigned i = 0; i < num_terms + 1; ++i) {
         s25o::muladd(p[i], mult, prod[i], p[i]);
       }
