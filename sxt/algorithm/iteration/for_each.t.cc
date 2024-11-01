d41c4699e3bb256b45cc0a2be4df50f1bb10ac4b
rnburn <ryan.burn@gmail.com>
2024-11-01 14:58:51 -0700

    add test case

--- sxt/algorithm/iteration/for_each.t.cc
+++ sxt/algorithm/iteration/for_each.t.cc
@@ -29,7 +29,7 @@ using namespace sxt;
 using namespace sxt::algi;
 
 TEST_CASE("we can launch a for_each kernel to iterate over a sequence of integers") {
-  for (unsigned n : {1, 2, 3, 5, 31, 32, 33, 63, 64, 65, 100, 1'000, 10'000, 100'000}) {
+  for (unsigned n : {0, 1, 2, 3, 5, 31, 32, 33, 63, 64, 65, 100, 1'000, 10'000, 100'000}) {
     memmg::managed_array<unsigned> a{n, memr::get_managed_device_resource()};
     auto data = a.data();
     auto f = [data] __device__ __host__(unsigned /*n*/, unsigned i) noexcept { data[i] = i; };
