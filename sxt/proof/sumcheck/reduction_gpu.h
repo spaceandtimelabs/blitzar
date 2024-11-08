d2dc1ef02fe608178daccb0e240c9e9c424867cb
rnburn <ryan.burn@gmail.com>
2024-11-08 14:32:01 -0800

    fill in reduction

--- sxt/proof/sumcheck/reduction_gpu.h
+++ sxt/proof/sumcheck/reduction_gpu.h
@@ -1,4 +1,14 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+
+namespace sxt::basdv { class stream; }
+namespace sxt::s25t { class element; }
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// reduce_sums 
+//--------------------------------------------------------------------------------------------------
+void reduce_sums(basct::span<s25t::element> p, basdv::stream& stream,
+                 basct::cspan<s25t::element> partial_terms, unsigned num_terms) noexcept;
 } // namespace sxt::prfsk
