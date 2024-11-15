f0572c8c7cbe37825720af013c2da13ffc673a24
rnburn <ryan.burn@gmail.com>
2024-11-14 17:39:11 -0800

    fill in device cache

--- sxt/proof/sumcheck/device_cache.cc
+++ sxt/proof/sumcheck/device_cache.cc
@@ -6,20 +6,29 @@
 
 namespace sxt::prfsk {
 //--------------------------------------------------------------------------------------------------
-// lookup
+// make_device_copy 
 //--------------------------------------------------------------------------------------------------
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
 #pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wunused-parameter"
+static std::unique_ptr<device_cache_data>
+make_device_copy(basct::cspan<std::pair<s25t::element, unsigned>> product_table,
+                 basct::cspan<unsigned> product_terms, basdv::stream& stream) noexcept {
+  return nullptr;
+}
+#pragma clang diagnostic pop
+
+//--------------------------------------------------------------------------------------------------
+// lookup
+//--------------------------------------------------------------------------------------------------
 void device_cache::lookup(basct::cspan<std::pair<s25t::element, unsigned>>& product_table,
                           basct::cspan<unsigned>& product_terms, basdv::stream& stream) noexcept {
   auto& ptr = data_[basdv::get_device()];
   if (ptr == nullptr) {
-    // TODO: copy data
+    ptr = make_device_copy(product_table_, product_terms_, stream);
   }
   product_table = ptr->product_table;
   product_terms = ptr->product_terms;
 }
-#pragma clang diagnostic pop
 } // namespace sxt::prfsk
