77cc478ed465a12d3aa2f07db332b64ba2248a75
rnburn <ryan.burn@gmail.com>
2024-11-14 18:35:10 -0800

    fill in mle utility

--- sxt/proof/sumcheck/mle_utility.cc
+++ sxt/proof/sumcheck/mle_utility.cc
@@ -1,1 +1,19 @@
 #include "sxt/proof/sumcheck/mle_utility.h"
+
+#include "sxt/base/device/stream.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/scalar25/type/element.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// copy_partial_mles 
+//--------------------------------------------------------------------------------------------------
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+void copy_partial_mles(memmg::managed_array<s25t::element> partial_mles, basdv::stream& stream,
+                       basct::cspan<s25t::element> mles, unsigned n, unsigned a,
+                       unsigned b) noexcept {}
+#pragma clang diagnostic pop
+} // namespace sxt::prfsk
--- sxt/proof/sumcheck/mle_utility.h
+++ sxt/proof/sumcheck/mle_utility.h
@@ -1,4 +1,16 @@
 #pragma once
 
+#include "sxt/base/container/span.h"
+#include "sxt/memory/management/managed_array_fwd.h"
+
+namespace sxt::basdv { class stream; }
+namespace sxt::s25t { class element; }
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// copy_partial_mles 
+//--------------------------------------------------------------------------------------------------
+void copy_partial_mles(memmg::managed_array<s25t::element> partial_mles, basdv::stream& stream,
+                       basct::cspan<s25t::element> mles, unsigned n, unsigned a,
+                       unsigned b) noexcept;
 } // namespace sxt::prfsk
