82cf3bffe74dc2fd077f44e74e797bf78249b539
rnburn <ryan.burn@gmail.com>
2024-11-21 14:30:27 -0800

    fill in chunked driver

--- sxt/proof/sumcheck/chunked_gpu_driver.cc
+++ sxt/proof/sumcheck/chunked_gpu_driver.cc
@@ -1,5 +1,8 @@
 #include "sxt/proof/sumcheck/chunked_gpu_driver.h"
 
+#include <algorithm>
+
+#include "sxt/base/num/ceil_log2.h"
 #include "sxt/execution/async/future.h"
 #include "sxt/memory/management/managed_array.h"
 #include "sxt/scalar25/type/element.h"
@@ -22,18 +25,19 @@ struct chunked_gpu_workspace final : public workspace {
 //--------------------------------------------------------------------------------------------------
 // make_workspace
 //--------------------------------------------------------------------------------------------------
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-function"
-#pragma clang diagnostic ignored "-Wunused-variable"
-#pragma clang diagnostic ignored "-Wunused-parameter"
 xena::future<std::unique_ptr<workspace>>
 chunked_gpu_driver::make_workspace(basct::cspan<s25t::element> mles,
                                    basct::cspan<std::pair<s25t::element, unsigned>> product_table,
                                    basct::cspan<unsigned> product_terms,
                                    unsigned n) const noexcept {
-  return {};
+  auto res = std::make_unique<chunked_gpu_workspace>();
+  res->mles0 = mles;
+  res->product_table =product_table;
+  res->product_terms = product_terms;
+  res->n = n;
+  res->num_variables = std::max(basn::ceil_log2(n), 1);
+  return xena::make_ready_future<std::unique_ptr<workspace>>(std::move(res));
 }
-#pragma clang diagnostic pop
 
 //--------------------------------------------------------------------------------------------------
 // sum
