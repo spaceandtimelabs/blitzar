e3049718b41b1c1ae5c43c2aa3e30798650bd1e7
rnburn <ryan.burn@gmail.com>
2024-11-21 14:25:38 -0800

    fill in chunked driver

--- sxt/proof/sumcheck/chunked_gpu_driver.cc
+++ sxt/proof/sumcheck/chunked_gpu_driver.cc
@@ -1,8 +1,24 @@
 #include "sxt/proof/sumcheck/chunked_gpu_driver.h"
 
 #include "sxt/execution/async/future.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/scalar25/type/element.h"
 
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// chunked_gpu_workspace
+//--------------------------------------------------------------------------------------------------
+namespace {
+struct chunked_gpu_workspace final : public workspace {
+  basct::cspan<s25t::element> mles0;
+  memmg::managed_array<s25t::element> mles;
+  basct::cspan<std::pair<s25t::element, unsigned>> product_table;
+  basct::cspan<unsigned> product_terms;
+  unsigned n;
+  unsigned num_variables;
+};
+} // namespace
+
 //--------------------------------------------------------------------------------------------------
 // make_workspace
 //--------------------------------------------------------------------------------------------------
