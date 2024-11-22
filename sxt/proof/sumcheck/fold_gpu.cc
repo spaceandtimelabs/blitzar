631a1dc3480e9ebf4f61fd3a3dd9ad09ded9e570
rnburn <ryan.burn@gmail.com>
2024-11-22 10:28:22 -0800

    fill in chunked fold

--- sxt/proof/sumcheck/fold_gpu.cc
+++ sxt/proof/sumcheck/fold_gpu.cc
@@ -1,12 +1,16 @@
 #include "sxt/proof/sumcheck/fold_gpu.h"
 
+#include "sxt/algorithm/iteration/kernel_fit.h"
 #include "sxt/base/device/property.h"
+#include "sxt/base/device/stream.h"
 #include "sxt/base/error/assert.h"
 #include "sxt/base/iterator/index_range_iterator.h"
 #include "sxt/base/iterator/index_range_utility.h"
 #include "sxt/execution/async/coroutine.h"
 #include "sxt/execution/async/future.h"
 #include "sxt/execution/device/for_each.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/async_device_resource.h"
 #include "sxt/scalar25/operation/sub.h"
 #include "sxt/scalar25/type/element.h"
 #include "sxt/scalar25/type/literal.h"
@@ -21,6 +25,9 @@ namespace sxt::prfsk {
 #pragma clang diagnostic ignored "-Wunused-parameter"
 static xena::future<> fold_impl(basct::span<s25t::element> mles, unsigned n, unsigned a, unsigned b,
                                 const s25t::element& r, const s25t::element one_m_r) noexcept {
+/* void copy_partial_mles(memmg::managed_array<s25t::element>& partial_mles, basdv::stream& stream, */
+/*                        basct::cspan<s25t::element> mles, unsigned n, unsigned a, */
+/*                        unsigned b) noexcept; */
   return {};
 }
 #pragma clang diagnostic pop
