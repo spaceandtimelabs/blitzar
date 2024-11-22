17223cc1d94603960bb62197b333570b0556b7c6
rnburn <ryan.burn@gmail.com>
2024-11-21 19:30:31 -0800

    fill in gpu fold

--- sxt/proof/sumcheck/fold_gpu.cc
+++ sxt/proof/sumcheck/fold_gpu.cc
@@ -1,1 +1,19 @@
 #include "sxt/proof/sumcheck/fold_gpu.h"
+
+#include "sxt/execution/async/future.h"
+#include "sxt/scalar25/type/element.h"
+#include "sxt/scalar25/type/literal.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// fold_gpu 
+//--------------------------------------------------------------------------------------------------
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+xena::future<> fold_gpu(basct::span<s25t::element> mles, unsigned n, const s25t::element& r) noexcept {
+  return {};
+}
+#pragma clang diagnostic pop
+} // namespace sxt::prfsk
--- sxt/proof/sumcheck/fold_gpu.h
+++ sxt/proof/sumcheck/fold_gpu.h
@@ -1,4 +1,13 @@
 #pragma once
 
+#include "sxt/execution/async/future_fwd.h"
+#include "sxt/base/container/span.h"
+
+namespace sxt::s25t { class element; }
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// fold_gpu 
+//--------------------------------------------------------------------------------------------------
+xena::future<> fold_gpu(basct::span<s25t::element> mles, unsigned n, const s25t::element& r) noexcept;
 } // namespace sxt::prfsk
