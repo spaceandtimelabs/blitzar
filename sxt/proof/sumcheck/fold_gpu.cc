9efa12d0095f77736dc70ad22ee4fd1a20f2ba4c
rnburn <ryan.burn@gmail.com>
2024-11-21 19:33:57 -0800

    fill in fold gpu

--- sxt/proof/sumcheck/fold_gpu.cc
+++ sxt/proof/sumcheck/fold_gpu.cc
@@ -1,6 +1,8 @@
 #include "sxt/proof/sumcheck/fold_gpu.h"
 
+#include "sxt/base/error/assert.h"
 #include "sxt/execution/async/future.h"
+#include "sxt/scalar25/operation/sub.h"
 #include "sxt/scalar25/type/element.h"
 #include "sxt/scalar25/type/literal.h"
 
@@ -13,6 +15,13 @@ namespace sxt::prfsk {
 #pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wunused-parameter"
 xena::future<> fold_gpu(basct::span<s25t::element> mles, unsigned n, const s25t::element& r) noexcept {
+  using s25t::operator""_s25;
+  auto num_mles = mles.size() / n;
+  SXT_DEBUG_ASSERT(
+      mles.size() == num_mles * n
+  );
+  s25t::element one_m_r = 0x1_s25;
+  s25o::sub(one_m_r, one_m_r, r);
   return {};
 }
 #pragma clang diagnostic pop
