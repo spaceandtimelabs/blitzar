4a7015245ca1b237b38cc0fbd2e9156c1ec28ba5
rnburn <ryan.burn@gmail.com>
2024-11-27 15:35:45 -0800

    chunk mx combination

---
+++ sxt/multiexp/pippenger2/combine_reduce.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <numeric>
+
+#include "sxt/base/container/span.h"
+#include "sxt/base/curve/element.h"
+#include "sxt/base/iterator/index_range_iterator.h"
+#include "sxt/base/iterator/index_range_utility.h"
+#include "sxt/execution/async/future.h"
+#include "sxt/memory/management/managed_array.h"
+
+namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// combine_reduce 
+//--------------------------------------------------------------------------------------------------
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+template <bascrv::element T>
+xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_bit_table,
+                              basct::cspan<T> partial_products) noexcept {
+  auto num_outputs = output_bit_table.size();
+
+  // partials
+  memmg::managed_array<unsigned> bit_table_partial_sums(num_outputs);
+  std::partial_sum(output_bit_table.begin(), output_bit_table.end(),
+                   bit_table_partial_sums.begin());
+  auto num_chunks = partial_products.size() / bit_table_partial_sums[num_outputs - 1];
+
+  // chunk
+}
+#pragma clang diagnostic pop
+} // namespace sxt::mtxpp2
---
+++ sxt/multiexp/pippenger2/combine_reduce.t.cc
@@ -0,0 +1,6 @@
+#include "sxt/multiexp/pippenger2/combine_reduce.h"
+
+#include "sxt/base/test/unit_test.h"
+
+TEST_CASE("todo") {
+}
