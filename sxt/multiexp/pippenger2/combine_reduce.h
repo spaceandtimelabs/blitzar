2f72aaf792a7e1c45454e2398b75293ef96326b2
rnburn <ryan.burn@gmail.com>
2024-12-04 14:10:25 -0800

    fill in combine_reduce

--- sxt/multiexp/pippenger2/combine_reduce.h
+++ sxt/multiexp/pippenger2/combine_reduce.h
@@ -6,16 +6,19 @@
 #include "sxt/base/container/span_utility.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/device/property.h"
+#include "sxt/base/device/stream.h"
 #include "sxt/base/iterator/index_range_iterator.h"
 #include "sxt/base/iterator/index_range_utility.h"
 #include "sxt/execution/async/coroutine.h"
 #include "sxt/execution/async/future.h"
 #include "sxt/execution/device/for_each.h"
+#include "sxt/execution/device/strided_copy.h"
 #include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/async_device_resource.h"
 
 namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
-// combine_reduce_cunk
+// combine_reduce_chunk
 //--------------------------------------------------------------------------------------------------
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
@@ -26,17 +29,27 @@ xena::future<> combine_reduce_chunk(basct::span<T> res,
                                     basct::cspan<unsigned> output_bit_table_partial_sums,
                                     basct::cspan<T> partial_products, unsigned reduction_size,
                                     unsigned partials_offset) noexcept {
-  return {};
+  auto num_partials = partial_products.size() / reduction_size;
+  auto num_outputs = output_bit_table_partial_sums.size();
+  unsigned slice_num_partials = output_bit_table_partial_sums[num_outputs - 1] - partials_offset;
+  basdv::stream stream;
+
+  // copy data
+  memr::async_device_resource resource{stream};
+  memmg::managed_array<T> partials_dev{slice_num_partials * reduction_size, &resource};
+  co_await xendv::strided_copy_host_to_device(partials_dev, stream, partial_products, num_partials,
+                                              slice_num_partials, partials_offset);
+  /* template <class T> */
+  /* xena::future<> strided_copy_host_to_device(basct::span<T> dst, const basdv::stream& stream, */
+  /*                                            basct::cspan<T> src, size_t stride, size_t
+   * slice_size, */
+  /*                                            size_t offset) noexcept { */
 }
 #pragma clang diagnostic pop
 
 //--------------------------------------------------------------------------------------------------
 // combine_reduce 
 //--------------------------------------------------------------------------------------------------
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-function"
-#pragma clang diagnostic ignored "-Wunused-variable"
-#pragma clang diagnostic ignored "-Wunused-parameter"
 template <bascrv::element T>
 xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_bit_table,
                               basct::cspan<T> partial_products) noexcept {
@@ -56,7 +69,6 @@ xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_
   co_await xendv::concurrent_for_each(
       chunk_first, chunk_last, [&](basit::index_range rng) noexcept -> xena::future<> {
         auto output_first = rng.a();
-        auto output_last = rng.b();
 
         auto res_chunk = res.subspan(output_first, rng.size());
         auto bit_table_partial_sums_chunk =
@@ -67,5 +79,4 @@ xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_
                                       reduction_size, partials_offset);
       });
 }
-#pragma clang diagnostic pop
 } // namespace sxt::mtxpp2
