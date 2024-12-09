38b002d4201d6af6d8c610350283c082e23ba802
Ryan Burn <ryan.burn@gmail.com>
2024-12-09 14:03:12 -0800

    feat: add pinned buffer pool (PROOF-923) (#203)
    * work on buffer pool
    
    * test pinned buffer pool
    
    * fill in testing

---
+++ sxt/base/device/pinned_buffer_pool.h
@@ -0,0 +1,56 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2024-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cstddef>
+
+namespace sxt::basdv {
+struct pinned_buffer_handle;
+
+constexpr unsigned pinned_buffer_size = 1024u * 1024u * 2u; // 2 megabytes
+
+//--------------------------------------------------------------------------------------------------
+// pinned_buffer_pool
+//--------------------------------------------------------------------------------------------------
+class pinned_buffer_pool {
+public:
+  explicit pinned_buffer_pool(size_t initial_size) noexcept;
+
+  ~pinned_buffer_pool() noexcept;
+
+  pinned_buffer_pool(const pinned_buffer_pool&) = delete;
+  pinned_buffer_pool(pinned_buffer_pool&&) = delete;
+  pinned_buffer_pool& operator=(const pinned_buffer_pool&) = delete;
+
+  pinned_buffer_handle* aquire_handle() noexcept;
+
+  void release_handle(pinned_buffer_handle* handle) noexcept;
+
+  size_t size() const noexcept;
+
+private:
+  pinned_buffer_handle* head_ = nullptr;
+};
+
+//--------------------------------------------------------------------------------------------------
+// get_pinned_buffer_pool
+//--------------------------------------------------------------------------------------------------
+/**
+ * Access the thread_local pinned pool.
+ */
+pinned_buffer_pool* get_pinned_buffer_pool(size_t initial_size = 16) noexcept;
+} // namespace sxt::basdv
---
+++ sxt/base/device/pinned_buffer_pool.t.cc
@@ -0,0 +1,42 @@
+/** Proofs GPU - Space and Time's cryptographic proof algorithms on the CPU and GPU.
+ *
+ * Copyright 2024-present Space and Time Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "sxt/base/device/pinned_buffer_pool.h"
+
+#include "sxt/base/test/unit_test.h"
+
+using namespace sxt;
+using namespace sxt::basdv;
+
+TEST_CASE("we can pool pinned buffers") {
+  size_t num_buffers = 3u;
+  pinned_buffer_pool pool{num_buffers};
+  REQUIRE(pool.size() == num_buffers);
+
+  SECTION("we can aquire and release buffers") {
+    auto h = pool.aquire_handle();
+    REQUIRE(pool.size() == num_buffers - 1);
+    pool.release_handle(h);
+    REQUIRE(pool.size() == num_buffers);
+  }
+
+  SECTION("we can aquire a handle from an empty pool") {
+    pinned_buffer_pool empty_pool{0};
+    auto h = empty_pool.aquire_handle();
+    empty_pool.release_handle(h);
+    REQUIRE(empty_pool.size() == 1);
+  }
+}
