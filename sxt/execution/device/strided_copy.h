40719c2509a2c62d23bf5e737b0101bd1e39723b
rnburn <ryan.burn@gmail.com>
2024-12-09 12:08:08 -0800

    fill in strided copy

--- sxt/execution/device/strided_copy.h
+++ sxt/execution/device/strided_copy.h
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 
+#include "sxt/base/error/assert.h"
 #include "sxt/base/container/span.h"
 #include "sxt/execution/async/future.h"
 
@@ -20,9 +21,17 @@ xena::future<> strided_copy_host_to_device(basct::span<T> dst, const basdv::stre
                                            basct::cspan<T> src, size_t stride, size_t slice_size,
                                            size_t offset) noexcept {
   if (slice_size == 0) {
+    SXT_RELEASE_ASSERT(dst.empty());
     return xena::make_ready_future();
   }
   auto count = dst.size() / slice_size;
+  SXT_RELEASE_ASSERT(
+      // clang-format off
+      stride >= slice_size &&
+      dst.size() == count * slice_size && 
+      src.size() >= offset + (count - 1u)*stride + slice_size
+      // clang-format on
+  );
   return strided_copy_host_to_device(reinterpret_cast<std::byte*>(dst.data()), stream,
                                      reinterpret_cast<const std::byte*>(src.data() + offset),
                                      slice_size * sizeof(T), count, stride * sizeof(T));
