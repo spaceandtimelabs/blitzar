902dfe46355969480ceb1f9883fa908fd5b0fb49
rnburn <ryan.burn@gmail.com>
2025-01-06 20:13:32 -0800

    rework combine reduce

--- sxt/multiexp/pippenger2/combine_reduce.h
+++ sxt/multiexp/pippenger2/combine_reduce.h
@@ -183,6 +183,48 @@ xena::future<> combine_reduce(basct::span<T> res, const basit::split_options& sp
       });
 }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+template <bascrv::element T>
+xena::future<> combine_reduce(basct::span<T> res, const basit::split_options& split_options,
+                              unsigned element_num_bytes,
+                              basct::cspan<T> partial_products) noexcept {
+  auto num_outputs = res.size();
+
+  if (res.empty()) {
+    co_return;
+  }
+
+  auto reduction_size = partial_products.size() / (num_outputs * element_num_bytes);
+#if 0
+  // don't split if partials are already in device memory
+  if (basdv::is_active_device_pointer(partial_products.data())) {
+    co_return co_await combine_reduce_chunk(res, bit_table_partial_sums, partial_products,
+                                            reduction_size, 0);
+  }
+
+  // split
+  auto [chunk_first, chunk_last] = basit::split(basit::index_range{0, num_outputs}, split_options);
+
+  // combine reduce
+  co_await xendv::concurrent_for_each(
+      chunk_first, chunk_last, [&](basit::index_range rng) noexcept -> xena::future<> {
+        auto output_first = rng.a();
+
+        auto res_chunk = res.subspan(output_first, rng.size());
+        auto bit_table_partial_sums_chunk =
+            basct::subspan(bit_table_partial_sums, output_first, rng.size());
+        auto partials_offset = output_first > 0 ? bit_table_partial_sums[output_first - 1] : 0u;
+
+        co_await combine_reduce_chunk(res_chunk, bit_table_partial_sums_chunk, partial_products,
+                                      reduction_size, partials_offset);
+      });
+#endif
+}
+#pragma clang diagnostic pop
+
 template <bascrv::element T>
 xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_bit_table,
                               basct::cspan<T> partial_products) noexcept {
@@ -191,5 +233,15 @@ xena::future<> combine_reduce(basct::span<T> res, basct::cspan<unsigned> output_
       .split_factor = basdv::get_num_devices(),
   };
   co_await combine_reduce(res, split_options, output_bit_table, partial_products);
+}
+
+template <bascrv::element T>
+xena::future<> combine_reduce(basct::span<T> res, unsigned element_num_bytes,
+                              basct::cspan<T> partial_products) noexcept {
+  basit::split_options split_options{
+      .max_chunk_size = 1024,
+      .split_factor = basdv::get_num_devices(),
+  };
+  co_await combine_reduce(res, split_options, element_num_bytes, partial_products);
 }
 } // namespace sxt::mtxpp2
