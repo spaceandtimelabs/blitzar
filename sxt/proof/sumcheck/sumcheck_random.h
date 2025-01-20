9070b01e362ff111b70b5b762cdc4ac09c17712e
rnburn <ryan.burn@gmail.com>
2025-01-20 11:41:33 -0800

    fill in random sumcheck

--- sxt/proof/sumcheck/sumcheck_random.h
+++ sxt/proof/sumcheck/sumcheck_random.h
@@ -1,24 +1,42 @@
 #pragma once
 
-#include <cstddef>
+#include <vector>
+#include <utility>
 
 #include "sxt/proof/sumcheck/constant.h"
+
+namespace sxt::s25t {
+class element;
+}
+namespace sxt::basn {
+class fast_random_number_generator;
+}
 
 namespace sxt::prfsk {
 //--------------------------------------------------------------------------------------------------
 // random_sumcheck_descriptor
 //--------------------------------------------------------------------------------------------------
 struct random_sumcheck_descriptor {
-  size_t min_length = 1;
-  size_t max_length = 10;
+  unsigned min_length = 1;
+  unsigned max_length = 10;
 
-  size_t min_num_products = 1;
-  size_t max_num_products = 5;
+  unsigned min_num_products = 1;
+  unsigned max_num_products = 5;
 
-  size_t min_product_length = 1;
-  size_t max_product_length = max_degree_v;
+  unsigned min_product_length = 1;
+  unsigned max_product_length = max_degree_v;
 
-  size_t min_num_mles = 1;
-  size_t max_num_mles = 5;
+  unsigned min_num_mles = 1;
+  unsigned max_num_mles = 5;
 };
+
+//--------------------------------------------------------------------------------------------------
+// generate_random_sumcheck_problem 
+//--------------------------------------------------------------------------------------------------
+void generate_random_sumcheck_problem(
+    std::vector<s25t::element>& mles,
+    std::vector<std::pair<s25t::element, unsigned>>& product_table,
+    std::vector<unsigned>& product_terms, unsigned& n,
+    basn::fast_random_number_generator& rng,
+    const random_sumcheck_descriptor& descriptor) noexcept;
 } // namespace sxt::prfsk
