533a5f831c472271fc782252f82f7c62902c2843
rnburn <ryan.burn@gmail.com>
2025-01-24 14:39:49 -0800

    clean up tests

--- sxt/proof/sumcheck/proof_computation.t.cc
+++ sxt/proof/sumcheck/proof_computation.t.cc
@@ -16,7 +16,6 @@
  */
 #include "sxt/proof/sumcheck/proof_computation.h"
 
-#include <iostream>
 #include <utility>
 #include <vector>
 
@@ -53,7 +52,6 @@ static void test_proof(const driver& drv) noexcept {
   };
   std::vector<unsigned> product_terms = {0};
 
-#if 0
   SECTION("we can prove a sum with n=1") {
     auto fut = prove_sum(polynomials, evaluation_point, transcript, drv, mles, product_table,
                          product_terms, 1);
@@ -152,7 +150,6 @@ static void test_proof(const driver& drv) noexcept {
     REQUIRE(polynomials[2] == mles[0]);
     REQUIRE(polynomials[3] == mles[1] - mles[0]);
   }
-#endif
 
   SECTION("we can verify random sumcheck problems") {
     basn::fast_random_number_generator rng{1, 2};
@@ -185,10 +182,8 @@ static void test_proof(const driver& drv) noexcept {
         prft::transcript transcript{"abc"};
         s25t::element expected_sum;
         sum_polynomial_01(expected_sum, basct::subspan(polynomials, 0, polynomial_length));
-        std::cout << "expected sum: " << expected_sum << std::endl;
         auto valid = verify_sumcheck_no_evaluation(expected_sum, evaluation_point, transcript,
                                                    polynomials, polynomial_length - 1u);
-        std::cerr << "v: " << valid << std::endl;
         REQUIRE(valid);
       }
 
