eb2bd5b230e961e8fd4cbc20f0f9401003f73f4c
rnburn <ryan.burn@gmail.com>
2025-01-29 14:35:02 -0800

    work on fileio

--- sxt/base/system/file_io.cc
+++ sxt/base/system/file_io.cc
@@ -1,1 +1,23 @@
 #include "sxt/base/system/file_io.h"
+
+#include <fstream>
+#include <cerrno>
+
+#include "sxt/base/error/panic.h"
+
+namespace sxt::bassy {
+//--------------------------------------------------------------------------------------------------
+// write_bytes
+//--------------------------------------------------------------------------------------------------
+void write_bytes(const char* filename, basct::cspan<uint8_t> bytes) noexcept {
+  std::ofstream out{filename, std::ios::binary};
+  if (!out.good()) {
+    baser::panic("failed to open {}: {}", filename, std::strerror(errno));
+  }
+  out.write(reinterpret_cast<const char*>(bytes.data()), bytes.size());
+  out.close();
+  if (!out.good()) {
+    baser::panic("failed to close {}: {}", filename, std::strerror(errno));
+  }
+}
+} // namespace sxt::bassy
--- sxt/base/system/file_io.h
+++ sxt/base/system/file_io.h
@@ -1,4 +1,12 @@
 #pragma once
 
+#include <cstddef>
+
+#include "sxt/base/container/span.h"
+
 namespace sxt::bassy {
+//--------------------------------------------------------------------------------------------------
+// write_bytes
+//--------------------------------------------------------------------------------------------------
+void write_bytes(const char* filename, basct::cspan<uint8_t> bytes) noexcept;
 } // namespace sxt::bassy
