37a8306e1f04c07c7040fdf67df020e48df47d2b
rnburn <ryan.burn@gmail.com>
2025-01-29 14:09:20 -0800

    serialization support

--- sxt/multiexp/pippenger2/multiexponentiation_serialization.h
+++ sxt/multiexp/pippenger2/multiexponentiation_serialization.h
@@ -1,22 +1,31 @@
 #pragma once
 
 #include <cstddef>
+#include <memory>
+#include <vector>
 
+#include "sxt/multiexp/pippenger2/partition_table_accessor.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/container/span.h"
 
 namespace sxt::mtxpp2 {
 //--------------------------------------------------------------------------------------------------
+// variable_length_multiexponentiation_descriptor
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element T, class U>
+struct variable_length_multiexponentiation_descriptor {
+  std::unique_ptr<partition_table_accessor<U>> accessor;
+  std::vector<unsigned> output_bit_table;
+  std::vector<unsigned> output_lengths;
+  std::vector<uint8_t> scalars;
+};
+
+//--------------------------------------------------------------------------------------------------
 // write_multiexponentiation 
 //--------------------------------------------------------------------------------------------------
-template <bascrv::element T>
-void write_multiexponentiation(const char* dir, basct::cspan<unsigned> output_bit_table,
+template <bascrv::element T, class U>
+void write_multiexponentiation(const char* dir, const partition_table_accessor<U>& accessor,
+                               basct::cspan<unsigned> output_bit_table,
                                basct::cspan<unsigned> output_lengths,
                                basct::cspan<uint8_t> scalars) noexcept;
-/* template <bascrv::element T, class U> */
-/*   requires std::constructible_from<T, U> */
-/* void multiexponentiate(basct::span<T> res, const partition_table_accessor<U>& accessor, */
-/*                        basct::cspan<unsigned> output_bit_table, */
-/*                        basct::cspan<unsigned> output_lengths, */
-/*                        basct::cspan<uint8_t> scalars) noexcept { */
 } // namespace sxt::mtxpp2
