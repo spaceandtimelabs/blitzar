59fbc968052fc3dd740d4a2702c5bb536359c534
rnburn <ryan.burn@gmail.com>
2025-01-30 14:26:06 -0800

    add temp directory

--- sxt/base/test/temp_directory.cc
+++ sxt/base/test/temp_directory.cc
@@ -1,14 +1,28 @@
 #include "sxt/base/test/temp_directory.h"
 
+#include <filesystem>
+#include <cstdio>
+
+#include "sxt/base/error/panic.h"
+
 namespace sxt::bastst {
 //--------------------------------------------------------------------------------------------------
 // constructor
 //--------------------------------------------------------------------------------------------------
-temp_directory::temp_directory() noexcept {}
+temp_directory::temp_directory() noexcept try : name_{std::tmpnam(nullptr)} {
+  std::filesystem::create_directory(name_);
+} catch (const std::exception& e) {
+  baser::panic("failed to create directory {}: {}", name_, e.what());
+}
 
 //--------------------------------------------------------------------------------------------------
 // destructor
 //--------------------------------------------------------------------------------------------------
 temp_directory::~temp_directory() noexcept {
+  try {
+  std::filesystem::remove(name_);
+  } catch(const std::exception& e) {
+  baser::panic("failed to remove directory {}: {}", name_, e.what());
+  }
 }
 } // namespace sxt::bastst
