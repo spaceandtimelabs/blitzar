b3c3b3263eba84ebd214c8db9f1b053c136e11d3
rnburn <ryan.burn@gmail.com>
2025-01-30 16:30:59 -0800

    test directory recorder

--- sxt/base/system/directory_recorder.t.cc
+++ sxt/base/system/directory_recorder.t.cc
@@ -1,7 +1,16 @@
 #include "sxt/base/system/directory_recorder.h"
 
+#include <fstream>
+#include <format>
+
+#include "sxt/base/test/temp_directory.h"
 #include "sxt/base/test/unit_test.h"
 using namespace sxt;
 using namespace sxt::bassy;
 
-TEST_CASE("todo") {}
+TEST_CASE("we can set up a directory for recording") {
+  bastst::temp_directory dir;
+  directory_recorder recorder{"abc", dir.name()};
+  std::ofstream out{std::format("{}/t", recorder.name())};
+  REQUIRE(out.good());
+}
