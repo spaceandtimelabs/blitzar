ce45981fe7949c811b53fd8dfe7b36ba0c1ae734
rnburn <ryan.burn@gmail.com>
2025-01-31 13:40:42 -0800

    fill in packed serialization

--- sxt/multiexp/pippenger2/multiexponentiation_serialization.h
+++ sxt/multiexp/pippenger2/multiexponentiation_serialization.h
@@ -21,16 +21,27 @@
 #include <format>
 #include <fstream>
 #include <memory>
+#include <numeric>
 #include <vector>
 
 #include "sxt/base/container/span.h"
 #include "sxt/base/curve/element.h"
 #include "sxt/base/error/assert.h"
+#include "sxt/base/num/divide_up.h"
 #include "sxt/base/system/file_io.h"
 #include "sxt/multiexp/pippenger2/in_memory_partition_table_accessor_utility.h"
 #include "sxt/multiexp/pippenger2/partition_table_accessor.h"
 
 namespace sxt::mtxpp2 {
+//--------------------------------------------------------------------------------------------------
+// packed_multiexponentiation_descriptor
+//--------------------------------------------------------------------------------------------------
+template <bascrv::element T, class U> struct packed_multiexponentiation_descriptor {
+  std::unique_ptr<partition_table_accessor<U>> accessor;
+  std::vector<unsigned> output_bit_table;
+  std::vector<uint8_t> scalars;
+};
+
 //--------------------------------------------------------------------------------------------------
 // variable_length_multiexponentiation_descriptor
 //--------------------------------------------------------------------------------------------------
@@ -44,6 +55,29 @@ template <bascrv::element T, class U> struct variable_length_multiexponentiation
 //--------------------------------------------------------------------------------------------------
 // write_multiexponentiation
 //--------------------------------------------------------------------------------------------------
+template <bascrv::element T, class U>
+void write_multiexponentiation(std::string_view dir, const partition_table_accessor<U>& accessor,
+                               basct::cspan<unsigned> output_bit_table,
+                               basct::cspan<uint8_t> scalars) noexcept {
+  size_t num_products = std::accumulate(output_bit_table.begin(), output_bit_table.end(), 0ull);
+  auto num_output_bytes = basn::divide_up<size_t>(num_products, 8);
+  SXT_DEBUG_ASSERT(
+      scalars.size() % num_output_bytes == 0
+  );
+  auto n = scalars.size() / num_output_bytes;
+
+  bassy::write_file(std::format("{}/output_bit_table.bin", dir), output_bit_table);
+  bassy::write_file(std::format("{}/scalars.bin", dir), scalars);
+
+  std::vector<U> generators(n);
+  accessor.copy_generators(generators);
+  bassy::write_file<U>(std::format("{}/generators.bin", dir), generators);
+
+  uint64_t window_width = accessor.window_width();
+  bassy::write_file(std::format("{}/window_width.bin", dir),
+                    basct::cspan<uint64_t>{&window_width, 1});
+}
+
 template <bascrv::element T, class U>
 void write_multiexponentiation(std::string_view dir, const partition_table_accessor<U>& accessor,
                                basct::cspan<unsigned> output_bit_table,
@@ -69,6 +103,23 @@ void write_multiexponentiation(std::string_view dir, const partition_table_acces
 //--------------------------------------------------------------------------------------------------
 // read_multiexponentiation
 //--------------------------------------------------------------------------------------------------
+template <bascrv::element T, class U>
+void read_multiexponentiation(packed_multiexponentiation_descriptor<T, U>& descr,
+                              std::string_view dir) noexcept {
+  bassy::read_file(descr.output_bit_table, std::format("{}/output_bit_table.bin", dir));
+  bassy::read_file(descr.scalars, std::format("{}/scalars.bin", dir));
+
+  // accessor
+  std::vector<uint64_t> window_width;
+  bassy::read_file(window_width, std::format("{}/window_width.bin", dir));
+  SXT_RELEASE_ASSERT(window_width.size() == 1);
+  std::vector<U> generators;
+  bassy::read_file(generators, std::format("{}/generators.bin", dir));
+  std::vector<T> generators_p{generators.begin(), generators.end()};
+  descr.accessor =
+      make_in_memory_partition_table_accessor<U>(generators_p, basm::alloc_t{}, window_width[0]);
+}
+
 template <bascrv::element T, class U>
 void read_multiexponentiation(variable_length_multiexponentiation_descriptor<T, U>& descr,
                               std::string_view dir) noexcept {
