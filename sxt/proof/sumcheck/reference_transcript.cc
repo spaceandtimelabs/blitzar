8199fa87dd3e50edfb622d42920fb53fca85f3ba
rnburn <ryan.burn@gmail.com>
2025-02-05 10:56:08 -0800

    rework sumcheck

---
+++ sxt/proof/sumcheck/reference_transcript.cc
@@ -0,0 +1,1 @@
+#include "sxt/proof/sumcheck/reference_transcript.h"
---
+++ sxt/proof/sumcheck/reference_transcript.t.cc
@@ -0,0 +1,6 @@
+#include "sxt/proof/sumcheck/reference_transcript.h"
+
+#include "sxt/base/test/unit_test.h"
+
+
+TEST_CASE("todo") {}
