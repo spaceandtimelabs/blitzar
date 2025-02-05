3d045b38bd5fc7c33b1cc9f62dcb1e31e6a34faf
rnburn <ryan.burn@gmail.com>
2025-02-05 13:19:31 -0800

    fill in reference transcript

--- sxt/proof/sumcheck/reference_transcript.cc
+++ sxt/proof/sumcheck/reference_transcript.cc
@@ -1,1 +1,20 @@
 #include "sxt/proof/sumcheck/reference_transcript.h"
+
+namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// init
+//--------------------------------------------------------------------------------------------------
+void reference_transcript::init(size_t num_variables, size_t round_degree) noexcept {
+  (void)num_variables;
+  (void)round_degree;
+}
+
+//--------------------------------------------------------------------------------------------------
+// round_challenge
+//--------------------------------------------------------------------------------------------------
+void reference_transcript::round_challenge(s25t::element& r,
+                                           basct::cspan<s25t::element> polynomial) noexcept {
+  (void)r;
+  (void)polynomial;
+}
+} // namespace sxt::prfsk
--- sxt/proof/sumcheck/reference_transcript.h
+++ sxt/proof/sumcheck/reference_transcript.h
@@ -1,4 +1,21 @@
 #pragma once
 
+#include "sxt/proof/sumcheck/sumcheck_transcript.h"
+
 namespace sxt::prfsk {
+//--------------------------------------------------------------------------------------------------
+// reference_transcript
+//--------------------------------------------------------------------------------------------------
+class reference_transcript final : public sumcheck_transcript {
+ public:
+   void init(size_t num_variables, size_t round_degree) noexcept override;
+
+   void round_challenge(s25t::element& r, basct::cspan<s25t::element> polynomial) noexcept override;
+
+   /* virtual void init(size_t num_variables, size_t round_degree) noexcept = 0; */
+   /*  */
+   /* virtual void round_challenge(s25t::element& r, */
+   /*                              basct::cspan<s25t::element> polynomial) noexcept = 0; */
+ private:
+};
 } // namespace sxt::prfsk
