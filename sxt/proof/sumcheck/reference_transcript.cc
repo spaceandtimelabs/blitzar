b8feab6b88ae9b76b1d820a14260d58bc0765967
rnburn <ryan.burn@gmail.com>
2025-02-05 17:43:33 -0800

    fill in reference transcript

--- sxt/proof/sumcheck/reference_transcript.cc
+++ sxt/proof/sumcheck/reference_transcript.cc
@@ -1,18 +1,16 @@
 #include "sxt/proof/sumcheck/reference_transcript.h"
 
 #include "sxt/proof/transcript/transcript_utility.h"
+#include "sxt/scalar25/type/element.h"
 
 namespace sxt::prfsk {
 //--------------------------------------------------------------------------------------------------
 // init
 //--------------------------------------------------------------------------------------------------
 void reference_transcript::init(size_t num_variables, size_t round_degree) noexcept {
-  (void)num_variables;
-  (void)round_degree;
-  (void)transcript_;
-  /* prft::set_domain(transcript, "sumcheck proof v1"); */
-  /* prft::append_value(transcript, "n", num_variables); */
-  /* prft::append_value(transcript, "k", round_degree); */
+  prft::set_domain(transcript_, "sumcheck proof v1");
+  prft::append_value(transcript_, "n", num_variables);
+  prft::append_value(transcript_, "k", round_degree);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -20,11 +18,7 @@ void reference_transcript::init(size_t num_variables, size_t round_degree) noexc
 //--------------------------------------------------------------------------------------------------
 void reference_transcript::round_challenge(s25t::element& r,
                                            basct::cspan<s25t::element> polynomial) noexcept {
-  (void)r;
-  (void)polynomial;
-#if 0
-  prft::append_values(transcript, "P", polynomial);
-  prft::challenge_value(r, transcript, "R");
-#endif
+  prft::append_values(transcript_, "P", polynomial);
+  prft::challenge_value(r, transcript_, "R");
 }
 } // namespace sxt::prfsk
