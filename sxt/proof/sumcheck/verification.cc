5f062e24c4d0b7100e4b43484fe615d083154818
rnburn <ryan.burn@gmail.com>
2025-02-06 14:06:00 -0800

    rework sumcheck transcript

--- sxt/proof/sumcheck/verification.cc
+++ sxt/proof/sumcheck/verification.cc
@@ -19,6 +19,7 @@
 #include "sxt/base/error/assert.h"
 #include "sxt/base/log/log.h"
 #include "sxt/proof/sumcheck/polynomial_utility.h"
+#include "sxt/proof/sumcheck/sumcheck_transcript.h"
 #include "sxt/proof/sumcheck/transcript_utility.h"
 #include "sxt/proof/transcript/transcript_utility.h"
 #include "sxt/scalar25/type/element.h"
@@ -67,6 +68,56 @@ bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
     // draw a random scalar
     s25t::element r;
     round_challenge(r, transcript, polynomial);
+    evaluation_point[round_index] = r;
+
+    // evaluate at random point
+    evaluate_polynomial(expected_sum, polynomial, r);
+  }
+
+  return true;
+}
+
+bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
+                                   basct::span<s25t::element> evaluation_point,
+                                   sumcheck_transcript& transcript,
+                                   basct::cspan<s25t::element> round_polynomials,
+                                   unsigned round_degree) noexcept {
+  return true;
+  auto num_variables = evaluation_point.size();
+  SXT_RELEASE_ASSERT(
+      // clang-format off
+      num_variables > 0 && round_degree > 0
+      // clang-format on
+  );
+
+  basl::info("verifying sumcheck of {} variables and round degree {}", num_variables, round_degree);
+
+  // check dimensions
+  if (auto expected_count = (round_degree + 1u) * num_variables;
+      round_polynomials.size() != expected_count) {
+    basl::info("sumcheck verification failed: expected {} scalars for round_polynomials but got {}",
+               expected_count, round_polynomials.size());
+    return false;
+  }
+
+  transcript.init(num_variables, round_degree);
+
+  // go through sumcheck rounds
+  for (unsigned round_index = 0; round_index < num_variables; ++round_index) {
+    auto polynomial =
+        round_polynomials.subspan((round_degree + 1u) * round_index, round_degree + 1u);
+
+    // check sum
+    s25t::element sum;
+    sum_polynomial_01(sum, polynomial);
+    if (expected_sum != sum) {
+      basl::info("sumcheck verification failed on round {}", round_index + 1);
+      return false;
+    }
+
+    // draw a random scalar
+    s25t::element r;
+    transcript.round_challenge(r, polynomial);
     evaluation_point[round_index] = r;
 
     // evaluate at random point
--- sxt/proof/sumcheck/verification.h
+++ sxt/proof/sumcheck/verification.h
@@ -26,12 +26,20 @@ class element;
 }
 
 namespace sxt::prfsk {
+class sumcheck_transcript;
+
 //--------------------------------------------------------------------------------------------------
 // verify_sumcheck_no_evaluation
 //--------------------------------------------------------------------------------------------------
 bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
                                    basct::span<s25t::element> evaluation_point,
                                    prft::transcript& transcript,
+                                   basct::cspan<s25t::element> round_polynomials,
+                                   unsigned round_degree) noexcept;
+
+bool verify_sumcheck_no_evaluation(s25t::element& expected_sum,
+                                   basct::span<s25t::element> evaluation_point,
+                                   sumcheck_transcript& transcript,
                                    basct::cspan<s25t::element> round_polynomials,
                                    unsigned round_degree) noexcept;
 } // namespace sxt::prfsk
