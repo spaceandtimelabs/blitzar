864bf3e407b078405b9814e13ac1e0460c675054
rnburn <ryan.burn@gmail.com>
2025-02-06 16:55:58 -0800

    drop dead code

--- sxt/proof/sumcheck/reference_transcript.h
+++ sxt/proof/sumcheck/reference_transcript.h
@@ -31,10 +31,6 @@ public:
 
   void round_challenge(s25t::element& r, basct::cspan<s25t::element> polynomial) noexcept override;
 
-  /* virtual void init(size_t num_variables, size_t round_degree) noexcept = 0; */
-  /*  */
-  /* virtual void round_challenge(s25t::element& r, */
-  /*                              basct::cspan<s25t::element> polynomial) noexcept = 0; */
 private:
   prft::transcript& transcript_;
 };
