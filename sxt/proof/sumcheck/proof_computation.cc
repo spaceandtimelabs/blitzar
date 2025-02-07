504d165e72b573cd9f0108b7d8a1ec4e27f05577
rnburn <ryan.burn@gmail.com>
2025-02-06 16:37:30 -0800

    drop dead code

--- sxt/proof/sumcheck/proof_computation.cc
+++ sxt/proof/sumcheck/proof_computation.cc
@@ -30,45 +30,6 @@ namespace sxt::prfsk {
 // prove_sum
 //--------------------------------------------------------------------------------------------------
 xena::future<> prove_sum(basct::span<s25t::element> polynomials,
-                         basct::span<s25t::element> evaluation_point, prft::transcript& transcript,
-                         const driver& drv, basct::cspan<s25t::element> mles,
-                         basct::cspan<std::pair<s25t::element, unsigned>> product_table,
-                         basct::cspan<unsigned> product_terms, unsigned n) noexcept {
-  SXT_RELEASE_ASSERT(0 < n);
-  auto num_variables = std::max(basn::ceil_log2(n), 1);
-  auto polynomial_length = polynomials.size() / num_variables;
-  auto num_mles = mles.size() / n;
-  SXT_RELEASE_ASSERT(
-      // clang-format off
-      polynomial_length > 1 &&
-      polynomials.size() == num_variables * polynomial_length &&
-      mles.size() == n * num_mles
-      // clang-format on
-  );
-
-  init_transcript(transcript, num_variables, polynomial_length - 1);
-
-  auto ws = co_await drv.make_workspace(mles, product_table, product_terms, n);
-
-  for (unsigned round_index = 0; round_index < num_variables; ++round_index) {
-    auto polynomial = polynomials.subspan(round_index * polynomial_length, polynomial_length);
-
-    // compute the round polynomial
-    co_await drv.sum(polynomial, *ws);
-
-    // draw the next random challenge
-    s25t::element r;
-    round_challenge(r, transcript, polynomial);
-    evaluation_point[round_index] = r;
-
-    // fold the polynomial
-    if (round_index < num_variables - 1u) {
-      co_await drv.fold(*ws, r);
-    }
-  }
-}
-
-xena::future<> prove_sum(basct::span<s25t::element> polynomials,
                          basct::span<s25t::element> evaluation_point,
                          sumcheck_transcript& transcript, const driver& drv,
                          basct::cspan<s25t::element> mles,
--- sxt/proof/sumcheck/proof_computation.h
+++ sxt/proof/sumcheck/proof_computation.h
@@ -36,12 +36,6 @@ class sumcheck_transcript;
 // prove_sum
 //--------------------------------------------------------------------------------------------------
 xena::future<> prove_sum(basct::span<s25t::element> polynomials,
-                         basct::span<s25t::element> evaluation_point, prft::transcript& transcript,
-                         const driver& drv, basct::cspan<s25t::element> mles,
-                         basct::cspan<std::pair<s25t::element, unsigned>> product_table,
-                         basct::cspan<unsigned> product_terms, unsigned n) noexcept;
-
-xena::future<> prove_sum(basct::span<s25t::element> polynomials,
                          basct::span<s25t::element> evaluation_point,
                          sumcheck_transcript& transcript, const driver& drv,
                          basct::cspan<s25t::element> mles,
