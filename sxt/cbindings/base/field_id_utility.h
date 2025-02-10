e013d6b4d6ec23bae15e066cc2ba9e26b0a96ce5
rnburn <ryan.burn@gmail.com>
2025-02-10 12:54:27 -0800

    fill in sumcheck cbindings

--- sxt/cbindings/base/field_id_utility.h
+++ sxt/cbindings/base/field_id_utility.h
@@ -1,6 +1,22 @@
 #pragma once
 
+#include <type_traits>
+
+#include "sxt/scalar25/type/element.h"
+#include "sxt/base/error/panic.h"
 #include "sxt/cbindings/base/field_id.h"
 
 namespace sxt::cbnb {
+//--------------------------------------------------------------------------------------------------
+// switch_field_type
+//--------------------------------------------------------------------------------------------------
+template <class F> void switch_field_type(field_id_t id, F f) {
+  switch (id) {
+  case field_id_t::scalar25519:
+    f(std::type_identity<s25t::element>{});
+    break;
+  default:
+    baser::panic("unsupported field id {}", static_cast<unsigned>(id));
+  }
+}
 } // namespace sxt::cbnb
