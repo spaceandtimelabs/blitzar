0fdb827144f578e239fffbd7e0bf397b4c6be5b4
rnburn <ryan.burn@gmail.com>
2025-02-12 09:54:07 -0800

    fix include

--- cbindings/sumcheck.cc
+++ cbindings/sumcheck.cc
@@ -14,8 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "cbindings/sumcheck.h"
 #include "cbindings/backend.h"
-#include "cbindings/fixed_pedersen.h"
 
 using namespace sxt;
 
