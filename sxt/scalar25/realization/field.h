5ac444bc7f07f5ea56be20a2f99061c6f4c14ca1
rnburn <ryan.burn@gmail.com>
2025-02-17 14:21:29 -0800

    fill in field concept

--- sxt/scalar25/realization/field.h
+++ sxt/scalar25/realization/field.h
@@ -1,4 +1,12 @@
 #pragma once
 
-#include "sxt/scalar25/type/element.h"
 #include "sxt/base/concept/field.h"
+#include "sxt/scalar25/operation/add.h"
+#include "sxt/scalar25/operation/mul.h"
+#include "sxt/scalar25/operation/muladd.h"
+#include "sxt/scalar25/operation/neg.h"
+#include "sxt/scalar25/operation/sub.h"
+#include "sxt/scalar25/type/element.h"
+
+static_assert(
+    sxt::bascpt::field<sxt::s25t::element>);
