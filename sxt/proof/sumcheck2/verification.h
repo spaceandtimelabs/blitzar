8b1d3c19efd67e2665df7723739cea905d3a888e
rnburn <ryan.burn@gmail.com>
2025-02-17 21:10:46 -0800

    rework sumcheck

---
+++ sxt/proof/sumcheck2/verification.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "sxt/base/container/span.h"
+
+namespace sxt::prfsk2 {
+class sumcheck_transcript;
+
+//--------------------------------------------------------------------------------------------------
+// verify_sumcheck_no_evaluation
+//--------------------------------------------------------------------------------------------------
+/* bool verify_sumcheck_no_evaluation(s25t::element& expected_sum, */
+/*                                    basct::span<s25t::element> evaluation_point, */
+/*                                    sumcheck_transcript& transcript, */
+/*                                    basct::cspan<s25t::element> round_polynomials, */
+/*                                    unsigned round_degree) noexcept; */
+} // namespace sxt::prfsk2
