19db19ade2afc721151b9ede4cf554c2cf8c1b22
rnburn <ryan.burn@gmail.com>
2025-02-18 17:09:50 -0800

    rework sumcheck

--- sxt/proof/sumcheck2/verification.t.cc
+++ sxt/proof/sumcheck2/verification.t.cc
@@ -1,5 +1,16 @@
 #include "sxt/proof/sumcheck2/verification.h"
 
+#include <vector>
+
 #include "sxt/base/test/unit_test.h"
+/* #include "sxt/proof/sumcheck/reference_transcript.h" */
+#include "sxt/proof/transcript/transcript.h"
+#include "sxt/scalar25/operation/overload.h"
+#include "sxt/scalar25/type/element.h"
+#include "sxt/scalar25/type/literal.h"
+
+using namespace sxt;
+using namespace sxt::prfsk2;
+using sxt::s25t::operator""_s25;
 
 TEST_CASE("todo") {}
