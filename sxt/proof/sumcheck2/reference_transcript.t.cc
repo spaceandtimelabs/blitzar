2bff522c42aebe4e6d89d2975b1a0b39f2230a78
rnburn <ryan.burn@gmail.com>
2025-02-18 22:15:33 -0800

    add tests

--- sxt/proof/sumcheck2/reference_transcript.t.cc
+++ sxt/proof/sumcheck2/reference_transcript.t.cc
@@ -1,5 +1,43 @@
 #include "sxt/proof/sumcheck2/reference_transcript.h"
 
 #include "sxt/base/test/unit_test.h"
+#include "sxt/proof/transcript/transcript.h"
+#include "sxt/scalar25/operation/overload.h"
+#include "sxt/scalar25/realization/field.h"
+#include "sxt/scalar25/type/literal.h"
 
-TEST_CASE("todo") {}
+using namespace sxt;
+using namespace sxt::prfsk2;
+using sxt::s25t::operator""_s25;
+
+TEST_CASE("we provide an implementation of sumcheck transcript") {
+  using T = s25t::element;
+  prft::transcript base_transcript{"abc"};
+  reference_transcript<T> transcript{base_transcript};
+  std::vector<T> p = {0x123_s25};
+  s25t::element r, rp;
+
+  SECTION("we don't draw the same challenge from a transcript") {
+    transcript.round_challenge(r, p);
+    transcript.round_challenge(rp, p);
+    REQUIRE(r != rp);
+
+    prft::transcript base_transcript_p{"abc"};
+    reference_transcript<T> transcript_p{base_transcript_p};
+    p[0] = 0x456_s25;
+    transcript_p.round_challenge(rp, p);
+    REQUIRE(r != rp);
+  }
+
+  SECTION("init_transcript produces different results based on parameters") {
+    transcript.init(1, 2);
+    transcript.round_challenge(r, p);
+
+    prft::transcript base_transcript_p{"abc"};
+    reference_transcript<T> transcript_p{base_transcript_p};
+    transcript.init(2, 1);
+    transcript.round_challenge(rp, p);
+
+    REQUIRE(r != rp);
+  }
+}
