29a3fc4178e63cfe68e92e90d7533656c543c7c8
rnburn <ryan.burn@gmail.com>
2025-02-18 17:26:35 -0800

    fill in reference transcript

--- sxt/proof/sumcheck2/reference_transcript.h
+++ sxt/proof/sumcheck2/reference_transcript.h
@@ -2,8 +2,9 @@
 
 #include "sxt/proof/sumcheck2/sumcheck_transcript.h"
 #include "sxt/proof/transcript/transcript.h"
+#include "sxt/proof/transcript/transcript_utility.h"
 
-namespace sxt::prfsk {
+namespace sxt::prfsk2 {
 //--------------------------------------------------------------------------------------------------
 // reference_transcript
 //--------------------------------------------------------------------------------------------------
@@ -13,16 +14,17 @@ public:
   explicit reference_transcript(prft::transcript& transcript) noexcept;
 
   void init(size_t num_variables, size_t round_degree) noexcept {
-    (void)num_variables;
-    (void)round_degree;
+    prft::set_domain(transcript_, "sumcheck proof v1");
+    prft::append_value(transcript_, "n", num_variables);
+    prft::append_value(transcript_, "k", round_degree);
   }
 
   void round_challenge(T& r, basct::cspan<T> polynomial) noexcept {
-    (void)r;
-    (void)polynomial;
+    prft::append_values(transcript_, "P", polynomial);
+    prft::challenge_value(r, transcript_, "R");
   }
 
 private:
   prft::transcript& transcript_;
 };
-} // namespace sxt::prfsk
+} // namespace sxt::prfsk2
