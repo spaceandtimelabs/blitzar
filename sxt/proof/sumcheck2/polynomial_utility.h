d6186d4947a618e7569dcbff877fd3863a64edf7
rnburn <ryan.burn@gmail.com>
2025-02-19 20:09:53 -0800

    rework sumcheck

--- sxt/proof/sumcheck2/polynomial_utility.h
+++ sxt/proof/sumcheck2/polynomial_utility.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cassert>
+
 #include "sxt/base/container/span.h"
 #include "sxt/base/field/element.h"
+#include "sxt/base/macro/cuda_callable.h"
 
 namespace sxt::prfsk2 {
 //--------------------------------------------------------------------------------------------------
@@ -38,6 +41,47 @@ void evaluate_polynomial(T& e, basct::cspan<T> polynomial, const T& x) noexcept 
   while (i > 0) {
     --i;
     muladd(e, e, x, polynomial[i]);
+  }
+}
+
+//--------------------------------------------------------------------------------------------------
+// expand_products
+//--------------------------------------------------------------------------------------------------
+template <basfld::element T>
+CUDA_CALLABLE
+void expand_products(basct::span<T> p, const T* mles, unsigned n,
+                     unsigned step, basct::cspan<unsigned> terms) noexcept {
+  auto num_terms = terms.size();
+  assert(
+      // clang-format off
+      num_terms > 0 && 
+      n > step &&
+      p.size() == num_terms + 1u
+      // clang-format on
+  );
+  T a, b;
+  auto mle_index = terms[0];
+  a = *(mles + mle_index * n);
+  b = *(mles + mle_index * n + step);
+  sub(b, b, a);
+  p[0] = a;
+  p[1] = b;
+
+  for (unsigned i = 1; i < num_terms; ++i) {
+    auto mle_index = terms[i];
+    a = *(mles + mle_index * n);
+    b = *(mles + mle_index * n + step);
+    sub(b, b, a);
+
+    auto c_prev = p[0];
+    mul(p[0], c_prev, a);
+    for (unsigned pow = 1u; pow < i + 1u; ++pow) {
+      auto c = p[pow];
+      mul(p[pow], c, a);
+      muladd(p[pow], c_prev, b, p[pow]);
+      c_prev = c;
+    }
+    mul(p[i + 1u], c_prev, b);
   }
 }
 } // namespace sxt::prfsk2
