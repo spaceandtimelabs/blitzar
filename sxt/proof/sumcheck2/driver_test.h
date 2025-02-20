d5574b02118ec0be63270bc71fb54b082e94257e
rnburn <ryan.burn@gmail.com>
2025-02-20 14:52:36 -0800

    add driver test

---
+++ sxt/proof/sumcheck2/driver_test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "sxt/proof/sumcheck2/driver.h"
+#include "sxt/scalar25/realization/field.h"
+
+namespace sxt::prfsk2 {
+//--------------------------------------------------------------------------------------------------
+// exercise_driver
+//--------------------------------------------------------------------------------------------------
+void exercise_driver(const driver<s25t::element>& drv);
+} // namespace sxt::prfsk2
