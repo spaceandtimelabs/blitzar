f09aab1f2f250ec96f78bce4ae7e0b56cd5b8b55
rnburn <ryan.burn@gmail.com>
2025-02-24 09:51:39 -0800

    rework sumcheck

--- sxt/proof/sumcheck2/gpu_driver.t.cc
+++ sxt/proof/sumcheck2/gpu_driver.t.cc
@@ -1,5 +1,12 @@
 #include "sxt/proof/sumcheck2/gpu_driver.h"
 
+#include "sxt/proof/sumcheck2/driver_test.h"
 #include "sxt/base/test/unit_test.h"
 
-TEST_CASE("todo") {}
+using namespace sxt;
+using namespace sxt::prfsk2;
+
+TEST_CASE("we can perform the primitive operations for sumcheck proofs") {
+  gpu_driver<s25t::element> drv;
+  exercise_driver(drv);
+}
