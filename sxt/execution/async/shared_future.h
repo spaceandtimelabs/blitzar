0866b3c30a74fd0c662a670e220bc5f380e720ba
rnburn <ryan.burn@gmail.com>
2025-03-18 18:33:35 -0700

    fill in shared future

--- sxt/execution/async/shared_future.h
+++ sxt/execution/async/shared_future.h
@@ -1,19 +1,23 @@
 #pragma once
 
 #include "sxt/execution/async/future.h"
+#include "sxt/execution/async/shared_future_state.h"
+#include <utility>
 
 namespace sxt::xena {
 //--------------------------------------------------------------------------------------------------
 // shared_future
 //--------------------------------------------------------------------------------------------------
-template <class T>
-class shared_future {
- public:
-   shared_future() noexcept = default;
+template <class T> class shared_future {
+public:
+  shared_future() noexcept = default;
 
-   future<T> get_future() const noexcept {
-      return {};
-   }
- private:
+  shared_future(future<T>&& fut) noexcept
+      : state_{std::make_shared<shared_future_state<T>>(std::move(fut))} {}
+
+  future<T> get_future() const noexcept { return state_->get_future(); }
+
+private:
+  std::shared_ptr<shared_future_state<T>> state_;
 };
 } // namespace sxt::xena
