28c5f995710ca3e0c14ef65e632aeb4ba8189280
rnburn <ryan.burn@gmail.com>
2025-03-21 13:28:11 -0700

    break down tests

--- sxt/execution/async/shared_future_state.t.cc
+++ sxt/execution/async/shared_future_state.t.cc
@@ -3,3 +3,9 @@
 #include "sxt/base/test/unit_test.h"
 
 TEST_CASE("todo") {}
+// shared future is kept alive even if there are no references to it
+// shared future is destroyed
+// we can create a future from a ready state
+// we can create a future from shared future state
+// we can create multiple futures from shared future state
+// we can create both events with a value and void events
