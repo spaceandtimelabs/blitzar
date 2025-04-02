b44b4722f5418513bf3c020f3f4956c12a26a6b5
rnburn <ryan.burn@gmail.com>
2025-04-02 13:59:55 -0700

    pseudocode

--- sxt/execution/device/basic_copier.cc
+++ sxt/execution/device/basic_copier.cc
@@ -1,1 +1,33 @@
 #include "sxt/execution/device/basic_copier.h"
+
+#include "sxt/execution/async/coroutine.h"
+
+namespace sxt::xendv {
+//--------------------------------------------------------------------------------------------------
+// copy
+//--------------------------------------------------------------------------------------------------
+xena::future<> basic_copier::copy(basct::cspan<std::byte> src) noexcept {
+  if (dst_.empty() || src.empty()) {
+    co_return;
+  }
+
+    // fill active buffer
+   // assert(!active_buffer_.empty());
+   // copy(active_buffer_, src);
+   // src <- rest
+
+
+  // if (!active_buffer.full() && !dst.full()) {
+  //    co_return;
+  // }
+  //
+  // fut = copy(dst, active_buffer);
+  // co_await alt_future;
+  // if (dst.full()) {
+  //    co_await fut;
+  //    co_return;
+  // }
+  // std::swap(active_buffer_, alt_buffer_);
+  // alt_future = std::move(fut);
+}
+} // sxt::xendv
--- sxt/execution/device/basic_copier.h
+++ sxt/execution/device/basic_copier.h
@@ -3,6 +3,7 @@
 #include <optional>
 
 #include "sxt/base/container/span.h"
+#include "sxt/base/device/pinned_buffer.h"
 #include "sxt/execution/async/future.h"
 
 namespace sxt::xendv {
@@ -15,5 +16,12 @@ class basic_copier {
    xena::future<> copy(basct::cspan<std::byte> src) noexcept;
 
  private:
+   basct::span<std::byte> dst_;
+
+   size_t active_count_ = 0;
+   std::optional<basdv::pinned_buffer> active_buffer_;
+
+   std::optional<basdv::pinned_buffer> alt_buffer_;
+   xena::future<> alt_future_;
 };
 } // sxt::xendv
