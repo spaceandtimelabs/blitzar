2506ae1736cb0601a9ea3eff352df36f4082a22d
rnburn <ryan.burn@gmail.com>
2025-04-07 10:58:37 -0700

    fill in testing

--- sxt/execution/device/to_device_copier.t.cc
+++ sxt/execution/device/to_device_copier.t.cc
@@ -1,6 +1,8 @@
 #include "sxt/execution/device/to_device_copier.h"
 
 #include "sxt/base/test/unit_test.h"
+#include "sxt/memory/management/managed_array.h"
+#include "sxt/memory/resource/managed_device_resource.h"
 using namespace sxt;
 using namespace sxt::xendv;
 
