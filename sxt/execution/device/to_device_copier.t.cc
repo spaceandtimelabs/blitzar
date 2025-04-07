29637ec035e34ebb62c6e2e51e38cce762e3529a
rnburn <ryan.burn@gmail.com>
2025-04-07 12:45:21 -0700

    fill in tests

--- sxt/execution/device/to_device_copier.t.cc
+++ sxt/execution/device/to_device_copier.t.cc
@@ -6,4 +6,14 @@
 using namespace sxt;
 using namespace sxt::xendv;
 
-TEST_CASE("todo") {}
+TEST_CASE("we can copy memory from host to device") {
+  memmg::managed_array<int> dev{memr::get_managed_device_resource()};
+  std::vector<int> host;
+  basdv::stream stream;
+
+  SECTION("we can copy an empty array") {
+    to_device_copier copier{dev, stream};
+    auto fut = copier.copy(host);
+    REQUIRE(fut.ready());
+  }
+}
