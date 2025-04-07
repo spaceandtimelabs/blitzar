5a022217a028d3f9ace2586618242bbd381a25a5
rnburn <ryan.burn@gmail.com>
2025-04-07 13:34:53 -0700

    fill in tests

--- sxt/execution/device/to_device_copier.t.cc
+++ sxt/execution/device/to_device_copier.t.cc
@@ -1,16 +1,19 @@
 #include "sxt/execution/device/to_device_copier.h"
 
+#include <numeric>
+#include <vector>
+#include <memory_resource>
+#include "sxt/base/device/pinned_buffer_pool.h"
 #include "sxt/base/device/synchronization.h"
 #include "sxt/base/test/unit_test.h"
 #include "sxt/execution/schedule/scheduler.h"
-#include "sxt/memory/management/managed_array.h"
 #include "sxt/memory/resource/managed_device_resource.h"
 using namespace sxt;
 using namespace sxt::xendv;
 
 TEST_CASE("we can copy memory from host to device") {
-  memmg::managed_array<int> dev{memr::get_managed_device_resource()};
-  std::vector<int> host;
+  std::pmr::vector<int> dev{memr::get_managed_device_resource()};
+  std::pmr::vector<int> host;
   basdv::stream stream;
 
   SECTION("we can copy an empty array") {
@@ -29,8 +32,7 @@ TEST_CASE("we can copy memory from host to device") {
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
-    memmg::managed_array<int> expected = {123};
-    REQUIRE(dev == expected);
+    REQUIRE(dev == host);
   }
 
   SECTION("we can copy two integers in to two passes") {
@@ -48,7 +50,20 @@ TEST_CASE("we can copy memory from host to device") {
     xens::get_scheduler().run();
     REQUIRE(fut.ready());
     basdv::synchronize_device();
-    memmg::managed_array<int> expected = {123, 456};
+    std::pmr::vector<int> expected = {123, 456};
     REQUIRE(dev == expected);
+  }
+
+  SECTION("we can copy a full buffer") {
+    dev.resize(basdv::pinned_buffer_size / sizeof(int));
+    to_device_copier copier{dev, stream};
+    host.resize(dev.size());
+    std::iota(host.begin(), host.end(), 0);
+    auto fut = copier.copy(host);
+    REQUIRE(!fut.ready());
+    xens::get_scheduler().run();
+    REQUIRE(fut.ready());
+    basdv::synchronize_device();
+    REQUIRE(dev == host);
   }
 }
