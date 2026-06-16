c757e50deb698b498ab168f60fa523bec5ec3851
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-06-16 16:16:23 +0000

    fix: format bazel.nix
    A recent release workflow created a new tag, and new crates, but failed
    to create a github release due to firewall issues. This introduces a
    minor change with `fix:` in an attempt to increment the release version
    and retry creating a new release.

--- nix/bazel.nix
+++ nix/bazel.nix
@@ -1,5 +1,8 @@
-{ pkgs, clang, cuda }:
-let
+{
+  pkgs,
+  clang,
+  cuda,
+}: let
   # Set PATH so that it only includes things bazel needs.
   path = pkgs.lib.strings.concatStringsSep ":" [
     "${clang}/bin"
@@ -16,21 +19,20 @@ let
   ];
   bazel = "${pkgs.bazel_7}/bin/bazel";
 in
-pkgs.writeShellScriptBin "bazel" ''
-  if [[
-    "$1" == "build" ||
-    "$1" == "test" ||
-    "$1" == "run"
-  ]]; then
-    exec ${bazel} $1 \
-     --action_env CC=${clang}/bin/clang \
-     --action_env CXX=${clang}/bin/clang++ \
-     --action_env PATH="${path}" \
-     --action_env CUDA_PATH="${cuda}" \
-     --action_env=BAZEL_LINKLIBS='-l%:libc++.a' \
-     --action_env=BAZEL_LINKOPTS='-L${clang}/lib' \
-     ''${@:2}
-  else
-    exec ${bazel} $@
-  fi''
-
+  pkgs.writeShellScriptBin "bazel" ''
+    if [[
+      "$1" == "build" ||
+      "$1" == "test" ||
+      "$1" == "run"
+    ]]; then
+      exec ${bazel} $1 \
+       --action_env CC=${clang}/bin/clang \
+       --action_env CXX=${clang}/bin/clang++ \
+       --action_env PATH="${path}" \
+       --action_env CUDA_PATH="${cuda}" \
+       --action_env=BAZEL_LINKLIBS='-l%:libc++.a' \
+       --action_env=BAZEL_LINKOPTS='-L${clang}/lib' \
+       ''${@:2}
+    else
+      exec ${bazel} $@
+    fi''
